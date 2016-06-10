c4e83c7ed5a82343625cacac297d66167f7b22f7
Alex Southan <southan@users.noreply.github.com>
2016-06-10 15:59:37 +0100

    Support hyphens in locales and language codes.

--- js/admin.js
+++ js/admin.js
@@ -56,8 +56,8 @@ jQuery( document ).ready(function( $ ) {
 	// languages form
 	// fills the fields based on the language dropdown list choice
 	$( '#lang_list' ).change(function() {
-		value = $( this ).val().split( '-' );
-		selected = $( "select option:selected" ).text().split( ' - ' );
+		var value = $( this ).val().split( ':' );
+		var selected = $( "select option:selected" ).text().split( ' - ' );
 		$( '#lang_slug' ).val( value[0] );
 		$( '#lang_locale' ).val( value[1] );
 		$( 'input[name="rtl"]' ).val( [value[2]] );
@@ -96,7 +96,7 @@ jQuery( document ).ready(function( $ ) {
 	// save settings
 	$( '#the-list' ).on( 'click', '.save', function(){
 		var tr = $( this ).closest( 'tr' );
-		var parts = tr.attr( 'id' ).split( '-' );
+		var parts = tr.attr( 'id' ).split( ':' );
 
 		var data = {
 			action:            'pll_save_options',
