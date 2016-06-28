aac16a97ec0826b9ab497a5ebde7a09386516f98
Frédéric Demarle <frederic.demarle@yahoo.fr>
2016-06-28 08:11:00 +0200

    Version 1.9.3

--- polylang.php
+++ polylang.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Polylang
 Plugin URI: https://polylang.pro
-Version: 1.9.2
+Version: 1.9.3
 Author: Frédéric Demarle
 Author uri: https://polylang.pro
 Description: Adds multilingual capability to WordPress
@@ -35,7 +35,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // don't access directly
 };
 
-define( 'POLYLANG_VERSION', '1.9.2' );
+define( 'POLYLANG_VERSION', '1.9.3' );
 define( 'PLL_MIN_WP_VERSION', '4.0' );
 
 define( 'POLYLANG_FILE', __FILE__ ); // this file
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://polylang.pro
 Tags: multilingual, bilingual, translate, translation, language, multilanguage, international, localization
 Requires at least: 4.0
 Tested up to: 4.5
-Stable tag: 1.9.2
+Stable tag: 1.9.3
 License: GPLv2 or later
 
 Making WordPress multilingual
