4e41d4e6dee7b07778cb1dbd7a449be931b68e14
Frédéric Demarle <frederic.demarle@yahoo.fr>
2016-06-28 17:23:13 +0200

    Fix impossible to directly enter the page number in strings translation table
    Fixes #29

--- settings/table-string.php
+++ settings/table-string.php
@@ -322,8 +322,8 @@ class PLL_Table_String extends WP_List_Table {
 		}
 
 		// To refresh the page ( possible thanks to the $_GET['noheader']=true )
-		$args = array_intersect_key( $_REQUEST, array_flip( array( 's', 'group' ) ) );
-		if ( ! empty( $_GET['paged'] ) ) {
+		$args = array_intersect_key( $_REQUEST, array_flip( array( 's', 'paged', 'group' ) ) );
+		if ( ! empty( $_GET['paged'] ) && ! empty( $_POST['submit'] ) ) {
 			$args['paged'] = (int) $_GET['paged']; // Don't rely on $_REQUEST['paged'] or $_POST['paged']. See #14
 		}
 		if ( ! empty( $args['s'] ) ) {
