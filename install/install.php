e666c7888f8c396b91c1f41377a18666495559a8
Frédéric Demarle <frederic.demarle@yahoo.fr>
2016-09-09 10:06:37 +0200

    Disable the translation of custom post types and custom taxonomies by default
    Previously, it was activated for show_ui === true

--- install/install.php
+++ install/install.php
@@ -48,8 +48,8 @@ class PLL_Install extends PLL_Install_Base {
 			'media_support' => 1, // support languages and translation for media by default
 			'uninstall'     => 0, // do not remove data when uninstalling Polylang
 			'sync'          => array(), // synchronisation is disabled by default ( was the opposite before 1.2 )
-			'post_types'    => array_values( get_post_types( array( '_builtin' => false, 'show_ui' => true ) ) ),
-			'taxonomies'    => array_values( get_taxonomies( array( '_builtin' => false, 'show_ui' => true ) ) ),
+			'post_types'    => array(),
+			'taxonomies'    => array(),
 			'domains'       => array(),
 			'version'       => POLYLANG_VERSION,
 		);
