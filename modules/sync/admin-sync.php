d5f5c5c12cc569d5cad85c2a7389315478260068
Frédéric Demarle <frederic.demarle@yahoo.fr>
2016-11-09 12:11:51 +0100

    Fix attachment post parent when created at the same time as a post translation
    1. When the duplicate content option is activated and the page parent
    sync is activated and the original post contains an untranslated media,
    the original media lost its post_parent when creating the media
    translation on the fly.
    
    2. When the duplicate content is activated and the original child page
    containts an untranslated media, the translated media created on the fly
    is attached to the translatet parent page instead of the translated
    child page

--- modules/sync/admin-sync.php
+++ modules/sync/admin-sync.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * manages copy and synchronization of terms and post metas
+ * Manages copy and synchronization of terms and post metas
  *
  * @since 1.2
  */
 class PLL_Admin_Sync {
 
 	/**
-	 * constructor
+	 * Constructor
 	 *
 	 * @since 1.2
 	 *
@@ -18,8 +18,8 @@ class PLL_Admin_Sync {
 		$this->model = &$polylang->model;
 		$this->options = &$polylang->options;
 
-		add_filter( 'wp_insert_post_parent', array( $this, 'wp_insert_post_parent' ) );
-		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ), 5, 2 ); // before Types which populates custom fields in same hook with priority 10
+		add_filter( 'wp_insert_post_parent', array( $this, 'wp_insert_post_parent' ), 10, 3 );
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ), 5, 2 ); // Before Types which populates custom fields in same hook with priority 10
 
 		add_action( 'pll_save_post', array( $this, 'pll_save_post' ), 10, 3 );
 		add_action( 'pll_save_term', array( $this, 'pll_save_term' ), 10, 3 );
@@ -32,19 +32,22 @@ class PLL_Admin_Sync {
 	}
 
 	/**
-	 * translate post parent if exists when using "Add new" ( translation )
+	 * Translate post parent if exists when using "Add new" ( translation )
 	 *
 	 * @since 0.6
 	 *
-	 * @param int $post_parent
+	 * @param int   $post_parent Post parent ID
+	 * @param int   $post_id     Post ID, unused
+	 * @param array $postarr     Array of parsed post data
 	 * @return int
 	 */
-	public function wp_insert_post_parent( $post_parent ) {
-		return isset( $_GET['from_post'], $_GET['new_lang'] ) && ( $id = wp_get_post_parent_id( (int) $_GET['from_post'] ) ) && ( $parent = $this->model->post->get_translation( $id, $_GET['new_lang'] ) ) ? $parent : $post_parent;
+	public function wp_insert_post_parent( $post_parent, $post_id, $postarr ) {
+		// Make sure not to impact media translations created at the same time
+		return isset( $_GET['from_post'], $_GET['new_lang'], $_GET['post_type'] ) && $_GET['post_type'] === $postarr['post_type'] && ( $id = wp_get_post_parent_id( (int) $_GET['from_post'] ) ) && ( $parent = $this->model->post->get_translation( $id, $_GET['new_lang'] ) ) ? $parent : $post_parent;
 	}
 
 	/**
-	 * copy post metas, menu order, comment and ping status when using "Add new" ( translation )
+	 * Copy post metas, menu order, comment and ping status when using "Add new" ( translation )
 	 * formerly used dbx_post_advanced deprecated in WP 3.7
 	 *
 	 * @since 1.2
@@ -54,7 +57,7 @@ class PLL_Admin_Sync {
 	 */
 	public function add_meta_boxes( $post_type, $post ) {
 		if ( 'post-new.php' == $GLOBALS['pagenow'] && isset( $_GET['from_post'], $_GET['new_lang'] ) && $this->model->is_translated_post_type( $post->post_type ) ) {
-			// capability check already done in post-new.php
+			// Capability check already done in post-new.php
 			$from_post_id = (int) $_GET['from_post'];
 			$from_post = get_post( $from_post_id );
 			$lang = $this->model->get_language( $_GET['new_lang'] );
@@ -77,7 +80,7 @@ class PLL_Admin_Sync {
 	}
 
 	/**
-	 * get the list of taxonomies to copy or to synchronize
+	 * Get the list of taxonomies to copy or to synchronize
 	 *
 	 * @since 1.7
 	 *
@@ -102,7 +105,7 @@ class PLL_Admin_Sync {
 	}
 
 	/**
-	 * copy or synchronize terms
+	 * Copy or synchronize terms
 	 *
 	 * @since 1.8
 	 *
@@ -112,29 +115,29 @@ class PLL_Admin_Sync {
 	 * @param bool $sync true if it is synchronization, false if it is a copy, defaults to false
 	 */
 	public function copy_taxonomies( $from, $to, $lang, $sync = false ) {
-		// get taxonomies to sync for this post type
+		// Get taxonomies to sync for this post type
 		$taxonomies = array_intersect( get_post_taxonomies( $from ), $this->get_taxonomies_to_copy( $sync ) );
 
-		// update the term cache to reduce the number of queries in the loop
+		// Update the term cache to reduce the number of queries in the loop
 		update_object_term_cache( $sync ? array( $from, $to ) : $from, get_post_type( $from ) );
 
-		// copy or synchronize terms
+		// Copy or synchronize terms
 		// FIXME quite a lot of query in foreach
 		foreach ( $taxonomies as $tax ) {
 			$terms = get_the_terms( $from, $tax );
 
-			// translated taxonomy
+			// Translated taxonomy
 			if ( $this->model->is_translated_taxonomy( $tax ) ) {
 				$newterms = array();
 				if ( is_array( $terms ) ) {
 					foreach ( $terms as $term ) {
 						if ( $term_id = $this->model->term->get_translation( $term->term_id, $lang ) ) {
-							$newterms[] = (int) $term_id; // cast is important otherwise we get 'numeric' tags
+							$newterms[] = (int) $term_id; // Cast is important otherwise we get 'numeric' tags
 						}
 					}
 				}
 
-				// for some reasons, the user may have untranslated terms in the translation. don't forget them.
+				// For some reasons, the user may have untranslated terms in the translation. don't forget them.
 				if ( $sync ) {
 					$tr_terms = get_the_terms( $to, $tax );
 					if ( is_array( $tr_terms ) ) {
@@ -151,8 +154,8 @@ class PLL_Admin_Sync {
 				}
 			}
 
-			// untranslated taxonomy ( post format )
-			// don't use simple get_post_format / set_post_format to generalize the case to other taxonomies
+			// Untranslated taxonomy ( post format )
+			// Don't use simple get_post_format / set_post_format to generalize the case to other taxonomies
 			else {
 				wp_set_object_terms( $to, is_array( $terms ) ? array_map( 'intval', wp_list_pluck( $terms, 'term_id' ) ) : null, $tax );
 			}
@@ -160,21 +163,21 @@ class PLL_Admin_Sync {
 	}
 
 	/**
-	 * copy or synchronize metas (custom fields)
+	 * Copy or synchronize metas (custom fields)
 	 *
 	 * @since 0.9
 	 *
 	 * @param int    $from id of the post from which we copy informations
 	 * @param int    $to   id of the post to which we paste informations
 	 * @param string $lang language slug
-	 * @param bool $sync true if it is synchronization, false if it is a copy, defaults to false
+	 * @param bool   $sync true if it is synchronization, false if it is a copy, defaults to false
 	 */
 	public function copy_post_metas( $from, $to, $lang, $sync = false ) {
-		// copy or synchronize post metas and allow plugins to do the same
+		// Copy or synchronize post metas and allow plugins to do the same
 		$metas = get_post_custom( $from );
 		$keys = array();
 
-		// get public meta keys ( including from translated post in case we just deleted a custom field )
+		// Get public meta keys ( including from translated post in case we just deleted a custom field )
 		if ( ! $sync || in_array( 'post_meta', $this->options['sync'] ) ) {
 			foreach ( $keys = array_unique( array_merge( array_keys( $metas ), array_keys( get_post_custom( $to ) ) ) ) as $k => $meta_key ) {
 				if ( is_protected_meta( $meta_key ) ) {
@@ -183,7 +186,7 @@ class PLL_Admin_Sync {
 			}
 		}
 
-		// add page template and featured image
+		// Add page template and featured image
 		foreach ( array( '_wp_page_template', '_thumbnail_id' ) as $meta ) {
 			if ( ! $sync || in_array( $meta, $this->options['sync'] ) ) {
 				$keys[] = $meta;
@@ -204,15 +207,15 @@ class PLL_Admin_Sync {
 		 */
 		$keys = array_unique( apply_filters( 'pll_copy_post_metas', $keys, $sync, $from, $to, $lang ) );
 
-		// and now copy / synchronize
+		// And now copy / synchronize
 		foreach ( $keys as $key ) {
-			delete_post_meta( $to, $key ); // the synchronization process of multiple values custom fields is easier if we delete all metas first
+			delete_post_meta( $to, $key ); // The synchronization process of multiple values custom fields is easier if we delete all metas first
 			if ( isset( $metas[ $key ] ) ) {
 				foreach ( $metas[ $key ] as $value ) {
-					// important: always maybe_unserialize value coming from get_post_custom. See codex.
-					// thanks to goncalveshugo http://wordpress.org/support/topic/plugin-polylang-pll_copy_post_meta
+					// Important: always maybe_unserialize value coming from get_post_custom. See codex.
+					// Thanks to goncalveshugo http://wordpress.org/support/topic/plugin-polylang-pll_copy_post_meta
 					$value = maybe_unserialize( $value );
-					// special case for featured images which can be translated
+					// Special case for featured images which can be translated
 					add_post_meta( $to, $key, ( '_thumbnail_id' == $key && $tr_value = $this->model->post->get_translation( $value, $lang ) ) ? $tr_value : $value );
 				}
 			}
@@ -220,7 +223,7 @@ class PLL_Admin_Sync {
 	}
 
 	/**
-	 * synchronizes terms and metas in translations
+	 * Synchronizes terms and metas in translations
 	 *
 	 * @since 1.2
 	 *
@@ -229,9 +232,9 @@ class PLL_Admin_Sync {
 	 * @param array  $translations post translations
 	 */
 	public function pll_save_post( $post_id, $post, $translations ) {
-		global $wpdb;
+		global $wpdb, $post_type;
 
-		// prepare properties to synchronize
+		// Prepare properties to synchronize
 		foreach ( array( 'comment_status', 'ping_status', 'menu_order', 'post_date' ) as $property ) {
 			if ( in_array( $property, $this->options['sync'] ) ) {
 				$postarr[ $property ] = $post->$property;
@@ -242,35 +245,36 @@ class PLL_Admin_Sync {
 			$postarr['post_date_gmt'] = $post->post_date_gmt;
 		}
 
-		// synchronize terms and metas in translations
+		// Synchronize terms and metas in translations
 		foreach ( $translations as $lang => $tr_id ) {
 			if ( ! $tr_id || $tr_id === $post_id ) {
 				continue;
 			}
 
-			// synchronize terms and metas
+			// Synchronize terms and metas
 			$this->copy_taxonomies( $post_id, $tr_id, $lang, true );
 			$this->copy_post_metas( $post_id, $tr_id, $lang, true );
 
-			// sticky posts
+			// Sticky posts
 			if ( in_array( 'sticky_posts', $this->options['sync'] ) ) {
 				isset( $_REQUEST['sticky'] ) ? stick_post( $tr_id ) : unstick_post( $tr_id );
 			}
 
-			// add comment status, ping status, menu order... to synchronization
+			// Add comment status, ping status, menu order... to synchronization
 			$tr_arr = empty( $postarr ) ? array() : $postarr;
 
-			// add post parent to synchronization
-			// do not udpate the translation parent if the user set a parent with no translation
-			if ( in_array( 'post_parent', $this->options['sync'] ) ) {
+			// Add post parent to synchronization
+			// Make sure not to impact media translations when creating them at the same time as post
+			// Do not udpate the translation parent if the user set a parent with no translation
+			if ( in_array( 'post_parent', $this->options['sync'] ) && $post_type === $post->post_type ) {
 				$post_parent = ( $parent_id = wp_get_post_parent_id( $post_id ) ) ? $this->model->post->get_translation( $parent_id, $lang ) : 0;
 				if ( ! ( $parent_id && ! $post_parent ) ) {
 					$tr_arr['post_parent'] = $post_parent;
 				}
 			}
 
-			// update all the row at once
-			// don't use wp_update_post to avoid infinite loop
+			// Update all the row at once
+			// Don't use wp_update_post to avoid infinite loop
 			if ( ! empty( $tr_arr ) ) {
 				$wpdb->update( $wpdb->posts, $tr_arr, array( 'ID' => $tr_id ) );
 				clean_post_cache( $tr_id );
@@ -279,7 +283,7 @@ class PLL_Admin_Sync {
 	}
 
 	/**
-	 * synchronize translations of a term in all posts
+	 * Synchronize translations of a term in all posts
 	 *
 	 * @since 1.2
 	 *
@@ -288,12 +292,12 @@ class PLL_Admin_Sync {
 	 * @param array  $translations translations of the term
 	 */
 	public function pll_save_term( $term_id, $taxonomy, $translations ) {
-		// check if the taxonomy is synchronized
+		// Check if the taxonomy is synchronized
 		if ( ! $this->model->is_translated_taxonomy( $taxonomy ) || ! in_array( $taxonomy, $this->get_taxonomies_to_copy( true ) ) ) {
 			return;
 		}
 
-		// get all posts associated to this term
+		// Get all posts associated to this term
 		$posts = get_posts( array(
 			'numberposts' => -1,
 			'nopaging'    => true,
@@ -308,7 +312,7 @@ class PLL_Admin_Sync {
 			) ),
 		) );
 
-		// associate translated term to translated post
+		// Associate translated term to translated post
 		// FIXME quite a lot of query in foreach
 		foreach ( $this->model->get_languages_list() as $language ) {
 			if ( $translated_term = $this->model->term->get( $term_id, $language ) ) {
@@ -320,16 +324,16 @@ class PLL_Admin_Sync {
 			}
 		}
 
-		// synchronize parent in translations
-		// calling clean_term_cache *after* this is mandatory otherwise the $taxonomy_children option is not correctly updated
-		// before WP 3.9 clean_term_cache could be called ( efficiently ) only one time due to static array which prevented to update the option more than once
-		// this is the reason to use the edit_term filter and not edited_term
-		// take care that $_POST contains the only valid values for the current term
+		// Synchronize parent in translations
+		// Calling clean_term_cache *after* this is mandatory otherwise the $taxonomy_children option is not correctly updated
+		// Before WP 3.9 clean_term_cache could be called ( efficiently ) only one time due to static array which prevented to update the option more than once
+		// This is the reason to use the edit_term filter and not edited_term
+		// Take care that $_POST contains the only valid values for the current term
 		// FIXME can I synchronize parent without using $_POST instead?
 		if ( isset( $_POST['term_tr_lang'] ) ) {
 			foreach ( $_POST['term_tr_lang'] as $lang => $tr_id ) {
 				if ( $tr_id ) {
-					if ( isset( $_POST['parent'] ) && -1 != $_POST['parent'] ) { // since WP 3.1
+					if ( isset( $_POST['parent'] ) && -1 != $_POST['parent'] ) { // Since WP 3.1
 						$term_parent = $this->model->term->get_translation( (int) $_POST['parent'], $lang );
 					}
 
@@ -346,7 +350,7 @@ class PLL_Admin_Sync {
 	}
 
 	/**
-	 * synchronizes terms and metas in translations for media
+	 * Synchronizes terms and metas in translations for media
 	 *
 	 * @since 1.8
 	 *
