02dbe11a26b262bb29e92d5d2fa8aea214fdad3a
Frédéric Demarle <frederic.demarle@yahoo.fr>
2016-11-21 08:24:27 +0100

    Remove draft posts from the language switcher even when user is logged in

--- include/links.php
+++ include/links.php
@@ -53,8 +53,13 @@ class PLL_Links {
 			return true;
 		}
 
-		$post_type_object = get_post_type_object( $post->post_type );
-		$user = wp_get_current_user();
-		return is_user_logged_in() && ( current_user_can( $post_type_object->cap->read_private_posts ) || $user->ID == $post->post_author ); // Comparison must not be strict!
+		// Follow WP practices, which shows links to private posts ( when readable ), but not for draft posts ( ex: get_adjacent_post_link() )
+		if ( in_array( $post->post_status, get_post_stati( array( 'private' => true ) ) ) ) {
+			$post_type_object = get_post_type_object( $post->post_type );
+			$user = wp_get_current_user();
+			return is_user_logged_in() && ( current_user_can( $post_type_object->cap->read_private_posts ) || $user->ID == $post->post_author ); // Comparison must not be strict!
+		}
+
+		return false;
 	}
 }
