b88bac23028b8b5a838fc2c0e8c8569aaad54e3e
Frédéric Demarle <frederic.demarle@yahoo.fr>
2016-11-22 11:35:37 +0100

    Move strings translations to post meta
    In an attempt to fix unserialize issues due to post content being
    filtered (by balanceTags() or whatever other function).
    Fix #63

--- include/mo.php
+++ include/mo.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- * manages strings translations storage
+ * Manages strings translations storage
  *
  * @since 1.2
+ * @since 2.1 Stores the strings in a post meta instead of post content to avoid unserialize issues (See #63)
  */
 class PLL_MO extends MO {
 
 	/**
-	 * registers the polylang_mo custom post type, only at first object creation
+	 * Registers the polylang_mo custom post type, only at first object creation
 	 *
 	 * @since 1.2
 	 */
@@ -22,45 +23,45 @@ class PLL_MO extends MO {
 	}
 
 	/**
-	 * writes a PLL_MO object into a custom post
+	 * Writes a PLL_MO object into a custom post meta
 	 *
 	 * @since 1.2
 	 *
-	 * @param object $lang the language in which we want to export strings
+	 * @param object $lang The language in which we want to export strings
 	 */
 	public function export_to_db( $lang ) {
-		$this->add_entry( $this->make_entry( '', '' ) ); // empty string translation, just in case
+		$this->add_entry( $this->make_entry( '', '' ) ); // Empty string translation, just in case
 
-		// would be convenient to store the whole object but it would take a huge space in DB
-		// so let's keep only the strings in an array
+		// Would be convenient to store the whole object but it would take a huge space in DB
+		// So let's keep only the strings in an array
 		$strings = array();
 		foreach ( $this->entries as $entry ) {
 			$strings[] = array( $entry->singular, $this->translate( $entry->singular ) );
 		}
 
-		// we need to make sure that $post is empty when $lang->mo_id is empty: see https://wordpress.org/support/topic/problem-when-adding-a-language
-		$post = empty( $lang->mo_id ) ? array() : get_post( $lang->mo_id, ARRAY_A ); // wp_insert_post wants an array
-
-		$post['post_title'] = 'polylang_mo_' . $lang->term_id;
-		// json_encode would take less space but is slower to decode
-		// wp_insert_post expects slashed data
-		$post['post_content'] = addslashes( serialize( $strings ) );
-		$post['post_status'] = 'private'; // to avoid a conflict with WP Super Cache. See https://wordpress.org/support/topic/polylang_mo-and-404s-take-2
-		$post['post_type'] = 'polylang_mo';
-		wp_insert_post( $post );
+		if ( empty( $lang->mo_id ) ) {
+			$post = array(
+				'post_title'  => 'polylang_mo_' . $lang->term_id,
+				'post_status' => 'private', // To avoid a conflict with WP Super Cache. See https://wordpress.org/support/topic/polylang_mo-and-404s-take-2
+				'post_type'   => 'polylang_mo',
+			);
+			$mo_id = wp_insert_post( $post );
+			update_post_meta( $mo_id, '_pll_strings_translations', $strings );
+		} else {
+			update_post_meta( $lang->mo_id, '_pll_strings_translations', $strings );
+		}
 	}
 
 	/**
-	 * reads a PLL_MO object from a custom post
+	 * Reads a PLL_MO object from a custom post meta
 	 *
 	 * @since 1.2
 	 *
-	 * @param object $lang the language in which we want to get strings
+	 * @param object $lang The language in which we want to get strings
 	 */
 	public function import_from_db( $lang ) {
 		if ( ! empty( $lang->mo_id ) ) {
-			$post = get_post( $lang->mo_id, OBJECT );
-			$strings = unserialize( $post->post_content );
+			$strings = get_post_meta( $lang->mo_id, '_pll_strings_translations', true );
 			if ( is_array( $strings ) ) {
 				foreach ( $strings as $msg ) {
 					$this->add_entry( $this->make_entry( $msg[0], $msg[1] ) );
@@ -70,7 +71,7 @@ class PLL_MO extends MO {
 	}
 
 	/**
-	 * returns the post id of the post storing the strings translations
+	 * Returns the post id of the post storing the strings translations
 	 *
 	 * @since 1.4
 	 *
