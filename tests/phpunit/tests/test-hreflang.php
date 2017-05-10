df2ce298aa016721b497e8d4c4a533a591c5ed5d
Frédéric Demarle <frederic.demarle@yahoo.fr>
2017-05-10 19:45:40 +0200

    Test no hreflang on paginated post and paged archives

--- tests/phpunit/tests/test-hreflang.php
+++ tests/phpunit/tests/test-hreflang.php
@@ -65,4 +65,63 @@ class Hreflang_Test extends PLL_UnitTestCase {
 		$this->assertNotFalse( strpos( $out, 'hreflang="fr"' ) );
 		$this->assertNotFalse( strpos( $out, 'x-default' ) );
 	}
+
+	function test_paginated_post() {
+		$uk = $this->factory->post->create( array( 'post_content' => 'en1<!--nextpage-->en2' ) );
+		self::$polylang->model->post->set_language( $uk, 'uk' );
+
+		$us = $this->factory->post->create( array( 'post_content' => 'en1<!--nextpage-->en2' ) );
+		self::$polylang->model->post->set_language( $us, 'us' );
+
+		self::$polylang->model->post->save_translations( $uk, compact( 'uk', 'us' ) );
+
+		// Page 1
+		$this->go_to( get_permalink( $uk ) );
+
+		ob_start();
+		self::$polylang->filters_links->wp_head();
+		$out = ob_get_clean();
+
+		$this->assertNotFalse( strpos( $out, 'hreflang="en-GB"' ) );
+
+		// Page 2
+		$this->go_to( add_query_arg( 'page', 2, get_permalink( $uk ) ) );
+
+		ob_start();
+		self::$polylang->filters_links->wp_head();
+		$out = ob_get_clean();
+
+		$this->assertEmpty( $out );
+	}
+
+	function test_paged_archive() {
+		update_option( 'posts_per_page', 2 ); // to avoid creating too much posts
+
+		$posts_us = $this->factory->post->create_many( 3 );
+		$posts_uk = $this->factory->post->create_many( 3 );
+
+		for( $i = 0; $i < 3; $i++ ) {
+			self::$polylang->model->post->set_language( $us = $posts_us[ $i ], 'us' );
+			self::$polylang->model->post->set_language( $uk = $posts_uk[ $i ], 'uk' );
+			self::$polylang->model->post->save_translations( $uk, compact( 'uk', 'us' ) );
+		}
+
+		// Page 1
+		$this->go_to( home_url( '?lang=us' ) );
+
+		ob_start();
+		self::$polylang->filters_links->wp_head();
+		$out = ob_get_clean();
+
+		$this->assertNotFalse( strpos( $out, 'hreflang="en-GB"' ) );
+
+		// Page 2
+		$this->go_to( home_url( '?lang=us&paged=2' ) );
+
+		ob_start();
+		self::$polylang->filters_links->wp_head();
+		$out = ob_get_clean();
+
+		$this->assertEmpty( $out );
+	}
 }
