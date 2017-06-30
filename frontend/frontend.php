bcae5832e7251e1df41ecf61c47b68c70827276c
Frédéric Demarle <frederic.demarle@yahoo.fr>
2017-06-30 08:56:35 +0200

    Fix fatal error on multisite when the front page displays posts
    Introduced by 19784c8ca4b27bcc474d22942fd1317b7512db29

--- frontend/frontend.php
+++ frontend/frontend.php
@@ -169,7 +169,11 @@ class PLL_Frontend extends PLL_Base {
 
 			$lang = $this->model->get_language( $restore_curlang );
 			$this->curlang = $lang ? $lang : $this->model->get_language( $this->options['default_lang'] );
-			$this->static_pages->init();
+
+			if ( isset( $this->static_pages ) ) {
+				$this->static_pages->init();
+			}
+
 			$this->load_strings_translations();
 		}
 	}
