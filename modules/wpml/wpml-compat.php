bb2d801ffba8f3b70aae73eba5ef360db8f5f896
Frédéric Demarle <frederic.demarle@yahoo.fr>
2017-07-20 20:34:03 +0200

    Fix WPML constants not defined on backend
    Occurs if the user locale does not match a Polylang language.
    Fix #151

--- modules/wpml/wpml-compat.php
+++ modules/wpml/wpml-compat.php
@@ -25,6 +25,7 @@ class PLL_WPML_Compat {
 		self::$strings = get_option( 'polylang_wpml_strings', array() );
 
 		add_action( 'pll_language_defined', array( $this, 'define_constants' ) );
+		add_action( 'pll_no_language_defined', array( $this, 'define_constants' ) );
 		add_filter( 'pll_get_strings', array( $this, 'get_strings' ) );
 	}
 
