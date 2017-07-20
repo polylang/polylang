polylang/polylang

commit b078305fd57c1ec3d440ed7dede340fc3aeebaad

Support a different content text direction in admin
Fix #45

js/post.js
@@ -161,9 +161,13 @@ jQuery( document ).ready(function( $ ) {
 
 	// ajax for changing the post's language in the languages metabox
 	$( '.post_lang_choice' ).change(function() {
+		var value = $( this ).val();
+		var lang  = $( this ).children( 'option[value="' + value + '"]' ).attr( 'lang' );
+		var dir   = $( '.pll-translation-column > span[lang="'+ lang + '"]' ).attr( 'dir' );
+
 		var data = {
 			action:     'post_lang_choice',
-			lang:       $( this ).val(),
+			lang:       value,
 			post_type:  $( '#post_type' ).val(),
 			taxonomies: taxonomies,
 			post_id:    $( '#post_ID' ).val(),
@@ -205,6 +209,11 @@ jQuery( document ).ready(function( $ ) {
 				var id = $( this ).attr( 'id' );
 				tagBox.get( id );
 			});
+
+			// Modifies the text direction
+			$( 'body' ).removeClass( 'pll-dir-rtl' ).removeClass( 'pll-dir-ltr' ).addClass( 'pll-dir-' + dir );
+			$( '#content_ifr' ).contents().find( 'html' ).attr( 'lang', lang ).attr( 'dir', dir );
+			$( '#content_ifr' ).contents().find( 'body' ).attr( 'dir', dir );
 		});
 	});
 

js/term.js
@@ -126,9 +126,13 @@ jQuery( document ).ready(function( $ ) {
 
 	// ajax for changing the term's language
 	$( '#term_lang_choice' ).change(function() {
+		var value = $( this ).val();
+		var lang  = $( this ).children( 'option[value="' + value + '"]' ).attr( 'lang' );
+		var dir   = $( '.pll-translation-column > span[lang="'+ lang + '"]' ).attr( 'dir' );
+
 		var data = {
 			action:     'term_lang_choice',
-			lang:       $( this ).val(),
+			lang:       value,
 			from_tag:   $( "input[name='from_tag']" ).val(),
 			term_id:    $( "input[name='tag_ID']" ).val(),
 			taxonomy:   $( "input[name='taxonomy']" ).val(),
@@ -155,6 +159,9 @@ jQuery( document ).ready(function( $ ) {
 					break;
 				}
 			});
+
+			// Modifies the text direction
+			$( 'body' ).removeClass( 'pll-dir-rtl' ).removeClass( 'pll-dir-ltr' ).addClass( 'pll-dir-' + dir );
 		});
 	});
 });

admin/admin-filters-post.php
@@ -42,6 +42,9 @@ class PLL_Admin_Filters_Post extends PLL_Admin_Filters_Post_Base {
 
 		// Filters the pages by language in the parent dropdown list in the page attributes metabox
 		add_filter( 'page_attributes_dropdown_pages_args', array( $this, 'page_attributes_dropdown_pages_args' ), 10, 2 );
+
+		// Sets the language in Tiny MCE
+		add_filter( 'tiny_mce_before_init', array( $this, 'tiny_mce_before_init' ) );
 	}
 
 	/**
@@ -588,5 +591,21 @@ class PLL_Admin_Filters_Post extends PLL_Admin_Filters_Post_Base {
 		}
 
 		return $dropdown_args;
+	}
+
+	/**
+	 * Sets the language attribute and text direction for Tiny MCE
+	 *
+	 * @since 2.2
+	 *
+	 * @param array $mce_init TinyMCE config
+	 * @return array
+	 */
+	public function tiny_mce_before_init( $mce_init ) {
+		if ( ! empty( $this->curlang ) ) {
+			$mce_init['wp_lang_attr'] = $this->curlang->get_locale( 'display' );
+			$mce_init['directionality'] = $this->curlang->is_rtl ? 'rtl' : 'ltr';
+		}
+		return $mce_init;
 	}
 }

admin/admin-filters.php
@@ -40,6 +40,8 @@ class PLL_Admin_Filters extends PLL_Filters {
 			add_filter( 'sanitize_title', array( $this, 'sanitize_title' ), 10, 3 );
 			add_filter( 'sanitize_user', array( $this, 'sanitize_user' ), 10, 3 );
 		}
+
+		add_filter( 'admin_body_class', array( $this, 'admin_body_class' ) );
 	}
 
 	/**
@@ -254,5 +256,20 @@ class PLL_Admin_Filters extends PLL_Filters {
 			$once = false;
 		}
 		return $username;
+	}
+
+	/**
+	 * Adds a text direction dependent class to the body
+	 *
+	 * @since 2.2
+	 *
+	 * @param string $classes Space-separated list of CSS classes.
+	 * @return string
+	 */
+	public function admin_body_class( $classes ) {
+		if ( ! empty( $this->curlang ) ) {
+			$classes .= ' pll-dir-' . ( $this->curlang->is_rtl ? 'rtl' : 'ltr' );
+		}
+		return $classes;
 	}
 }

admin/view-translations-post.php
@@ -47,13 +47,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 				printf( '
 					<label class="screen-reader-text" for="tr_lang_%1$s">%2$s</label>
 					<input type="hidden" name="post_tr_lang[%1$s]" id="htr_lang_%1$s" value="%3$s" />
-					<input type="text" class="tr_lang" id="tr_lang_%1$s" value="%4$s"%5$s />',
+					<span lang="%6$s" dir="%7$s"><input type="text" class="tr_lang" id="tr_lang_%1$s" value="%4$s"%5$s /></span>',
 					esc_attr( $language->slug ),
 					/* translators: accessibility text */
 					esc_html__( 'Translation', 'polylang' ),
 					empty( $value ) ? 0 : esc_attr( $selected->ID ),
 					empty( $value ) ? '' : esc_attr( $selected->post_title ),
-					empty( $link ) ? ' disabled="disabled"' : ''
+					empty( $link ) ? ' disabled="disabled"' : '',
+					esc_attr( $language->get_locale( 'display' ) ),
+					$language->is_rtl ? 'rtl' : 'ltr'
 				); ?>
 			</td>
 		</tr><?php

admin/view-translations-term.php
@@ -58,13 +58,15 @@ else {
 				printf( '
 					<label class="screen-reader-text" for="tr_lang_%1$s">%2$s</label>
 					<input type="hidden" class="htr_lang" name="term_tr_lang[%1$s]" id="htr_lang_%1$s" value="%3$s" />
-					<input type="text" class="tr_lang" id="tr_lang_%1$s" value="%4$s"%5$s />',
+					<span lang="%6$s" dir="%7$s"><input type="text" class="tr_lang" id="tr_lang_%1$s" value="%4$s"%5$s /></span>',
 					esc_attr( $language->slug ),
 					/* translators: accessibility text */
 					esc_html__( 'Translation', 'polylang' ),
 					empty( $translation ) ? 0 : esc_attr( $translation->term_id ),
 					empty( $translation ) ? '' : esc_attr( $translation->name ),
-					empty( $disabled ) ? '' : ' disabled="disabled"'
+					empty( $disabled ) ? '' : ' disabled="disabled"',
+					esc_attr( $language->get_locale( 'display' ) ),
+					$language->is_rtl ? 'rtl' : 'ltr'
 				); ?>
 			</td>
 		</tr><?php

css/admin.css
@@ -148,6 +148,21 @@ td[class*='column-language_'] {
 	width: 1.5em;
 }
 
+/* Text direcction in post.php and edit-tags.php */
+.pll-dir-rtl textarea,
+.pll-dir-rtl input {
+	direction: rtl;
+}
+
+.pll-dir-ltr textarea,
+.pll-dir-ltr input {
+	direction: ltr;
+}
+
+.pll-dir-ltr .tr_lang,
+.pll-dir-rtl .tr_lang {
+	direction: inherit;
+}
 
 /* languages metabox in post.php */
 #post-translations p {

css/admin.min.css
@@ -1,1 +1,1 @@
-#pll-licenses-table td,.translation label{vertical-align:top}#add-lang select{width:95%}.column-locale,.languages .column-slug{width:15%}.column-default_lang{width:5%}.column-count,.column-flag,.column-term_group{width:10%}.icon-default-lang:before{font-family:dashicons;content:"\f155"}.form-field input[type=radio]{width:auto;margin-right:2px}#pll-about-box p,#pll-recommended p{text-align:justify}#pll-about-box input{margin:0;padding:0;float:right}.stringstranslations .column-context,.stringstranslations .column-name{width:10%}.stringstranslations .column-string{width:33%}.translation label{display:inline-block;width:23%}.translation input,.translation textarea{width:72%}.pll-settings{margin-top:20px}.pll-settings .plugin-title{width:25%}#wpbody-content .pll-settings .pll-configure tr{display:table-row}#wpbody-content .pll-settings .pll-configure td{display:table-cell}#wpbody-content .pll-settings .pll-configure>td{padding:20px 20px 20px 40px}.pll-configure legend{font-size:14px;font-weight:600;margin-bottom:.5em}.pll-configure td .description{margin-top:2px;margin-bottom:.5em}.pll-configure p.submit{margin-top:20px}.pll-configure .button{margin-right:20px}.pll-configure fieldset{margin-bottom:1.5em}.pll-inline-block-list{margin:0}.pll-inline-block-list li{display:inline-block;margin:0;width:250px}#pll-domains-table td{padding:2px 2px 2px 1.5em;-webkit-box-shadow:none;box-shadow:none;border:none}.pll-settings-url-col{display:inline-block;width:49%;vertical-align:top}#pll-licenses-table label{font-size:1em;font-weight:600}.pll-configure .pll-deactivate-license{margin:0 0 0 20px}td[class*=column-language_],th[class*=column-language_]{width:1.5em}#post-translations p{float:left}#post-translations table{table-layout:fixed;width:100%;clear:both}#post-translations a{text-decoration:none}#post-translations .pll-column-icon,#post-translations .pll-language-column{width:20px}#post-translations .tr_lang{width:100%}#post-translations td{padding:2px}#post-translations .spinner,#term-translations .spinner{float:none;margin:0;background-position:center;width:auto}.pll-column-icon{text-align:center}#select-post-language .pll-select-flag{padding:4px;margin-right:32px}#select-media-language .pll-select-flag{padding:4px;margin-right:10px}.pll-media-edit-column{float:right}.pll-translation-flag{margin-right:14px}#select-add-term-language .pll-select-flag{padding:11px;margin-right:13px}#select-edit-term-language .pll-select-flag{padding:11px;margin-right:4px}#term-translations p{font-size:12px;font-style:normal;padding:2px;color:#23282d}#add-term-translations,#edit-term-translations{width:95%}#term-translations .pll-language-column{line-height:28px;width:20%}#add-term-translations .pll-language-column,#term-translations .pll-edit-column{width:20px}#edit-term-translations .pll-language-column{padding:15px 10px;font-weight:400}.pll_icon_tick:before{content:"\f147"}.pll_icon_add:before{content:"\f132"}.pll_icon_edit:before{content:"\f464"}[class^=pll_icon_]{font:20px/1 dashicons;vertical-align:middle}#wpadminbar #wp-admin-bar-languages .ab-item img{margin:0 8px 0 2px}#wpadminbar #wp-admin-bar-languages #wp-admin-bar-all .ab-item .ab-icon{float:none;top:4px}#wpadminbar #wp-admin-bar-languages .ab-icon:before{content:"\f326";top:1px}@media screen and (max-width:782px){#wpbody-content .pll-settings .pll-configure>td{padding:20px}#wpbody-content .pll-settings #cb{padding:20px 9px}.pll-inline-block{width:auto}.pll-settings-url-col{display:block;width:100%}#wpbody-content .pll-settings #pll-licenses-table td{display:block}.pll-configure .pll-deactivate-license{margin:10px 0 5px}.stringstranslations .column-context,.stringstranslations .column-name{display:none}.translation label{display:block;width:95%;padding-left:0}.translation input{width:95%}#edit-term-translations .pll-language-name,#select-add-term-language .pll-select-flag,#select-edit-term-language .pll-select-flag{display:none}#edit-term-translations{width:100%}#add-term-translations .pll-language-column{line-height:38px}#edit-term-translations td{padding:8px 10px}#edit-term-translations .pll-edit-column,#edit-term-translations .pll-language-column{width:20px}.term-translations .pll-edit-column,.term-translations .pll-language-column,.term-translations .pll-translation-column{display:table-cell}.term-translations .hidden{display:none}#wpadminbar #wp-admin-bar-languages{display:block}#wpadminbar #wp-admin-bar-languages>.ab-item{width:50px;text-align:center}#wpadminbar #wp-admin-bar-languages>.ab-item .ab-icon:before{font:32px/1 dashicons;top:-1px}#wpadminbar #wp-admin-bar-languages>.ab-item img{margin:19px 0}#wpadminbar #wp-admin-bar-languages #wp-admin-bar-all .ab-item .ab-icon{margin-right:6px;font-size:20px!important;line-height:20px!important}}
+#pll-licenses-table td,.translation label{vertical-align:top}#add-lang select{width:95%}.column-locale,.languages .column-slug{width:15%}.column-default_lang{width:5%}.column-count,.column-flag,.column-term_group{width:10%}.icon-default-lang:before{font-family:dashicons;content:"\f155"}.form-field input[type=radio]{width:auto;margin-right:2px}#pll-about-box p,#pll-recommended p{text-align:justify}#pll-about-box input{margin:0;padding:0;float:right}.stringstranslations .column-context,.stringstranslations .column-name{width:10%}.stringstranslations .column-string{width:33%}.translation label{display:inline-block;width:23%}.translation input,.translation textarea{width:72%}.pll-settings{margin-top:20px}.pll-settings .plugin-title{width:25%}#wpbody-content .pll-settings .pll-configure tr{display:table-row}#wpbody-content .pll-settings .pll-configure td{display:table-cell}#wpbody-content .pll-settings .pll-configure>td{padding:20px 20px 20px 40px}.pll-configure legend{font-size:14px;font-weight:600;margin-bottom:.5em}.pll-configure td .description{margin-top:2px;margin-bottom:.5em}.pll-configure p.submit{margin-top:20px}.pll-configure .button{margin-right:20px}.pll-configure fieldset{margin-bottom:1.5em}.pll-inline-block-list{margin:0}.pll-inline-block-list li{display:inline-block;margin:0;width:250px}#pll-domains-table td{padding:2px 2px 2px 1.5em;-webkit-box-shadow:none;box-shadow:none;border:none}.pll-settings-url-col{display:inline-block;width:49%;vertical-align:top}#pll-licenses-table label{font-size:1em;font-weight:600}.pll-configure .pll-deactivate-license{margin:0 0 0 20px}td[class*=column-language_],th[class*=column-language_]{width:1.5em}.pll-dir-rtl input,.pll-dir-rtl textarea{direction:rtl}.pll-dir-ltr input,.pll-dir-ltr textarea{direction:ltr}.pll-dir-ltr .tr_lang,.pll-dir-rtl .tr_lang{direction:inherit}#post-translations p{float:left}#post-translations table{table-layout:fixed;width:100%;clear:both}#post-translations a{text-decoration:none}#post-translations .pll-column-icon,#post-translations .pll-language-column{width:20px}#post-translations .tr_lang{width:100%}#post-translations td{padding:2px}#post-translations .spinner,#term-translations .spinner{float:none;margin:0;background-position:center;width:auto}.pll-column-icon{text-align:center}#select-post-language .pll-select-flag{padding:4px;margin-right:32px}#select-media-language .pll-select-flag{padding:4px;margin-right:10px}.pll-media-edit-column{float:right}.pll-translation-flag{margin-right:14px}#select-add-term-language .pll-select-flag{padding:11px;margin-right:13px}#select-edit-term-language .pll-select-flag{padding:11px;margin-right:4px}#term-translations p{font-size:12px;font-style:normal;padding:2px;color:#23282d}#add-term-translations,#edit-term-translations{width:95%}#term-translations .pll-language-column{line-height:28px;width:20%}#add-term-translations .pll-language-column,#term-translations .pll-edit-column{width:20px}#edit-term-translations .pll-language-column{padding:15px 10px;font-weight:400}.pll_icon_tick:before{content:"\f147"}.pll_icon_add:before{content:"\f132"}.pll_icon_edit:before{content:"\f464"}[class^=pll_icon_]{font:20px/1 dashicons;vertical-align:middle}#wpadminbar #wp-admin-bar-languages .ab-item img{margin:0 8px 0 2px}#wpadminbar #wp-admin-bar-languages #wp-admin-bar-all .ab-item .ab-icon{float:none;top:4px}#wpadminbar #wp-admin-bar-languages .ab-icon:before{content:"\f326";top:1px}@media screen and (max-width:782px){#wpbody-content .pll-settings .pll-configure>td{padding:20px}#wpbody-content .pll-settings #cb{padding:20px 9px}.pll-inline-block{width:auto}.pll-settings-url-col{display:block;width:100%}#wpbody-content .pll-settings #pll-licenses-table td{display:block}.pll-configure .pll-deactivate-license{margin:10px 0 5px}.stringstranslations .column-context,.stringstranslations .column-name{display:none}.translation label{display:block;width:95%;padding-left:0}.translation input{width:95%}#edit-term-translations .pll-language-name,#select-add-term-language .pll-select-flag,#select-edit-term-language .pll-select-flag{display:none}#edit-term-translations{width:100%}#add-term-translations .pll-language-column{line-height:38px}#edit-term-translations td{padding:8px 10px}#edit-term-translations .pll-edit-column,#edit-term-translations .pll-language-column{width:20px}.term-translations .pll-edit-column,.term-translations .pll-language-column,.term-translations .pll-translation-column{display:table-cell}.term-translations .hidden{display:none}#wpadminbar #wp-admin-bar-languages{display:block}#wpadminbar #wp-admin-bar-languages>.ab-item{width:50px;text-align:center}#wpadminbar #wp-admin-bar-languages>.ab-item .ab-icon:before{font:32px/1 dashicons;top:-1px}#wpadminbar #wp-admin-bar-languages>.ab-item img{margin:19px 0}#wpadminbar #wp-admin-bar-languages #wp-admin-bar-all .ab-item .ab-icon{margin-right:6px;font-size:20px!important;line-height:20px!important}}

commit bb94767ec0cee3dd78c5adcc8bb379d9dd312e13

Fix + icon not changed into pen icon
when selecting a post or term in translation autocomplete input fields.
The bug had been introduced by b078305fd57c1ec3d440ed7dede340fc3aeebaad

js/post.js
@@ -221,7 +221,7 @@ jQuery( document ).ready(function( $ ) {
 	function init_translations() {
 		$( '.tr_lang' ).each(function(){
 			var tr_lang = $( this ).attr( 'id' ).substring( 8 );
-			var td = $( this ).parent().siblings( '.pll-edit-column' );
+			var td = $( this ).parent().parent().siblings( '.pll-edit-column' );
 
 			$( this ).autocomplete({
 				minLength: 0,

js/term.js
@@ -93,7 +93,7 @@ jQuery( document ).ready(function( $ ) {
 	function init_translations() {
 		$( '.tr_lang' ).each(function(){
 			var tr_lang = $( this ).attr( 'id' ).substring( 8 );
-			var td = $( this ).parent().siblings( '.pll-edit-column' );
+			var td = $( this ).parent().parent().siblings( '.pll-edit-column' );
 
 			$( this ).autocomplete({
 				minLength: 0,