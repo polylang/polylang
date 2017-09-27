a247344a1b3c1ba0e5b70cfe23bf547936ae8c86
Frédéric Demarle <frederic.demarle@yahoo.fr>
2017-09-27 14:30:02 +0200

    Add filter 'pll_hide_archive_translation_url'
    By default, an empty translated archive is not shown in the language
    switcher. This filter allows to show it anyway.
    Fix #174

--- frontend/frontend-links.php
+++ frontend/frontend-links.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
- * manages links filters and url of translations on frontend
+ * Manages links filters and url of translations on frontend
  *
  * @since 1.2
  */
 class PLL_Frontend_Links extends PLL_Links {
 	public $curlang;
-	public $cache; // our internal non persistent cache object
+	public $cache; // Our internal non persistent cache object
 
 	/**
-	 * constructor
+	 * Constructor
 	 *
 	 * @since 1.2
 	 *
@@ -25,7 +25,7 @@ class PLL_Frontend_Links extends PLL_Links {
 	}
 
 	/**
-	 * returns the url of the translation ( if exists ) of the current page
+	 * Returns the url of the translation ( if exists ) of the current page
 	 *
 	 * @since 0.1
 	 *
@@ -39,8 +39,8 @@ class PLL_Frontend_Links extends PLL_Links {
 			return $translation_url;
 		}
 
-		// make sure that we have the queried object
-		// see https://wordpress.org/support/topic/patch-for-fixing-a-notice
+		// Make sure that we have the queried object
+		// See https://wordpress.org/support/topic/patch-for-fixing-a-notice
 		$queried_object_id = $wp_query->get_queried_object_id();
 
 		/**
@@ -49,20 +49,20 @@ class PLL_Frontend_Links extends PLL_Links {
 		 *
 		 * @since 1.8
 		 *
-		 * @param string $url               empty or the url of the translation of teh current page
-		 * @param object $language          language of the translation
-		 * @param int    $queried_object_id queried object id
+		 * @param string $url               Empty or the url of the translation of teh current page
+		 * @param object $language          Language of the translation
+		 * @param int    $queried_object_id Queried object id
 		 */
 		if ( ! $url = apply_filters( 'pll_pre_translation_url', '', $language, $queried_object_id ) ) {
 			$qv = $wp_query->query_vars;
 			$hide = $this->options['default_lang'] == $language->slug && $this->options['hide_default'];
 
-			// post and attachment
+			// Post and attachment
 			if ( is_single() && ( $this->options['media_support'] || ! is_attachment() ) && ( $id = $this->model->post->get( $queried_object_id, $language ) ) && $this->current_user_can_read( $id ) ) {
 				$url = get_permalink( $id );
 			}
 
-			// page
+			// Page
 			elseif ( is_page() && ( $id = $this->model->post->get( $queried_object_id, $language ) ) && $this->current_user_can_read( $id ) ) {
 				$url = get_page_link( $id );
 			}
@@ -70,17 +70,17 @@ class PLL_Frontend_Links extends PLL_Links {
 			elseif ( is_search() ) {
 				$url = $this->get_archive_url( $language );
 
-				// special case for search filtered by translated taxonomies: taxonomy terms are translated in the translation url
+				// Special case for search filtered by translated taxonomies: taxonomy terms are translated in the translation url
 				if ( ! empty( $wp_query->tax_query->queries ) ) {
 					foreach ( $wp_query->tax_query->queries as $tax_query ) {
 						if ( ! empty( $tax_query['taxonomy'] ) && $this->model->is_translated_taxonomy( $tax_query['taxonomy'] ) ) {
 
 							$tax = get_taxonomy( $tax_query['taxonomy'] );
-							$terms = get_terms( $tax->name, array( 'fields' => 'id=>slug' ) ); // filtered by current language
+							$terms = get_terms( $tax->name, array( 'fields' => 'id=>slug' ) ); // Filtered by current language
 
 							foreach ( $tax_query['terms'] as $slug ) {
-								$term_id = array_search( $slug, $terms ); // what is the term_id corresponding to taxonomy term?
-								if ( $term_id && $term_id = $this->model->term->get_translation( $term_id, $language ) ) { // get the translated term_id
+								$term_id = array_search( $slug, $terms ); // What is the term_id corresponding to taxonomy term?
+								if ( $term_id && $term_id = $this->model->term->get_translation( $term_id, $language ) ) { // Get the translated term_id
 									$term = get_term( $term_id, $tax->name );
 									$url = str_replace( $slug, $term->slug, $url );
 								}
@@ -90,42 +90,64 @@ class PLL_Frontend_Links extends PLL_Links {
 				}
 			}
 
-			// translated taxonomy
-			// take care that is_tax() is false for categories and tags
+			// Translated taxonomy
+			// Take care that is_tax() is false for categories and tags
 			elseif ( ( is_category() || is_tag() || is_tax() ) && ( $term = get_queried_object() ) && $this->model->is_translated_taxonomy( $term->taxonomy ) ) {
 				$lang = $this->model->term->get_language( $term->term_id );
 
 				if ( ! $lang || $language->slug == $lang->slug ) {
-					$url = wpcom_vip_get_term_link( $term, $term->taxonomy ); // self link
+					$url = wpcom_vip_get_term_link( $term, $term->taxonomy ); // Self link
 				}
 
 				elseif ( $tr_id = $this->model->term->get_translation( $term->term_id, $language ) ) {
-					$tr_term = get_term( $tr_id, $term->taxonomy );
-					// check if translated term ( or children ) have posts
-					if ( $tr_term && ( $tr_term->count || ( is_taxonomy_hierarchical( $term->taxonomy ) && array_sum( wp_list_pluck( get_terms( $term->taxonomy, array( 'child_of' => $tr_term->term_id, 'lang' => $language->slug ) ), 'count' ) ) ) ) ) {
-						$url = wpcom_vip_get_term_link( $tr_term, $term->taxonomy );
+					if ( $tr_term = get_term( $tr_id, $term->taxonomy ) ) {
+						// Check if translated term ( or children ) have posts
+						$count = $tr_term->count || ( is_taxonomy_hierarchical( $term->taxonomy ) && array_sum( wp_list_pluck( get_terms( $term->taxonomy, array( 'child_of' => $tr_term->term_id, 'lang' => $language->slug ) ), 'count' ) ) );
+
+						/**
+						 * Filter whether to hide an archive translation url
+						 *
+						 * @since 2.2.4
+						 *
+						 * @param bool   $hide True to hide the translation url.
+						 *                     defaults to true when the translated archive is empty, false otherwise.
+						 * @param string $lang The language code of the translation
+						 * @param array  $args Arguments used to evaluated the number of posts in the archive
+						 */
+						if ( ! apply_filters( 'pll_hide_archive_translation_url', ! $count , $language->slug, array( 'taxonomy' => $term->taxonomy ) ) ) {
+							$url = wpcom_vip_get_term_link( $tr_term, $term->taxonomy );
+						}
 					}
 				}
 			}
 
-			// post type archive
+			// Post type archive
 			elseif ( is_post_type_archive() ) {
-				if ( $this->model->is_translated_post_type( $qv['post_type'] ) && $this->model->count_posts( $language, array( 'post_type' => $qv['post_type'] ) ) ) {
-					$url = $this->get_archive_url( $language );
+				if ( $this->model->is_translated_post_type( $qv['post_type'] ) ) {
+					$args = array( 'post_type' => $qv['post_type'] );
+					$count = $this->model->count_posts( $language, $args );
+
+					/** This filter is documented in frontend/frontend-links.php */
+					if ( ! apply_filters( 'pll_hide_archive_translation_url', ! $count , $language->slug, $args ) ) {
+						$url = $this->get_archive_url( $language );
+					}
 				}
 			}
 
+			// Other archives
 			elseif ( is_archive() ) {
 				$keys = array( 'post_type', 'm', 'year', 'monthnum', 'day', 'author', 'author_name' );
 				$keys = array_merge( $keys, $this->model->get_filtered_taxonomies_query_vars() );
+				$args = array_intersect_key( $qv, array_flip( $keys ) );
+				$count = $this->model->count_posts( $language, $args );
 
-				// check if there are existing translations before creating the url
-				if ( $this->model->count_posts( $language, array_intersect_key( $qv, array_flip( $keys ) ) ) ) {
+				/** This filter is documented in frontend/frontend-links.php */
+				if ( ! apply_filters( 'pll_hide_archive_translation_url', ! $count , $language->slug, $args ) ) {
 					$url = $this->get_archive_url( $language );
 				}
 			}
 
-			// front page when it is the list of posts
+			// Front page when it is the list of posts
 			elseif ( is_front_page() ) {
 				$url = $this->get_home_url( $language );
 			}
@@ -136,8 +158,8 @@ class PLL_Frontend_Links extends PLL_Links {
 		 *
 		 * @since 1.1.2
 		 *
-		 * @param null|string $url      the translation url, null if none was found
-		 * @param string      $language the language code of the translation
+		 * @param null|string $url      The translation url, null if none was found
+		 * @param string      $language The language code of the translation
 		 */
 		$translation_url = apply_filters( 'pll_translation_url', ( isset( $url ) && ! is_wp_error( $url ) ? $url : null ), $language->slug );
 		$this->cache->set( 'translation_url:' . $language->slug, $translation_url );
@@ -145,7 +167,7 @@ class PLL_Frontend_Links extends PLL_Links {
 	}
 
 	/**
-	 * get the translation of the current archive url
+	 * Get the translation of the current archive url
 	 * used also for search
 	 *
 	 * @since 1.2
@@ -163,14 +185,14 @@ class PLL_Frontend_Links extends PLL_Links {
 		 *
 		 * @since 1.6
 		 *
-		 * @param string $url      url of the archive
-		 * @param object $language language of the archive
+		 * @param string $url      Url of the archive
+		 * @param object $language Language of the archive
 		 */
 		return apply_filters( 'pll_get_archive_url', $url, $language );
 	}
 
 	/**
-	 * returns the home url in the right language
+	 * Returns the home url in the right language
 	 *
 	 * @since 0.1
 	 *
