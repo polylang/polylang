a51970112aba22b09c90d5a6ecbe51c9f2a2253b
Janne Aalto <janne.aalto@frantic.com>
2018-04-23 14:55:47 +0300

    fix incompatability with php 7.1 (PHP Fatal error: Uncaught Error: [] operator not supported for strings)

--- include/query.php
+++ include/query.php
@@ -106,9 +106,11 @@ class PLL_Query {
 				array( $tax_query ),
 				'relation' => 'AND',
 			);
-		} else {
-			$tax_query[] = $lang_query;
-		}
+		} else if ( is_array( $tax_query ) ) {
+            $tax_query[] = $lang_query;
+        } else {
+            $tax_query = array( $lang_query );
+        }
 	}
 
 	/**
