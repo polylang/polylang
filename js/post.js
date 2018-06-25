345610598077fca4db4a942694d75088e6dfcdf1
Frédéric Demarle <frederic.demarle@yahoo.fr>
2018-06-25 10:27:29 +0200

    Fix Uncaught TypeError: s.split is not a function
    This is conflict with Gutenberg due to it sometimes sendind FormData
    objects instead of JSON.
    Fix #262

--- js/post.js
+++ js/post.js
@@ -131,9 +131,11 @@
 			});
 		}
 
-		var data = wpAjax.unserialize( settings.data ); // what were the data sent by the ajax request?
-		if ( 'undefined' != typeof( data['action'] ) && 'inline-save' == data['action'] ) {
-			update_rows( data['post_ID'] );
+		if ( 'string' == typeof( settings.data ) ) { // Need to check the type due to Gutenberg sometime sending FormData objects
+			var data = wpAjax.unserialize( settings.data ); // what were the data sent by the ajax request?
+			if ( 'undefined' != typeof( data['action'] ) && 'inline-save' == data['action'] ) {
+				update_rows( data['post_ID'] );
+			}
 		}
 	});
 })( jQuery );
