57de00b36257f02fb45c0253e9bc10f33c8809a0
Frédéric Demarle <frederic.demarle@yahoo.fr>
2018-11-22 11:16:15 +0100

    Block editor: save the post and reload the page after changing the language
    Props @manooweb

--- js/block-editor.js
+++ js/block-editor.js
@@ -5,7 +5,7 @@
  */
 wp.apiFetch.use( ( options, next ) => {
 	// If options.url is defined, this is not a REST request but a direct call to post.php for legacy metaboxes.
-	if ( 'undefined' === typeof options.url ) {
+if ( 'undefined' === typeof options.url ) {
 		if ( 'undefined' === typeof options.data ) {
 			// GET
 			options.path += ( ( options.path.indexOf ( '?' ) >= 0 ) ? '&lang=' : '?lang=' ) + getCurrentLanguage();
@@ -27,3 +27,46 @@ wp.apiFetch.use( ( options, next ) => {
 function getCurrentLanguage() {
 	return document.querySelector( '[name=post_lang_choice]' ).value;
 }
+
+/**
+ * save post after lang choice is done and redirect to the same page for refreshing all the data
+ *
+ * @since 2.4
+ */
+jQuery( document ).ready(function( $ ) {
+	// savePost after changing the post's language and reload page for refreshing post translated data
+	$( '.post_lang_choice' ).change(function() {
+		const select = wp.data.select;
+		const dispatch = wp.data.dispatch;
+		const subscribe = wp.data.subscribe;
+
+		let unsubscribe = null;
+
+		// listen if the savePost is done
+		const savePostIsDone = new Promise( function( resolve, reject ) {
+			unsubscribe = subscribe( function() {
+				const isSavePostSucceeded = select('core/editor').didPostSaveRequestSucceed();
+				const isSavePostFailed = select('core/editor').didPostSaveRequestFail();
+				if ( isSavePostSucceeded || isSavePostFailed ) {
+					if ( isSavePostFailed ) {
+						reject();
+					} else {
+						resolve();
+					}
+				}
+			} );
+		});
+
+		dispatch( 'core/editor' ).savePost();
+
+		savePostIsDone
+			.then( function() {
+				// if the post is well saved, we can reload the page
+				unsubscribe();
+				window.location.reload();
+			} )
+			.catch( function() {
+				unsubscribe();
+			} );
+	} );
+} );
