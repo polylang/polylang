5c3473929f33b285c192f1f60a9b4913c74bb2da
raaaahman <sylvain@devindetails.com>
2019-02-28 17:31:22 +0100

    refactor(Polylang): autoload now loads duplicate and landing-page modules

--- include/class-polylang.php
+++ include/class-polylang.php
@@ -68,7 +68,7 @@ class Polylang {
 		}
 
 		$class = str_replace( '_', '-', strtolower( substr( $class, 4 ) ) );
-		$to_find = array( 'media', 'share', 'slug', 'slugs', 'sync', 'translate', 'wpml', 'xdata', 'rest', 'bulk' );
+		$to_find = array( 'media', 'share', 'slug', 'slugs', 'sync', 'translate', 'wpml', 'xdata', 'rest', 'bulk', 'duplicate', 'landing', 'page' );
 		$dir = implode( '-', array_intersect( explode( '-', $class ), $to_find ) );
 
 		$dirs = array(
