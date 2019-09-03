37ec5b00919bd2456f223e0a699a31bc9ee4342e
Frédéric Demarle <frederic.demarle@yahoo.fr>
2019-09-03 14:51:44 +0200

    Fix add_filter() used instead of add_action()

--- admin/admin-base.php
+++ admin/admin-base.php
@@ -59,7 +59,7 @@ class PLL_Admin_Base extends PLL_Base {
 
 		// Filter admin language for users
 		// We must not call user info before WordPress defines user roles in wp-settings.php
-		add_filter( 'setup_theme', array( $this, 'init_user' ) );
+		add_action( 'setup_theme', array( $this, 'init_user' ) );
 		add_filter( 'request', array( $this, 'request' ) );
 
 		// Adds the languages in admin bar
