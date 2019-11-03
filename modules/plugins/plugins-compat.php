16a9835adcbaf9fb7d7c8ce9891d792dadf591e1
Frédéric Demarle <frederic.demarle@yahoo.fr>
2019-11-03 15:02:48 +0100

    Twenty Seventeen: Translate header video Youtube url

--- modules/plugins/plugins-compat.php
+++ modules/plugins/plugins-compat.php
@@ -266,10 +266,18 @@ class PLL_Plugins_Compat {
 	 * @since 2.0.10
 	 */
 	public function twenty_seventeen_init() {
-		if ( 'twentyseventeen' === get_template() && function_exists( 'twentyseventeen_panel_count' ) && did_action( 'pll_init' ) && PLL() instanceof PLL_Frontend ) {
-			$num_sections = twentyseventeen_panel_count();
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-				add_filter( 'theme_mod_panel_' . $i, 'pll_get_post' );
+		if ( 'twentyseventeen' === get_template() && did_action( 'pll_init' ) ) {
+			if ( function_exists( 'twentyseventeen_panel_count' ) && PLL() instanceof PLL_Frontend ) {
+				$num_sections = twentyseventeen_panel_count();
+				for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
+					add_filter( 'theme_mod_panel_' . $i, 'pll_get_post' );
+				}
+			}
+
+			if ( PLL() instanceof PLL_Frontend ) {
+				add_filter( 'theme_mod_external_header_video', 'pll__' );
+			} else {
+				pll_register_string( __( 'Header video', 'polylang' ), get_theme_mod( 'external_header_video' ), 'Twenty Seventeen', false );
 			}
 		}
 	}
