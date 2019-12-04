655d3d11bba59d6ba2427e14e51da00e5e389b94
Frédéric Demarle <frederic.demarle@yahoo.fr>
2019-12-04 15:37:05 +0100

    Remove Test for Yoast SEO sitemaps for subdomains
    as we are skipping the test for more than 2 years now and See
    https://github.com/Yoast/wordpress-seo/issues/6926 has been closed
    without fixing the issue.

--- tests/phpunit/tests/plugins/test-wpseo.php
+++ tests/phpunit/tests/plugins/test-wpseo.php
@@ -151,68 +151,6 @@ if ( file_exists( DIR_TESTROOT . '/../wordpress-seo/wp-seo.php' ) ) {
 			$this->assertNotFalse( strpos( $output, "<loc>$en</loc>" ) );
 			$this->assertNotFalse( strpos( $output, "<loc>$fr</loc>" ) );
 		}
-
-		function test_post_sitemap_for_subdomains() {
-			// FIXME The test works alone but static vars in Yoast SEO make it conclict with test_post_sitemap_for_code_in_url()
-			// See https://github.com/Yoast/wordpress-seo/issues/6926
-			$this->markTestSkipped();
-
-			global $wp_rewrite;
-
-			// setup subdomains
-			$this->hosts = array(
-				'en' => 'http://example.org',
-				'fr' => 'http://fr.example.org',
-			);
-
-			self::$polylang->options['hide_default'] = 1;
-			self::$polylang->options['force_lang'] = 2;
-
-			// switch to pretty permalinks
-			$wp_rewrite->init();
-			$wp_rewrite->set_permalink_structure( $this->structure );
-
-			self::$polylang->model->post->register_taxonomy(); // needs this for 'lang' query var
-			create_initial_taxonomies();
-			self::$polylang->links_model = self::$polylang->model->get_links_model();
-
-			$wp_rewrite->flush_rules();
-
-			// init frontend
-			self::$polylang = new PLL_Frontend( self::$polylang->links_model );
-			self::$polylang->init();
-			self::$polylang->curlang = self::$polylang->model->get_language( 'fr' );
-			do_action( 'pll_language_defined' );
-
-			// de-activate cache for links
-			self::$polylang->links->cache = $this->getMockBuilder( 'PLL_Cache' )->getMock();
-			self::$polylang->links->cache->method( 'get' )->willReturn( false );
-			self::$polylang->filters_links->cache = $this->getMockBuilder( 'PLL_Cache' )->getMock();
-			self::$polylang->filters_links->cache->method( 'get' )->willReturn( false );
-
-			// create posts
-			$en = $this->factory->post->create();
-			self::$polylang->model->post->set_language( $en, 'en' );
-
-			$fr = $this->factory->post->create();
-			self::$polylang->model->post->set_language( $fr, 'fr' );
-
-			$this->go_to( $this->hosts['fr'] . '/post-sitemap.xml' );
-			$sm = new WPSEO_Sitemaps_Double();
-			set_query_var( 'sitemap', 'post' );
-
-			ob_start();
-			$sm->redirect( $GLOBALS['wp_the_query'] );
-			$output = ob_get_clean();
-
-			$en = htmlspecialchars( get_permalink( $en ) ); // WPSEO uses htmlspecialchars
-			$fr = htmlspecialchars( get_permalink( $fr ) );
-
-			// the sitemap must contain only one language
-			$this->assertNotFalse( strpos( $output, '<loc>' . $this->hosts['fr'] . '</loc>' ) );
-			$this->assertNotFalse( strpos( $output, "<loc>$fr</loc>" ) );
-			$this->assertFalse( strpos( $output, "<loc>$en</loc>" ) );
-		}
 	}
 
 } // file_exists
