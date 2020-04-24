343e92a180b06aa768333a27f8715797b58c7680
Frédéric Demarle <frederic.demarle@yahoo.fr>
2020-04-24 15:31:53 +0200

    Fix PHP notice in plugin updater

--- install/plugin-updater.php
+++ install/plugin-updater.php
@@ -135,7 +135,7 @@ class PLL_Plugin_Updater {
 				$no_update->plugin      = $this->name;
 				$no_update->new_version = $version_info->new_version;
 				$no_update->url         = $version_info->homepage;
-				$no_update->package     = $version_info->package;
+				$no_update->package     = isset( $version_info->package ) ? $version_info->package : ''; #modified#
 				$no_update->icons       = $version_info->icons;
 				$no_update->banners     = $version_info->banners;
 				$no_update->banners_rtl = array();
@@ -230,7 +230,7 @@ class PLL_Plugin_Updater {
 				$no_update->plugin      = $this->name;
 				$no_update->new_version = $version_info->new_version;
 				$no_update->url         = $version_info->homepage;
-				$no_update->package     = $version_info->package;
+				$no_update->package     = isset( $version_info->package ) ? $version_info->package : ''; #modified#
 				$no_update->icons       = $version_info->icons;
 				$no_update->banners     = $version_info->banners;
 				$no_update->banners_rtl = array();
