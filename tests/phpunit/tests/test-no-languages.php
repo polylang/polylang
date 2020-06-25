ba99777a8a12a8ebc6891e08934a4e4720d1b232
Emmanuel Hesry <ehesry@gmail.com>
2020-06-25 17:10:25 +0200

    We can now test switcher result in an admin context when there is no languages defined

--- tests/phpunit/tests/test-no-languages.php
+++ tests/phpunit/tests/test-no-languages.php
@@ -5,9 +5,10 @@ class No_Languages_Test extends PLL_UnitTestCase {
 	// bug fixed in 1.8.2
 	function test_api_on_admin() {
 		require_once POLYLANG_DIR . '/include/api.php'; // usually loaded only if an instance of Polylang exists
+		self::$polylang->links = new PLL_Admin_Links( self::$polylang );
 		$GLOBALS['polylang'] = self::$polylang;
 
-		// FIXME can't test pll_the_languages due to the constant PLL_ADMIN
+		$this->assertEmpty( pll_the_languages() );
 		$this->assertFalse( pll_current_language() );
 		$this->assertFalse( pll_default_language() ); // the bug
 		$this->assertEquals( home_url( '/' ), pll_home_url() );
