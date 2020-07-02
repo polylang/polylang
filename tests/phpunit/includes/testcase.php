867f43fddd0f3b5dd4607b10ef0fdaccd4d11313
raaaahman <sylvain@devindetails.com>
2020-07-02 16:18:57 +0200

    test(includes): Add a method to easily generate Polylang environments

--- tests/phpunit/includes/testcase.php
+++ tests/phpunit/includes/testcase.php
@@ -12,4 +12,23 @@ class PLL_UnitTestCase extends WP_UnitTestCase {
 		add_filter( 'wp_using_themes', '__return_true' ); // To pass the test in PLL_Choose_Lang::init() by default.
 		add_filter( 'wp_doing_ajax', '__return_false' );
 	}
+
+	/**
+	 * Creates a Polylang environment to recreate an administration dashboard context.
+	 *
+	 * @since 2.8
+	 *
+	 * @param $options_overrides
+	 * @return array
+	 */
+	protected function admin_setup($options_overrides)
+	{
+		$options = array_merge(PLL_Install::get_default_options(), $options_overrides);
+		$model = new PLL_Admin_Model($options);
+		$links_model = new PLL_Links_Default($model);
+		$polylang = new PLL_Admin($links_model);
+		$polylang->init();
+		do_action('wp_loaded');
+		return $polylang;
+	}
 }
