be03ce1b723c8f5e7e47d3fd64c28deb66be09a3
Emmanuel Hesry <ehesry@gmail.com>
2020-08-14 12:41:24 +0200

    Improve $steps property documentation

--- modules/wizard/wizard.php
+++ modules/wizard/wizard.php
@@ -26,7 +26,14 @@ class PLL_Wizard {
 	/**
 	 * List of steps
 	 *
-	 * @var array $steps
+	 * @var array $steps {
+	 *     @type string $name      i18n string which names the step.
+	 *     @type callable $view    The callback function use to display the step content.
+	 *     @type callable $handler The callback function use to process the step after it is submitted.
+	 *     @type array $scripts    List of scripts handle needed by the step.
+	 *     @type array $styles     The list of styles handle needed by the step.
+	 *
+	 * }
 	 */
 	protected $steps = array();
 
