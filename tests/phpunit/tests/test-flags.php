54c029ba6e83185c0acd5bc20786c3da304356b9
Chouby <frederic.demarle@yahoo.fr>
2020-08-24 15:00:54 +0200

    Fix get_display_flag_url() returning html instead of url (#558)
    Add a few tests for the 'raw' option of pll_the_languages()
    Review existing and add tests for flags.

--- tests/phpunit/tests/test-flags.php
+++ tests/phpunit/tests/test-flags.php
@@ -1,6 +1,6 @@
 <?php
 
-class Custom_Data_Test extends PLL_UnitTestCase {
+class Flags_Test extends PLL_UnitTestCase {
 
 	static function wpSetUpBeforeClass() {
 		parent::wpSetUpBeforeClass();
@@ -23,6 +23,12 @@ class Custom_Data_Test extends PLL_UnitTestCase {
 		self::$polylang = new PLL_Frontend( self::$polylang->links_model );
 	}
 
+	function test_default_flag() {
+		$lang = self::$polylang->model->get_language( 'en' );
+		$this->assertEquals( plugins_url( '/flags/us.png', POLYLANG_FILE ), $lang->get_display_flag_url() ); // Bug fixed in 2.8.1.
+		$this->assertEquals( 1, preg_match( '#<img src="data:image\/png;base64,(.+)" title="English" alt="English" width="16" height="11" style="(.+)" \/>#', $lang->get_display_flag() ) );
+	}
+
 	function test_custom_flag() {
 		$lang = self::$polylang->model->get_language( 'fr' );
 		$this->assertEquals( content_url( '/polylang/fr_FR.png' ), $lang->get_display_flag_url() );
@@ -32,18 +38,22 @@ class Custom_Data_Test extends PLL_UnitTestCase {
 	/*
 	 * bug fixed in 1.8
 	 */
-	function test_ssl_custom_flag() {
+	function test_default_flag_ssl() {
 		$_SERVER['HTTPS'] = 'on';
 
-		// test ssl also for default flags
 		$lang = self::$polylang->model->get_language( 'en' );
-		$this->assertContains( 'https', $lang->flag_url );
-
-		// custom flags
-		$lang = self::$polylang->model->get_language( 'fr' );
-		$this->assertEquals( content_url( '/polylang/fr_FR.png' ), $lang->get_display_flag_url() );
 		$this->assertContains( 'https', $lang->get_display_flag_url() );
+
 		unset( $_SERVER['HTTPS'] );
 	}
 
+	function test_custom_flag_ssl() {
+		$_SERVER['HTTPS'] = 'on';
+
+		$lang = self::$polylang->model->get_language( 'fr' );
+		$this->assertEquals( content_url( '/polylang/fr_FR.png' ), $lang->get_display_flag_url() );
+		$this->assertContains( 'https', $lang->get_display_flag_url() );
+
+		unset( $_SERVER['HTTPS'] );
+	}
 }
