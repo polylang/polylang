fe013e8b642bc55b762da50ab89859cb219df21f
Frédéric Demarle <frederic.demarle@yahoo.fr>
2020-08-24 18:10:10 +0200

    Delete the languages transient when updating to 2.8.1

--- install/upgrade.php
+++ install/upgrade.php
@@ -92,7 +92,7 @@ class PLL_Upgrade {
 	 * @since 1.2
 	 */
 	public function _upgrade() {
-		foreach ( array( '0.9', '1.0', '1.1', '1.2', '1.2.1', '1.2.3', '1.3', '1.4', '1.4.1', '1.4.4', '1.5', '1.6', '1.7.4', '1.8', '2.0.8', '2.1', '2.7', '2.8' ) as $version ) {
+		foreach ( array( '0.9', '1.0', '1.1', '1.2', '1.2.1', '1.2.3', '1.3', '1.4', '1.4.1', '1.4.4', '1.5', '1.6', '1.7.4', '1.8', '2.0.8', '2.1', '2.7', '2.8.1' ) as $version ) {
 			if ( version_compare( $this->options['version'], $version, '<' ) ) {
 				call_user_func( array( $this, 'upgrade_' . str_replace( '.', '_', $version ) ) );
 			}
@@ -635,16 +635,17 @@ class PLL_Upgrade {
 	}
 
 	/**
-	 * Upgrades if the previous version is < 2.8
+	 * Upgrades if the previous version is < 2.8.1
 	 *
 	 * Deletes language cache due to:
 	 * - 'redirect_lang' option removed for subdomains and multiple domains in 2.2
 	 * - W3C and Facebook locales added to PLL_Language objects in 2.3
 	 * - flags moved to a different directory in Polylang Pro 2.8
+	 * - bug of flags url returning html fixed in 2.8.1
 	 *
-	 * @since 2.8
+	 * @since 2.8.1
 	 */
-	protected function upgrade_2_8() {
+	protected function upgrade_2_8_1() {
 		delete_transient( 'pll_languages_list' );
 	}
 }
