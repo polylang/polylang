660d61db5f708c0802e35e38d57e1657966e070d
raaaahman <sylvain@devindetails.com>
2020-08-26 18:20:51 +0200

    test(flags): disable PLL_Frontend constructor to avoid fatal errors in subsequent tests
    The PLL_Frontend_Filters::get_locale() filter would trigger a fatal error because $this->curlang is not defined.
    This happened if we only run Flags_Test and Swtcher_Test (both set new instances of PLL_Frontend).
    If we run the aforementioned tests with some other tests, no test would break (tried with Admin_Static_Pages_Test and Static_Pages_Test, but this may happen with other tests also).

--- tests/phpunit/tests/test-flags.php
+++ tests/phpunit/tests/test-flags.php
@@ -20,17 +20,25 @@ class Flags_Test extends PLL_UnitTestCase {
 	}
 
 	function setUp() {
-		self::$polylang = new PLL_Frontend( self::$polylang->links_model );
+		$options = PLL_Install::get_default_options();
+		$model = new PLL_Model( $options );
+		$links_model = new PLL_Links_Default( $model );
+		$this->pll_env = $this->getMockBuilder( PLL_Frontend::class )
+			->disableOriginalconstructor()
+			->getMock();
+		$this->pll_env->links_model = $links_model;
+		$this->pll_env->model = $model;
+		$this->pll_env->options = $options;
 	}
 
 	function test_default_flag() {
-		$lang = self::$polylang->model->get_language( 'en' );
+		$lang = $this->pll_env->model->get_language( 'en' );
 		$this->assertEquals( plugins_url( '/flags/us.png', POLYLANG_FILE ), $lang->get_display_flag_url() ); // Bug fixed in 2.8.1.
 		$this->assertEquals( 1, preg_match( '#<img src="data:image\/png;base64,(.+)" title="English" alt="English" width="16" height="11" style="(.+)" \/>#', $lang->get_display_flag() ) );
 	}
 
 	function test_custom_flag() {
-		$lang = self::$polylang->model->get_language( 'fr' );
+		$lang = $this->pll_env->model->get_language( 'fr' );
 		$this->assertEquals( content_url( '/polylang/fr_FR.png' ), $lang->get_display_flag_url() );
 		$this->assertEquals( '<img src="/wp-content/polylang/fr_FR.png" title="Français" alt="Français" />', $lang->get_display_flag() );
 	}
@@ -41,7 +49,7 @@ class Flags_Test extends PLL_UnitTestCase {
 	function test_default_flag_ssl() {
 		$_SERVER['HTTPS'] = 'on';
 
-		$lang = self::$polylang->model->get_language( 'en' );
+		$lang = $this->pll_env->model->get_language( 'en' );
 		$this->assertContains( 'https', $lang->get_display_flag_url() );
 
 		unset( $_SERVER['HTTPS'] );
@@ -50,7 +58,7 @@ class Flags_Test extends PLL_UnitTestCase {
 	function test_custom_flag_ssl() {
 		$_SERVER['HTTPS'] = 'on';
 
-		$lang = self::$polylang->model->get_language( 'fr' );
+		$lang = $this->pll_env->model->get_language( 'fr' );
 		$this->assertEquals( content_url( '/polylang/fr_FR.png' ), $lang->get_display_flag_url() );
 		$this->assertContains( 'https', $lang->get_display_flag_url() );
 
