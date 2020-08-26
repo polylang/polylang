29cf1f8398a0b771f026e5ebbc0d6e68edc79e7e
Frédéric Demarle <frederic.demarle@yahoo.fr>
2020-08-26 10:44:14 +0200

    Fix fatal error when no language is defined

--- modules/site-health/load.php
+++ modules/site-health/load.php
@@ -9,6 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Don't access directly.
 };
 
-if ( $polylang instanceof PLL_Admin ) {
+if ( $polylang instanceof PLL_Admin && $polylang->model->get_languages_list() ) {
 	$polylang->site_health = new PLL_Admin_Site_Health( $polylang );
 }
