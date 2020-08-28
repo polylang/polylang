59e37f6d0f215874f0b9cbfe0a1cf1113d8f354d
raaaahman <sylvain@devindetails.com>
2020-08-28 12:04:51 +0200

    test(flags): Removed unused tearDown method

--- tests/phpunit/tests/test-flags.php
+++ tests/phpunit/tests/test-flags.php
@@ -39,12 +39,6 @@ class Flags_Test extends PLL_UnitTestCase {
 		parent::tearDown();
 	}
 
-	function tearDown() {
-		remove_filter( 'pll_custom_flag', array( $this, 'override_custom_flag' ) );
-
-		parent::tearDown();
-	}
-
 	function test_default_flag() {
 		$lang = self::$model->get_language( 'fr' );
 		$this->assertEquals( plugins_url( '/flags/fr.png', POLYLANG_FILE ), $lang->get_display_flag_url() ); // Bug fixed in 2.8.1.
