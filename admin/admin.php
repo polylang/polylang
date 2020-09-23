366d7159f2fc48e33aa66905caa70cf17807e86a
Emmanuel Hesry <ehesry@gmail.com>
2020-09-23 12:08:13 +0200

    Change of PLL_Filters_Sanitization signature

--- admin/admin.php
+++ admin/admin.php
@@ -149,7 +149,7 @@ class PLL_Admin extends PLL_Admin_Base {
 	 * @since 2.7 instantiate a PLL_Bulk_Translate instance.
 	 */
 	public function add_filters() {
-		$this->filters_sanitization = new PLL_Filters_Sanitization( $this, $this->get_locale_for_sanitization() );
+		$this->filters_sanitization = new PLL_Filters_Sanitization( $this->get_locale_for_sanitization() );
 		// All these are separated just for convenience and maintainability
 		$classes = array( 'Filters', 'Filters_Columns', 'Filters_Post', 'Filters_Term', 'Nav_Menu', 'Classic_Editor', 'Block_Editor' );
 
