3a92580adcc513608e19c882cd9f824369ce86c4
Sylvain Schellenberger <sylvain@devindetails.com>
2020-10-21 14:03:38 +0200

    Fix tests for canonical redirect (#592)

--- tests/phpunit/includes/testcase-canonical.php
+++ tests/phpunit/includes/testcase-canonical.php
@@ -1,54 +1,86 @@
 <?php
 
-class PLL_Canonical_UnitTestCase extends PLL_UnitTestCase {
+class PLL_Canonical_UnitTestCase extends WP_Canonical_UnitTestCase {
+	use PLL_UnitTestCase_Trait;
 
-	// mainly copy paste from WP_Canonical_UnitTestCase::assertCanonical
-	public function assertCanonical( $test_url, $expected ) {
-		if ( is_string( $expected ) ) {
-			$expected = array( 'url' => $expected );
-		} elseif ( is_array( $expected ) && ! isset( $expected['url'] ) && ! isset( $expected['qv'] ) ) {
-			$expected = array( 'qv' => $expected );
+	private $options;
+
+	public function setUp() {
+		parent::setUp();
+
+		add_filter( 'wp_using_themes', '__return_true' ); // To pass the test in PLL_Choose_Lang::init() by default.
+		add_filter( 'wp_doing_ajax', '__return_false' );
+
+		$this->options = array_merge(
+			PLL_Install::get_default_options(),
+			array(
+				'default_lang' => 'en',
+				'hide_default' => 0,
+				'post_types'   => array(
+					'cpt' => 'pllcanonical',
+					// translate the cpt // FIXME /!\ 'after_setup_theme' already fired and the list of translated post types is already cached :(
+				),
+			)
+		);
+	}
+
+	/**
+	 * Set up the Polylang environment before testing canonical redirects.
+	 *
+	 * @param string $test_url
+	 * @param string $expected
+	 * @param int    $ticket
+	 * @param array  $expected_doing_it_wrong
+	 */
+	public function assertCanonical( $test_url, $expected, $ticket = 0, $expected_doing_it_wrong = array() ) {
+		global $wp_rewrite;
+
+		if ( did_action( 'pll_language_defined' ) ) {
+			$this->fail( 'Canonical tests MUST have only one call to PLL_UnitTestCase_Canonical::assertCanonical() per test.' );
 		}
 
-		if ( ! isset( $expected['url'] ) && ! isset( $expected['qv'] ) ) {
-			$this->markTestSkipped( 'No valid expected output was provided' );
+		// Needed by {@see pll_requested_url()}.
+		$_SERVER['REQUEST_URI'] = $test_url;
+
+		$model = new PLL_Model( $this->options );
+
+		// switch to pretty permalinks
+		$wp_rewrite->init();
+		$wp_rewrite->set_permalink_structure( $this->structure );
+
+		// register post types and taxonomies
+		$model->post->register_taxonomy(); // needs this for 'lang' query var
+		create_initial_taxonomies();
+
+		// reset the links model according to the permalink structure
+		$links_model    = $model->get_links_model();
+		self::$polylang = new PLL_Frontend( $links_model );
+		self::$polylang->init();
+		do_action_ref_array( 'pll_init', array( self::$polylang ) );
+
+		// flush rules
+		$wp_rewrite->extra_rules_top = array(); // brute force since WP does not do it :(
+		$wp_rewrite->flush_rules();
+
+		return parent::assertCanonical( $test_url, $expected, $ticket, $expected_doing_it_wrong );
+	}
+
+	/**
+	 * Parses the canonical url if redirect, by either Polylang and/or WordPress.
+	 *
+	 * The {@see PLL_Frontend_Filters_Links::check_canonical_url()} method is hooked on {@see https://github.com/WordPress/wordpress-develop/blob/505fe2f0b87bba956d399f657f85a7073c978289/src/wp-includes/template-loader.php#L13 template_redirect}, which is not triggered during automated tests.
+	 *
+	 * @param string $test_url
+	 *
+	 * @return string Either the canonical url, if redirected, or the inputted $test_url.
+	 */
+	public function get_canonical( $test_url ) {
+		$pll_redirected_url = self::$polylang->filters_links->check_canonical_url( home_url( $test_url ), false );
+		$wp_redirected_url  = redirect_canonical( $pll_redirected_url, false );
+		if ( ! $wp_redirected_url ) {
+			return $pll_redirected_url;
 		}
 
-		$this->go_to( home_url( $test_url ) );
-
-		// Does the redirect match what's expected?
-		$can_url = self::$polylang->filters_links->check_canonical_url( home_url( $test_url ), false ); // FIXME TODO define links ( need $curlang )
-		if ( $wp_can_url = redirect_canonical( $can_url, false ) ) {
-			$parsed_can_url = wp_parse_url( $wp_can_url );
-		} else {
-			$parsed_can_url = wp_parse_url( $can_url );
-		}
-
-		// Just test the Path and Query if present
-		if ( isset( $expected['url'] ) ) {
-			$this->assertEquals( $expected['url'], $parsed_can_url['path'] . ( ! empty( $parsed_can_url['query'] ) ? '?' . $parsed_can_url['query'] : '' ) );
-		}
-
-		if ( ! isset( $expected['qv'] ) ) {
-			return;
-		}
-
-		// "make" that the request and check the query is correct
-		$this->go_to( $can_url );
-
-		// Are all query vars accounted for, And correct?
-		global $wp;
-
-		$query_vars = array_diff( $wp->query_vars, $wp->extra_query_vars );
-		if ( ! empty( $parsed_can_url['query'] ) ) {
-			parse_str( $parsed_can_url['query'], $_qv );
-
-			// $_qv should not contain any elements which are set in $query_vars already ( ie. $_GET vars should not be present in the Rewrite )
-			$this->assertEquals( array(), array_intersect( $query_vars, $_qv ) );
-
-			$query_vars = array_merge( $query_vars, $_qv );
-		}
-
-		$this->assertEquals( $expected['qv'], $query_vars );
+		return $wp_redirected_url;
 	}
 }
--- tests/phpunit/tests/test-canonical.php
+++ tests/phpunit/tests/test-canonical.php
@@ -3,55 +3,77 @@
 class Canonical_Test extends PLL_Canonical_UnitTestCase {
 	public $structure = '/%postname%/';
 
-	static function wpSetUpBeforeClass() {
-		parent::wpSetUpBeforeClass();
+	private static $post_en;
+	private static $page_id;
+	private static $custom_post_id;
+	private static $term_en;
+	private static $page_for_posts_en;
+	private static $page_for_posts_fr;
+	private static $page_on_front_en;
+
+	public static function wpSetUpBeforeClass( $factory ) {
+		parent::wpSetUpBeforeClass( $factory );
 
 		self::create_language( 'en_US' );
 		self::create_language( 'fr_FR' );
 
 		require_once POLYLANG_DIR . '/include/api.php';
 		$GLOBALS['polylang'] = &self::$polylang;
-		self::$polylang->options['hide_default'] = 0;
-	}
 
-	function setUp() {
-		parent::setUp();
+		self::$post_en = $factory->post->create( array( 'post_title' => 'post-format-test-audio' ) );
+		self::$polylang->model->post->set_language( self::$post_en, 'en' );
 
-		global $wp_rewrite;
+		self::$page_id = $factory->post->create( array( 'post_type' => 'page', 'post_title' => 'parent-page' ) );
+		self::$polylang->model->post->set_language( self::$page_id, 'en' );
 
-		self::$polylang->options['post_types'] = array(
-			'cpt' => 'cpt', // translate the cpt // FIXME /!\ 'after_setup_theme' already fired and the list of translated post types is already cached :(
+		add_action(
+			'registered_taxonomy',
+			function( $taxonomy ) {
+				if ( 'post_format' === $taxonomy && ! post_type_exists( 'pllcanonical' ) ) { // Last taxonomy registered in {@see https://github.com/WordPress/wordpress-develop/blob/36ef9cbca96fca46e7daf1ee687bb6a20788385c/src/wp-includes/taxonomy.php#L158-L174 create_initial_taxonomies()}
+					register_post_type( 'pllcanonical', array( 'public' => true ) );
+				}
+			}
 		);
 
-		// switch to pretty permalinks
-		$wp_rewrite->init();
-		$wp_rewrite->set_permalink_structure( $this->structure );
+		self::$custom_post_id = $factory->post->create( array( 'import_id' => 416, 'post_type' => 'pllcanonical', 'post_title' => 'custom-post' ) );
+		self::$polylang->model->post->set_language( self::$custom_post_id, 'en' );
 
-		// register post types and taxonomies
-		self::$polylang->model->post->register_taxonomy(); // needs this for 'lang' query var
-		create_initial_taxonomies();
-		register_post_type( 'cpt', array( 'public' => true ) ); // add custom post type
+		self::$term_en = $factory->term->create( array( 'taxonomy' => 'category', 'name' => 'parent' ) );
+		self::$polylang->model->term->set_language( self::$term_en, 'en' );
 
-		// reset the links model according to the permalink structure
-		self::$polylang->links_model = self::$polylang->model->get_links_model();
-		self::$polylang->links_model->init();
+		self::$polylang->static_pages = new PLL_Admin_Static_Pages( self::$polylang );
+		update_option( 'show_on_front', 'page' );
 
-		// flush rules
-		$wp_rewrite->extra_rules_top = array(); // brute force since WP does not do it :(
-		$wp_rewrite->flush_rules();
+		self::$page_for_posts_en = $factory->post->create( array( 'post_title' => 'posts', 'post_type' => 'page' ) );
+		self::$polylang->model->post->set_language( self::$page_for_posts_en, 'en' );
 
-		self::$polylang->filters_links = new PLL_Frontend_Filters_Links( self::$polylang );
+		self::$page_for_posts_fr = $factory->post->create( array( 'post_title' => 'articles', 'post_type' => 'page' ) );
+		self::$polylang->model->post->set_language( self::$page_for_posts_fr, 'fr' );
+
+		self::$polylang->model->post->save_translations( self::$page_for_posts_en, compact( 'en', 'fr' ) );
+
+		update_option( 'page_for_posts', self::$page_for_posts_fr );
+
+		self::$page_on_front_en = $factory->post->create( array( 'post_type' => 'page', 'post_title' => 'parent-page' ) );
+		self::$polylang->model->post->set_language( self::$page_on_front_en, 'en' );
+
+		self::$polylang->static_pages = new PLL_Admin_Static_Pages( self::$polylang );
+		update_option( 'show_on_front', 'posts' );
 	}
 
-	function tearDown() {
-		parent::tearDown();
+	public static function wpTearDownAfterClass() {
+		self::$post_en = null;
+		self::$page_id = null;
+		self::$custom_post_id = null;
+		self::$term_en = null;
+		self::$page_for_posts_en = null;
+		self::$page_for_posts_fr = null;
+		self::$page_on_front_en = null;
 
-		_unregister_post_type( 'cpt' );
+		_unregister_post_type( 'pllcanonical' );
 	}
 
-	function test_post() {
-		$post_id = $this->factory->post->create( array( 'post_title' => 'post-format-test-audio' ) );
-		self::$polylang->model->post->set_language( $post_id, 'en' );
+	public function test_post_with_name_and_language() {
 		$this->assertCanonical(
 			'/en/post-format-test-audio/',
 			array(
@@ -59,14 +81,17 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 				'qv'  => array( 'lang' => 'en', 'name' => 'post-format-test-audio', 'page' => '' ),
 			)
 		);
+	}
+
+	public function test_post_with_incorrect_language() {
 		$this->assertCanonical( '/fr/post-format-test-audio/', '/en/post-format-test-audio/' );
+	}
+
+	public function test_post_without_language() {
 		$this->assertCanonical( '/post-format-test-audio/', '/en/post-format-test-audio/' );
 	}
 
-	function test_page() {
-		$post_id = $this->factory->post->create( array( 'post_type' => 'page', 'post_title' => 'parent-page' ) );
-		self::$polylang->model->post->set_language( $post_id, 'en' );
-
+	public function test_page_with_name_and_language() {
 		$this->assertCanonical(
 			'/en/parent-page/',
 			array(
@@ -74,33 +99,36 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 				'qv'  => array( 'lang' => 'en', 'pagename' => 'parent-page', 'page' => '' ),
 			)
 		);
+	}
+
+	public function test_page_with_incorrect_language() {
 		$this->assertCanonical( '/fr/parent-page/', '/en/parent-page/' );
+	}
+
+	public function test_page_without_language() {
 		$this->assertCanonical( '/parent-page/', '/en/parent-page/' );
 	}
 
-	function test_cpt() {
-		// custom post type
-		$post_id = $this->factory->post->create( array( 'import_id' => 416, 'post_type' => 'cpt', 'post_title' => 'custom-post' ) );
-		self::$polylang->model->post->set_language( $post_id, 'en' );
-
+	public function test_custom_post_type_with_name_and_language() {
 		$this->assertCanonical(
-			'/en/cpt/custom-post/',
+			'/en/pllcanonical/custom-post/',
 			array(
-				'url' => '/en/cpt/custom-post/',
-				'qv'  => array( 'lang' => 'en', 'cpt' => 'custom-post', 'name' => 'custom-post', 'post_type' => 'cpt', 'page' => '' ),
+				'url' => '/en/pllcanonical/custom-post/',
+				'qv'  => array( 'lang' => 'en', 'pllcanonical' => 'custom-post', 'name' => 'custom-post', 'post_type' => 'pllcanonical', 'page' => '' ),
 			)
 		);
-		$this->assertCanonical( '/fr/cpt/custom-post/', '/en/cpt/custom-post/' );
-		$this->assertCanonical( '/cpt/custom-post/', '/en/cpt/custom-post/' );
 	}
 
-	function test_category() {
-		$term_id = $this->factory->term->create( array( 'taxonomy' => 'category', 'name' => 'parent' ) );
-		self::$polylang->model->term->set_language( $term_id, 'en' );
+	public function test_custom_post_type_with_incorrect_language() {
+		$this->assertCanonical( '/fr/pllcanonical/custom-post/', '/en/pllcanonical/custom-post/' );
+	}
 
-		$term_id = $this->factory->term->create( array( 'taxonomy' => 'category', 'name' => 'parent-fr' ) );
-		self::$polylang->model->term->set_language( $term_id, 'fr' );
 
+	public function test_custom_post_type_without_language() {
+		$this->assertCanonical( '/pllcanonical/custom-post/', '/en/pllcanonical/custom-post/' );
+	}
+
+	public function test_category_with_name_and_language() {
 		$this->assertCanonical(
 			'/en/category/parent/',
 			array(
@@ -108,34 +136,17 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 				'qv'  => array( 'lang' => 'en', 'category_name' => 'parent' ),
 			)
 		);
-		$this->assertCanonical( '/fr/category/parent/', '/en/category/parent/' );
-		$this->assertCanonical( '/category/parent/', '/en/category/parent/' );
-
-		$this->assertCanonical( '/en/category/parent-fr/', '/fr/category/parent-fr/' );
-		$this->assertCanonical( '/category/parent-fr/', '/fr/category/parent-fr/' );
 	}
 
-	function test_posts_page() {
-		self::$polylang->static_pages = new PLL_Admin_Static_Pages( self::$polylang );
-		update_option( 'show_on_front', 'page' );
+	public function test_category_with_incorrect_language() {
+		$this->assertCanonical( '/fr/category/parent/', '/en/category/parent/' );
+	}
 
-		$this->posts_en = $en = $this->factory->post->create( array( 'post_title' => 'posts', 'post_type' => 'page' ) );
-		self::$polylang->model->post->set_language( $en, 'en' );
+	public function test_category_without_language() {
+		$this->assertCanonical( '/category/parent/', '/en/category/parent/' );
+	}
 
-		$this->posts_fr = $fr = $this->factory->post->create( array( 'post_title' => 'articles', 'post_type' => 'page' ) );
-		self::$polylang->model->post->set_language( $fr, 'fr' );
-
-		self::$polylang->model->post->save_translations( $en, compact( 'en', 'fr' ) );
-
-		update_option( 'page_for_posts', $fr );
-
-		// go to frontend
-		self::$polylang = new PLL_Frontend( self::$polylang->links_model );
-		self::$polylang->init();
-
-		self::$polylang->static_pages = new PLL_Frontend_Static_Pages( self::$polylang );
-		self::$polylang->static_pages->pll_language_defined();
-
+	public function test_page_for_posts_with_name_and_language() {
 		$this->assertCanonical(
 			'/en/posts/',
 			array(
@@ -143,28 +154,25 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 				'qv'  => array( 'lang' => 'en', 'pagename' => 'posts', 'page' => '' ),
 			)
 		);
-		$this->assertCanonical( '/fr/posts/', '/en/posts/' );
-		$this->assertCanonical( '/posts/', '/en/posts/' );
+	}
 
+	public function test_page_for_posts_should_match_page_for_post_option_when_language_is_incorrect() {
+		$this->assertCanonical( '/fr/posts/', '/en/posts/' );
+	}
+
+	public function test_page_for_should_match_page_for_post_option_posts_without_language() {
+		$this->assertCanonical( '/posts/', '/en/posts/' );
+	}
+
+	public function test_page_for_post_option_should_be_translated_when_language_is_incorrect() {
 		$this->assertCanonical( '/en/articles/', '/fr/articles/' );
+	}
+
+	public function test_page_for_post_option_should_be_translated_when_no_language_is_set() {
 		$this->assertCanonical( '/articles/', '/fr/articles/' );
 	}
 
-	// bug introduced in 1.8.2 and fixed in 1.8.3
-	function test_page_when_static_front_page_displays_posts() {
-		$post_id = $this->factory->post->create( array( 'post_type' => 'page', 'post_title' => 'parent-page' ) );
-		self::$polylang->model->post->set_language( $post_id, 'en' );
-
-		self::$polylang->static_pages = new PLL_Admin_Static_Pages( self::$polylang );
-		update_option( 'show_on_front', 'posts' );
-
-		// go to frontend
-		self::$polylang = new PLL_Frontend( self::$polylang->links_model );
-		self::$polylang->init();
-
-		self::$polylang->static_pages = new PLL_Frontend_Static_Pages( self::$polylang );
-		self::$polylang->static_pages->pll_language_defined();
-
+	public function test_static_front_page_with_name_and_language() {
 		$this->assertCanonical(
 			'/en/parent-page/',
 			array(
@@ -172,7 +180,13 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 				'qv'  => array( 'lang' => 'en', 'pagename' => 'parent-page', 'page' => '' ),
 			)
 		);
+	}
+
+	public function test_static_front_page_with_incorrect_language() {
 		$this->assertCanonical( '/fr/parent-page/', '/en/parent-page/' );
+	}
+
+	public function test_static_front_page_without_language() {
 		$this->assertCanonical( '/parent-page/', '/en/parent-page/' );
 	}
 }
