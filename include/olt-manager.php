5f73c25bc8ff11450400d9cbe61237518e1b96af
Frédéric Demarle <frederic.demarle@yahoo.fr>
2020-10-28 16:18:23 +0100

    Fix deprecated function usage in WP 5.6

--- include/olt-manager.php
+++ include/olt-manager.php
@@ -78,10 +78,12 @@ class PLL_OLT_Manager {
 		// Now we can load all overridden text domains with the right language
 		if ( ! empty( $this->list_textdomains ) ) {
 
-			// Since WP 4.7 we need to reset the internal cache of _get_path_to_translation when switching from any locale to en_US
-			// See WP_Locale_Switcher::change_locale()
-			// FIXME test _get_path_to_translation for backward compatibility with WP < 4.7
-			if ( function_exists( '_get_path_to_translation' ) ) {
+			/*
+			 * FIXME: Backward compatibility with WP < 5.6
+			 * From WP 4.7 to 5.5, we need to reset the internal cache of _get_path_to_translation when switching from any locale to en_US.
+			 * See WP_Locale_Switcher::change_locale()
+			 */
+			if ( ! class_exists( 'WP_Textdomain_Registry' ) && function_exists( '_get_path_to_translation' ) ) {
 				_get_path_to_translation( null, true );
 			}
 
