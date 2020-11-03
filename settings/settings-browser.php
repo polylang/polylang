fcd9da312d5a0c40689eef12f465d21bfb25c787
Marianne <marianne.honczaruk@gmail.com>
2020-11-03 17:03:41 +0100

    Fix jQuery.fn.change() event shorthand is deprecated

--- settings/settings-browser.php
+++ settings/settings-browser.php
@@ -86,7 +86,7 @@ class PLL_Settings_Browser extends PLL_Settings_Module {
 		<script type='text/javascript'>
 			//<![CDATA[
 			( function( $ ){
-				$( "input[name='force_lang']" ).change( function() {
+				$( "input[name='force_lang']" ).on( 'change', function() {
 					var value = $( this ).val();
 					if ( 3 > value ) {
 						$( "#pll-module-browser" ).<?php echo $func; // phpcs:ignore WordPress.Security.EscapeOutput ?>.children( "td" ).children( ".row-actions" ).html( '<?php echo $link; // phpcs:ignore WordPress.Security.EscapeOutput ?>' );
