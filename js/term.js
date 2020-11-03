fad50eb8c9deffd2fbb137e34bdbb258d6655afc
Frédéric Demarle <frederic.demarle@yahoo.fr>
2020-11-03 15:07:34 +0100

    term.js code modernization

--- js/term.js
+++ js/term.js
@@ -2,117 +2,124 @@
  * @package Polylang
  */
 
-// quick edit
-(function( $ ) {
-	$( document ).bind(
-		'DOMNodeInserted',
-		function( e ) {
-			var t = $( e.target );
+/**
+ * Quick edit
+ */
+jQuery(
+	function( $ ) {
+		$( document ).on(
+			'DOMNodeInserted',
+			function( e ) {
+				var t = $( e.target );
 
-			// WP inserts the quick edit from
-			if ( 'inline-edit' == t.attr( 'id' ) ) {
-				var term_id = t.prev().attr( 'id' ).replace( "tag-", "" );
+				// WP inserts the quick edit from
+				if ( 'inline-edit' == t.attr( 'id' ) ) {
+					var term_id = t.prev().attr( 'id' ).replace( "tag-", "" );
 
-				if ( term_id > 0 ) {
-					// language dropdown
-					var select = t.find( ':input[name="inline_lang_choice"]' );
-					var lang = $( '#lang_' + term_id ).html();
-					select.val( lang ); // populates the dropdown
+					if ( term_id > 0 ) {
+						// language dropdown
+						var select = t.find( ':input[name="inline_lang_choice"]' );
+						var lang = $( '#lang_' + term_id ).html();
+						select.val( lang ); // populates the dropdown
 
-					// disable the language dropdown for default categories
-					var default_cat = $( '#default_cat_' + term_id ).html();
-					if ( term_id == default_cat ) {
-						select.prop( 'disabled', true );
+						// disable the language dropdown for default categories
+						var default_cat = $( '#default_cat_' + term_id ).html();
+						if ( term_id == default_cat ) {
+							select.prop( 'disabled', true );
+						}
 					}
 				}
 			}
-		}
-	);
-})( jQuery );
+		);
+	}
+);
 
+/**
+ * Update rows of translated terms when adding / deleting a translation or when the language is modified in quick edit.
+ * Acts on ajaxSuccess event.
+ */
+jQuery(
+	function( $ ) {
+		$( document ).ajaxSuccess(
+			function( event, xhr, settings ) {
+				function update_rows( term_id ) {
+					// collect old translations
+					var translations = new Array();
+					$( '.translation_' + term_id ).each(
+						function() {
+							translations.push( $( this ).parent().parent().attr( 'id' ).substring( 4 ) );
+						}
+					);
 
-// update rows of translated terms when adding / deleting a translation or when the language is modified in quick edit
-// acts on ajaxSuccess event
-(function( $ ) {
-	$( document ).ajaxSuccess(
-		function( event, xhr, settings ) {
-			function update_rows( term_id ) {
-				// collect old translations
-				var translations = new Array();
-				$( '.translation_' + term_id ).each(
-					function() {
-						translations.push( $( this ).parent().parent().attr( 'id' ).substring( 4 ) );
-					}
-				);
+					var data = {
+						action:       'pll_update_term_rows',
+						term_id:      term_id,
+						translations: translations.join( ',' ),
+						taxonomy:     $( "input[name='taxonomy']" ).val(),
+						post_type:    $( "input[name='post_type']" ).val(),
+						screen:       $( "input[name='screen']" ).val(),
+						_pll_nonce:   $( '#_pll_nonce' ).val()
+					};
 
-				var data = {
-					action:       'pll_update_term_rows',
-					term_id:      term_id,
-					translations: translations.join( ',' ),
-					taxonomy:     $( "input[name='taxonomy']" ).val(),
-					post_type:    $( "input[name='post_type']" ).val(),
-					screen:       $( "input[name='screen']" ).val(),
-					_pll_nonce:   $( '#_pll_nonce' ).val()
-				};
+					// get the modified rows in ajax and update them
+					$.post(
+						ajaxurl,
+						data,
+						function( response ) {
+							if ( response ) {
+								var res = wpAjax.parseAjaxResponse( response, 'ajax-response' );
+								$.each(
+									res.responses,
+									function() {
+										if ( 'row' == this.what ) {
+											// data is built with a call to WP_Terms_List_Table::single_row method
+											// which uses internally other WordPress methods which escape correctly values.
+											// For Polylang language columns the HTML code is correctly escaped in PLL_Admin_Filters_Columns::term_column method.
+											$( "#tag-" + this.supplemental.term_id ).replaceWith( this.data ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.replaceWith
+										}
+									}
+								);
+							}
+						}
+					);
+				}
 
-				// get the modified rows in ajax and update them
-				$.post(
-					ajaxurl,
-					data,
-					function( response ) {
-						if ( response ) {
-							var res = wpAjax.parseAjaxResponse( response, 'ajax-response' );
+				var data = wpAjax.unserialize( settings.data ); // what were the data sent by the ajax request?
+				if ( 'undefined' != typeof( data['action'] ) ) {
+					switch ( data['action'] ) {
+						// when adding a term, the new term_id is in the ajax response
+						case 'add-tag':
+							res = wpAjax.parseAjaxResponse( xhr.responseXML, 'ajax-response' );
 							$.each(
 								res.responses,
 								function() {
-									if ( 'row' == this.what ) {
-										// data is built with a call to WP_Terms_List_Table::single_row method
-										// which uses internally other WordPress methods which escape correctly values.
-										// For Polylang language columns the HTML code is correctly escaped in PLL_Admin_Filters_Columns::term_column method.
-										$( "#tag-" + this.supplemental.term_id ).replaceWith( this.data ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.replaceWith
+									if ( 'term' == this.what ) {
+										update_rows( this.supplemental.term_id );
 									}
 								}
 							);
-						}
+
+							// and also reset translations hidden input fields
+							$( '.htr_lang' ).val( 0 );
+						break;
+
+						// when deleting a term
+						case 'delete-tag':
+							update_rows( data['tag_ID'] );
+						break;
+
+						// in case the language is modified in quick edit and breaks translations
+						case 'inline-save-tax':
+							update_rows( data['tax_ID'] );
+						break;
 					}
-				);
-			}
-
-			var data = wpAjax.unserialize( settings.data ); // what were the data sent by the ajax request?
-			if ( 'undefined' != typeof( data['action'] ) ) {
-				switch ( data['action'] ) {
-					// when adding a term, the new term_id is in the ajax response
-					case 'add-tag':
-						res = wpAjax.parseAjaxResponse( xhr.responseXML, 'ajax-response' );
-						$.each(
-							res.responses,
-							function() {
-								if ( 'term' == this.what ) {
-									update_rows( this.supplemental.term_id );
-								}
-							}
-						);
-
-						// and also reset translations hidden input fields
-						$( '.htr_lang' ).val( 0 );
-					break;
-
-					// when deleting a term
-					case 'delete-tag':
-						update_rows( data['tag_ID'] );
-					break;
-
-					// in case the language is modified in quick edit and breaks translations
-					case 'inline-save-tax':
-						update_rows( data['tax_ID'] );
-					break;
 				}
 			}
-		}
-	);
-})( jQuery );
+		);
+	}
+);
 
-jQuery( document ).ready(
+jQuery(
 	function( $ ) {
 		// translations autocomplete input box
 		function init_translations() {
@@ -140,7 +147,8 @@ jQuery( document ).ready(
 					);
 
 					// when the input box is emptied
-					$( this ).blur(
+					$( this ).on(
+						'blur',
 						function() {
 							if ( ! $( this ).val() ) {
 								$( '#htr_lang_' + tr_lang ).val( 0 );
