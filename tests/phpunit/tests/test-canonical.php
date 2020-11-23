polylang/polylang

commit 36b3aa5cc83b7b48c4fd2ceda30de01922f7c70b

Fix canonical redirect for taxonomy terms (#597)
Co-authored-by: Sylvain Schellenberger <sylvain@devindetails.com>

frontend/frontend-filters-links.php
@@ -383,10 +383,10 @@ class PLL_Frontend_Filters_Links extends PLL_Filters_Links {
 			}
 		}
 
-		elseif ( is_category() || is_tag() || is_tax() ) {
-			$obj = $wp_query->get_queried_object();
-			if ( ! empty( $obj ) && $this->model->is_translated_taxonomy( $obj->taxonomy ) ) {
-				$language = $this->model->term->get_language( (int) $obj->term_id );
+		elseif ( is_category() || is_tag() || is_tax() || ( is_404() && ! empty( $wp_query->tax_query ) ) ) {
+			if ( $this->model->is_translated_taxonomy( $this->get_queried_taxonomy( $wp_query->tax_query ) ) ) {
+				$term_id = $this->get_queried_term_id( $wp_query->tax_query );
+				$language = $this->model->term->get_language( $term_id );
 			}
 		}
 
@@ -410,6 +410,10 @@ class PLL_Frontend_Filters_Links extends PLL_Filters_Links {
 					$redirect_url = str_replace( '://' . $requested_host, '://' . $host, $requested_url );
 				}
 			}
+		}
+
+		if ( isset( $term_id ) ) {
+			$redirect_url = get_term_link( $term_id );
 		}
 
 		if ( empty( $language ) ) {
@@ -444,5 +448,52 @@ class PLL_Frontend_Filters_Links extends PLL_Filters_Links {
 		}
 
 		return $redirect_url;
+	}
+
+	/**
+	 * Returns the term_id of the requested term.
+	 *
+	 * @since 2.9
+	 *
+	 * @param object $tax_query An instance of WP_Tax_Query.
+	 * @return int
+	 */
+	protected function get_queried_term_id( $tax_query ) {
+		$queried_terms = $tax_query->queried_terms;
+		$taxonomy = $this->get_queried_taxonomy( $tax_query );
+
+		$field = $queried_terms[ $taxonomy ]['field'];
+		$term  = reset( $queried_terms[ $taxonomy ]['terms'] );
+
+		// We can get a term_id when requesting a plain permalink, eg /?cat=1.
+		if ( 'term_id' === $field ) {
+			return $term;
+		}
+
+		// We get a slug when requesting a pretty permalink with the wrong language.
+		$args = array(
+			'lang' => '',
+			'taxonomy' => $taxonomy,
+			$field => $term,
+			'hide_empty' => false,
+			'fields' => 'ids',
+		);
+		$terms = get_terms( $args );
+		return reset( $terms );
+	}
+
+	/**
+	 * Find the taxonomy being queried.
+	 *
+	 * @since 2.9
+	 *
+	 * @param object $tax_query An instance of WP_Tax_Query.
+	 * @return string A taxonomy slug
+	 */
+	protected function get_queried_taxonomy( $tax_query ) {
+		$queried_terms = $tax_query->queried_terms;
+		unset( $queried_terms['language'] );
+
+		return key( $queried_terms );
 	}
 }

tests/phpunit/tests/test-canonical.php
@@ -47,17 +47,17 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 		self::$polylang->static_pages = new PLL_Admin_Static_Pages( self::$polylang );
 		update_option( 'show_on_front', 'page' );
 
-		self::$page_for_posts_en = $factory->post->create( array( 'post_title' => 'posts', 'post_type' => 'page' ) );
+		$en = self::$page_for_posts_en = $factory->post->create( array( 'post_title' => 'posts', 'post_type' => 'page' ) );
 		self::$polylang->model->post->set_language( self::$page_for_posts_en, 'en' );
 
-		self::$page_for_posts_fr = $factory->post->create( array( 'post_title' => 'articles', 'post_type' => 'page' ) );
+		$fr = self::$page_for_posts_fr = $factory->post->create( array( 'post_title' => 'articles', 'post_type' => 'page' ) );
 		self::$polylang->model->post->set_language( self::$page_for_posts_fr, 'fr' );
 
 		self::$polylang->model->post->save_translations( self::$page_for_posts_en, compact( 'en', 'fr' ) );
 
 		update_option( 'page_for_posts', self::$page_for_posts_fr );
 
-		self::$page_on_front_en = $factory->post->create( array( 'post_type' => 'page', 'post_title' => 'parent-page' ) );
+		self::$page_on_front_en = $factory->post->create( array( 'post_type' => 'page', 'post_title' => 'home' ) );
 		self::$polylang->model->post->set_language( self::$page_on_front_en, 'en' );
 
 		self::$polylang->static_pages = new PLL_Admin_Static_Pages( self::$polylang );
@@ -94,6 +94,10 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 		$this->assertCanonical( '/post-format-test-audio/', '/en/post-format-test-audio/' );
 	}
 
+	public function test_post_from_plain_permalink() {
+		$this->assertCanonical( '?p=' . self::$post_en, '/en/post-format-test-audio/' );
+	}
+
 	public function test_page_with_name_and_language() {
 		$this->assertCanonical(
 			'/en/parent-page/',
@@ -110,6 +114,10 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 
 	public function test_page_without_language() {
 		$this->assertCanonical( '/parent-page/', '/en/parent-page/' );
+	}
+
+	public function test_page_from_plain_permalink() {
+		$this->assertCanonical( '?page_id=' . self::$page_id, '/en/parent-page/' );
 	}
 
 	public function test_custom_post_type_with_name_and_language() {
@@ -149,6 +157,10 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 		$this->assertCanonical( '/category/parent/', '/en/category/parent/' );
 	}
 
+	public function test_category_from_plain_permalink() {
+		$this->assertCanonical( '?cat=' . self::$term_en, '/en/category/parent/' );
+	}
+
 	public function test_page_for_posts_with_name_and_language() {
 		$this->assertCanonical(
 			'/en/posts/',
@@ -163,8 +175,12 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 		$this->assertCanonical( '/fr/posts/', '/en/posts/' );
 	}
 
-	public function test_page_for_should_match_page_for_post_option_posts_without_language() {
+	public function test_page_for_posts_should_match_page_for_post_option_posts_without_language() {
 		$this->assertCanonical( '/posts/', '/en/posts/' );
+	}
+
+	public function test_page_for_posts_should_match_page_for_post_option_posts_from_plain_permalink() {
+		$this->assertCanonical( '?page_id=' . self::$page_for_posts_en, '/en/posts/' );
 	}
 
 	public function test_page_for_post_option_should_be_translated_when_language_is_incorrect() {
@@ -175,21 +191,29 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 		$this->assertCanonical( '/articles/', '/fr/articles/' );
 	}
 
+	public function test_page_for_post_option_should_be_translated_from_plain_permalink() {
+		$this->assertCanonical( '?page_id=' . self::$page_for_posts_fr, '/fr/articles/' );
+	}
+
 	public function test_static_front_page_with_name_and_language() {
 		$this->assertCanonical(
-			'/en/parent-page/',
+			'/en/home/',
 			array(
-				'url' => '/en/parent-page/',
-				'qv'  => array( 'lang' => 'en', 'pagename' => 'parent-page', 'page' => '' ),
+				'url' => '/en/home/',
+				'qv'  => array( 'lang' => 'en', 'pagename' => 'home', 'page' => '' ),
 			)
 		);
 	}
 
 	public function test_static_front_page_with_incorrect_language() {
-		$this->assertCanonical( '/fr/parent-page/', '/en/parent-page/' );
+		$this->assertCanonical( '/fr/home/', '/en/home/' );
 	}
 
 	public function test_static_front_page_without_language() {
-		$this->assertCanonical( '/parent-page/', '/en/parent-page/' );
+		$this->assertCanonical( '/home/', '/en/home/' );
+	}
+
+	public function test_static_front_page_from_plain_permalink() {
+		$this->assertCanonical( '?page_id=' . self::$page_on_front_en, '/en/home/' );
 	}
 }

commit 134165c6d09c12205c7c606e7818c9dbd51f3dd3

Correct regression introduced by PR #597 and #634 (#648)
Co-authored-by: Frédéric Demarle <frederic.demarle@yahoo.fr>

frontend/frontend-filters-links.php
@@ -383,20 +383,34 @@ class PLL_Frontend_Filters_Links extends PLL_Filters_Links {
 			}
 		}
 
-		elseif ( is_category() || is_tag() || is_tax() || ( is_404() && ! empty( $wp_query->tax_query ) ) ) {
+		elseif ( $this->links_model->using_permalinks && is_category() && ! empty( $wp_query->query['cat'] ) ) {
+			// When we receive a plain permaling with a cat query var, we need to redirect to the pretty permalink.
 			if ( $this->model->is_translated_taxonomy( $this->get_queried_taxonomy( $wp_query->tax_query ) ) ) {
 				$term_id = $this->get_queried_term_id( $wp_query->tax_query );
 				$language = $this->model->term->get_language( $term_id );
-				$redirect_url = get_term_link( $term_id );
+				$redirect_url = $this->maybe_add_page_to_redirect_url( get_term_link( $term_id ) );
+			}
+		}
+
+		elseif ( is_category() || is_tag() || is_tax() ) {
+			// We need to switch the language when there is no language provided in a pretty permalink.
+			$obj = $wp_query->get_queried_object();
+			if ( ! empty( $obj ) && $this->model->is_translated_taxonomy( $obj->taxonomy ) ) {
+				$language = $this->model->term->get_language( (int) $obj->term_id );
+			}
+		}
+
+		elseif ( is_404() && ! empty( $wp_query->tax_query ) ) {
+			// When a wrong language is passed through a pretty permalink, we just need to switch the language.
+			if ( $this->model->is_translated_taxonomy( $this->get_queried_taxonomy( $wp_query->tax_query ) ) ) {
+				$term_id = $this->get_queried_term_id( $wp_query->tax_query );
+				$language = $this->model->term->get_language( $term_id );
 			}
 		}
 
 		elseif ( $this->links_model->using_permalinks && $wp_query->is_posts_page && ! empty( $wp_query->query['page_id'] ) && $id = get_query_var( 'page_id' ) ) {
 			$language = $this->model->post->get_language( (int) $id );
-			$redirect_url = get_permalink( $id );
-			if ( ! empty( $wp_query->query['paged'] ) && $page = get_query_var( 'paged' ) ) {
-				$redirect_url = $this->links_model->add_paged_to_link( $redirect_url, $page );
-			}
+			$redirect_url = $this->maybe_add_page_to_redirect_url( get_permalink( $id ) );
 		}
 
 		elseif ( $wp_query->is_posts_page ) {
@@ -446,6 +460,23 @@ class PLL_Frontend_Filters_Links extends PLL_Filters_Links {
 			exit;
 		}
 
+		return $redirect_url;
+	}
+
+	/**
+	 * Returns the link to the paged page if requested.
+	 *
+	 * @since 2.9
+	 *
+	 * @param string $redirect_url The url to redirect to.
+	 * @return string
+	 */
+	protected function maybe_add_page_to_redirect_url( $redirect_url ) {
+		global $wp_query;
+
+		if ( ! empty( $wp_query->query['paged'] ) && $page = get_query_var( 'paged' ) ) {
+			$redirect_url = $this->links_model->add_paged_to_link( $redirect_url, $page );
+		}
 		return $redirect_url;
 	}
 

tests/phpunit/tests/test-canonical.php
@@ -176,6 +176,20 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 		);
 	}
 
+	public function test_paged_category_with_name_and_language() {
+		$this->assertCanonical(
+			'/en/category/parent/page/2/',
+			array(
+				'url' => '/en/category/parent/page/2/',
+				'qv'  => array(
+					'lang'          => 'en',
+					'category_name' => 'parent',
+					'paged'         => 2,
+				),
+			)
+		);
+	}
+
 	public function test_category_with_incorrect_language() {
 		$this->assertCanonical( '/fr/category/parent/', '/en/category/parent/' );
 	}
@@ -186,6 +200,20 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 
 	public function test_category_from_plain_permalink() {
 		$this->assertCanonical( '?cat=' . self::$term_en, '/en/category/parent/' );
+	}
+
+	public function test_paged_category_from_plain_permalink() {
+		update_option( 'posts_per_page', 1 );
+
+		// Create 1 additional English post to have a paged category.
+		$en = $this->factory->post->create();
+		self::$polylang->model->post->set_language( $en, 'en' );
+
+		// Set category to the posts.
+		wp_set_post_terms( self::$post_en, array( self::$term_en ), 'category' );
+		wp_set_post_terms( $en, array( self::$term_en ), 'category' );
+
+		$this->assertCanonical( '?paged=2&cat=' . self::$term_en, '/en/category/parent/page/2/' );
 	}
 
 	public function test_page_for_posts_with_name_and_language() {