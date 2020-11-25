14448338ebce2b7131db43532094e15d07f92a9c
Marianne380 <marianne@wp-syntex.fr>
2020-11-25 11:02:44 +0100

    Add tests for default category (#647)

--- tests/phpunit/tests/test-columns.php
+++ tests/phpunit/tests/test-columns.php
@@ -145,6 +145,17 @@ class Columns_Test extends PLL_UnitTestCase {
 		$this->assertFalse( strpos( $column, 'href' ) );
 	}
 
+	function test_term_language_with_default_category() {
+		$GLOBALS['post_type'] = 'post';
+		$GLOBALS['taxonomy'] = 'category';
+
+		$default = (int) get_option( 'default_category' );
+
+		// with capability
+		$column = self::$polylang->filters_columns->term_column( '', 'language_en', $default );
+		$this->assertNotFalse( strpos( $column, 'default_cat' ) );
+	}
+
 	function test_untranslated_term() {
 		$GLOBALS['post_type'] = 'post';
 		$GLOBALS['taxonomy'] = 'category';
