421ec30d3ae8380eb66da357d813d2eabc1b8875
Frédéric Demarle <frederic.demarle@yahoo.fr>
2020-12-14 15:53:30 +0100

    Update install-plugins.sh

--- tests/bin/install-plugins.sh
+++ tests/bin/install-plugins.sh
@@ -2,9 +2,9 @@
 
 TMPDIR=${TMPDIR-/tmp}
 TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
-WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
 WORKING_DIR="$PWD"
+WP_CORE_DIR=$WORKING_DIR/tmp/wordpress
 
 download() {
     if [ `which curl` ]; then
@@ -26,23 +26,23 @@ fi
 download https://downloads.wordpress.org/plugin/wordpress-importer.zip $TMPDIR/downloads/wordpress-importer.zip
 unzip -q $TMPDIR/downloads/wordpress-importer.zip -d  $TMPDIR/downloads/
 mkdir -p $TMPDIR/wordpress-importer
-mv $TMPDIR/downloads/wordpress-importer/* $TMPDIR/wordpress-importer/
+mv $TMPDIR/downloads/wordpress-importer/* $WORKING_DIR/tmp/wordpress-importer/
 
 # Install Jetpack
 download https://downloads.wordpress.org/plugin/jetpack.zip $TMPDIR/downloads/jetpack.zip
 unzip -q $TMPDIR/downloads/jetpack.zip -d  $TMPDIR/downloads/
 mkdir -p $TMPDIR/jetpack
-mv $TMPDIR/downloads/jetpack/* $TMPDIR/jetpack/
+mv $TMPDIR/downloads/jetpack/* $WORKING_DIR/tmp/jetpack/
 
 #Install Yoast SEO
 download https://downloads.wordpress.org/plugin/wordpress-seo.zip $TMPDIR/downloads/wordpress-seo.zip
 unzip -q $TMPDIR/downloads/wordpress-seo.zip -d  $TMPDIR/downloads/
 mkdir -p $TMPDIR/wordpress-seo
-mv $TMPDIR/downloads/wordpress-seo/* $TMPDIR/wordpress-seo/
+mv $TMPDIR/downloads/wordpress-seo/* $WORKING_DIR/tmp/wordpress-seo/
 
 #Install Duplicate Post
 download https://downloads.wordpress.org/plugin/duplicate-post.zip $TMPDIR/downloads/duplicate-post.zip
 unzip -q $TMPDIR/downloads/duplicate-post.zip -d  $TMPDIR/downloads/
 mkdir -p $TMPDIR/duplicate-post
-mv $TMPDIR/downloads/duplicate-post/* $TMPDIR/duplicate-post/
+mv $TMPDIR/downloads/duplicate-post/* $WORKING_DIR/tmp/duplicate-post/
 
