8b63ed6da3aad336702aa2bac1f70c702da5c6e2
Frédéric Demarle <frederic.demarle@yahoo.fr>
2020-12-14 16:09:33 +0100

    Fix install-plugins.sh

--- tests/bin/install-plugins.sh
+++ tests/bin/install-plugins.sh
@@ -25,24 +25,24 @@ fi
 # Install WordPress Importer
 download https://downloads.wordpress.org/plugin/wordpress-importer.zip $TMPDIR/downloads/wordpress-importer.zip
 unzip -q $TMPDIR/downloads/wordpress-importer.zip -d  $TMPDIR/downloads/
-mkdir -p $TMPDIR/wordpress-importer
+mkdir -p $WORKING_DIR/tmp/wordpress-importer
 mv $TMPDIR/downloads/wordpress-importer/* $WORKING_DIR/tmp/wordpress-importer/
 
 # Install Jetpack
 download https://downloads.wordpress.org/plugin/jetpack.zip $TMPDIR/downloads/jetpack.zip
 unzip -q $TMPDIR/downloads/jetpack.zip -d  $TMPDIR/downloads/
-mkdir -p $TMPDIR/jetpack
+mkdir -p $WORKING_DIR/tmp/jetpack
 mv $TMPDIR/downloads/jetpack/* $WORKING_DIR/tmp/jetpack/
 
 #Install Yoast SEO
 download https://downloads.wordpress.org/plugin/wordpress-seo.zip $TMPDIR/downloads/wordpress-seo.zip
 unzip -q $TMPDIR/downloads/wordpress-seo.zip -d  $TMPDIR/downloads/
-mkdir -p $TMPDIR/wordpress-seo
+mkdir -p $WORKING_DIR/tmp/wordpress-seo
 mv $TMPDIR/downloads/wordpress-seo/* $WORKING_DIR/tmp/wordpress-seo/
 
 #Install Duplicate Post
 download https://downloads.wordpress.org/plugin/duplicate-post.zip $TMPDIR/downloads/duplicate-post.zip
 unzip -q $TMPDIR/downloads/duplicate-post.zip -d  $TMPDIR/downloads/
-mkdir -p $TMPDIR/duplicate-post
+mkdir -p $WORKING_DIR/tmp/duplicate-post
 mv $TMPDIR/downloads/duplicate-post/* $WORKING_DIR/tmp/duplicate-post/
 
