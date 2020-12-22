ce8dc05a0ef82d02fe92331ca17e7b26bc6b388e
raaaahman <sylvain@devindetails.com>
2020-12-22 14:08:58 +0100

    test(choose-lang): Refactor to parameterized tests

--- tests/phpunit/tests/test-choose-lang.php
+++ tests/phpunit/tests/test-choose-lang.php
@@ -24,7 +24,24 @@ class Choose_Lang_Test extends PLL_UnitTestCase {
 		$this->frontend = new PLL_Frontend( $links_model );
 	}
 
-	function test_browser_preferred_language() {
+	function accepted_languages_provider() {
+		return array(
+			array( 'fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3', 'en' ),
+			array( 'en-us;q=0.5,de-de', 'de' ),
+			array( 'de-de', 'de' ),
+			array( 'es-es,fr-fr;q=0.8', false ),
+			array( 'de;q=0.3,en;q=1', 'en' ),
+			array( 'de;q=0.3,en;q=1.0', 'en' ),
+			array( 'de;q=0,es-es;', false ),
+		);
+	}
+
+	/**
+	 * @dataProvider accepted_languages_provider
+	 * @param string      $accept_languages_header Accept-Language HTTP header like those issued by web browsers.
+	 * @param string|bool $expected_preference Expected results of our preferred browser language detection.
+	 */
+	function test_browser_preferred_language( $accept_languages_header, $expected_preference ) {
 		self::create_language( 'en_US' );
 		self::create_language( 'de_DE_formal' );
 		self::create_language( 'fr_FR' );
@@ -37,32 +54,27 @@ class Choose_Lang_Test extends PLL_UnitTestCase {
 
 		$choose_lang = new PLL_Choose_Lang_Url( $this->frontend );
 
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3';
-		$this->assertEquals( 'en', $choose_lang->get_preferred_browser_language() );
-
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en-us;q=0.5,de-de'; // just to test sorting
-		$this->assertEquals( 'de', $choose_lang->get_preferred_browser_language() );
-
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'de-de';
-		$this->assertEquals( 'de', $choose_lang->get_preferred_browser_language() );
-
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'es-es,fr-fr;q=0.8';
-		$this->assertFalse( $choose_lang->get_preferred_browser_language() );
-
-		// Bugs fixed in 2.4 with exotic values of q
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'de;q=0.3,en;q=1';
-		$this->assertEquals( 'en', $choose_lang->get_preferred_browser_language() );
-
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'de;q=0.3,en;q=1.0';
-		$this->assertEquals( 'en', $choose_lang->get_preferred_browser_language() );
-
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'de;q=0,es-es;';
-		$this->assertFalse( $choose_lang->get_preferred_browser_language() );
+		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = $accept_languages_header;
+		$this->assertEquals( $expected_preference, $choose_lang->get_preferred_browser_language() );
 	}
 
-	// bug fixed in 1.8
-	// see https://wordpress.org/support/topic/browser-detection
-	function test_browser_preferred_language_with_same_slug() {
+	function accepted_languages_with_same_slug_provider() {
+		return array(
+			array( 'en-gb;q=0.8,en-us;q=0.5,en;q=0.3', 'en' ),
+			array( 'en-us;q=0.8,en-gb;q=0.5,en;q=0.3', 'us' ),
+			array( 'en;q=0.8,fr;q=0.5', 'us' ),
+		);
+	}
+
+	/**
+	 * @since 1.8 Bugfix
+	 * @see https://wordpress.org/support/topic/browser-detection
+	 *
+	 * @dataProvider accepted_languages_with_same_slug_provider
+	 * @param string      $accept_languages_header Accept-Language HTTP header like those issued by web browsers.
+	 * @param string|bool $expected_preference Expected results of our preferred browser language detection.
+	 */
+	function test_browser_preferred_language_with_same_slug( $accept_languages_header, $expected_preference ) {
 		self::create_language( 'en_GB', array( 'term_group' => 2 ) );
 		self::create_language( 'en_US', array( 'slug' => 'us', 'term_group' => 1 ) );
 
@@ -76,22 +88,26 @@ class Choose_Lang_Test extends PLL_UnitTestCase {
 
 		$choose_lang = new PLL_Choose_Lang_Url( $this->frontend );
 
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en-gb;q=0.8,en-us;q=0.5,en;q=0.3';
-		$this->assertEquals( 'en', $choose_lang->get_preferred_browser_language() );
+		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = $accept_languages_header;
+		$this->assertEquals( $expected_preference, $choose_lang->get_preferred_browser_language() );
+	}
 
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en-us;q=0.8,en-gb;q=0.5,en;q=0.3';
-		$this->assertEquals( 'us', $choose_lang->get_preferred_browser_language() );
-
-		// when the exact locale is not specified, return the preferred language according to the order
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en;q=0.8,fr;q=0.5';
-		$this->assertEquals( 'us', $choose_lang->get_preferred_browser_language() );
+	function accepted_language_with_script_provider() {
+		return array(
+			array( 'zh-Hant-HK,zh-HK;q=0.8,zh;q=0.5', 'zh-hk' ),
+			array( 'zh-Hant-HK', 'zh-hk' ),
+		);
 	}
 
 	/**
-	 * @since 3.0
+	 * @since 3.0 Bugifx
 	 * @see https://github.com/polylang/polylang/issues/591
+	 *
+	 * @dataProvider accepted_language_with_script_provider
+	 * @param string      $accept_languages_header Accept-Language HTTP header like those issued by web browsers.
+	 * @param string|bool $expected_preference Expected results of our preferred browser language detection.
 	 */
-	function test_browser_preferred_language_with_script_tag() {
+	function test_browser_preferred_language_with_script_tag( $accept_languages_header, $expected_preference ) {
 		self::create_language( 'zh_CN', array( 'slug' => 'zh' ) );
 		self::create_language( 'zh_HK', array( 'slug' => 'zh-hk' ) );
 		self::create_language( 'zh_HK', array( 'slug' => 'zh-hant-hk' ) );
@@ -107,7 +123,7 @@ class Choose_Lang_Test extends PLL_UnitTestCase {
 
 		$choose_lang = new PLL_Choose_Lang_Url( self::$polylang );
 
-		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'zh-Hant-HK,zh-HK;q=0.8,zh;q=0.5';
-		$this->assertEquals( 'zh-hk', $choose_lang->get_preferred_browser_language() );
+		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = $accept_languages_header;
+		$this->assertEquals( $expected_preference, $choose_lang->get_preferred_browser_language() );
 	}
 }
