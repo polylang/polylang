1d39412ae2f4b7c508e99fa58f3697735fd085c2
raaaahman <sylvain@devindetails.com>
2020-12-24 11:07:05 +0100

    tool(webpack): Extract functions from encapsulating function
    Also remove duplication in .gitignore

--- webpack/FileNames.js
+++ webpack/FileNames.js
@@ -31,39 +31,48 @@ function getJsFileNamesEntries( jsFoldersToIgnore , jsFileNamesToIgnore ) {
 	).map( filename => `./${ filename }` );
 	console.log( 'js files to minify:', jsFileNames );
 
-	function computeBuildFilename( filename, suffix ) {
-		const nameWithoutSuffix = path.parse( filename ).name.split( '.' )[0];
-		suffix = suffix ? '.' + suffix : '';
-
-		return `${ path.parse( filename ).dir }/${ nameWithoutSuffix + suffix }.js`; // phpcs:ignore WordPress.WhiteSpace.OperatorSpacing
-	}
-
-	// Prepare webpack configuration to minify js files to source folder as target folder and suffix file name with .min.js extension.
-	function mapJsFiles( jsFileNames, minimize = false ) {
-		return jsFileNames.map( 
-			( filename ) => {
-				const entry = {};
-				entry[ path.parse( filename ).name ] = filename;
-				const output = {
-					filename: computeBuildFilename( filename, minimize ? 'min' : '' ),
-					path: process.cwd(),
-					iife: false, // Avoid Webpack to wrap files into a IIFE which is not needed for this kind of javascript files.
-				};
-				const config = {
-					entry: entry,
-					output: output,
-					optimization: { minimize: minimize }
-				};
-				return config;
-			},
-			{}
-		);
-	}
 	const jsFileNamesEntries = [
 		...mapJsFiles( [ ...jsFileNames, ...jsSourceFileNames ], true ),
 		...mapJsFiles( jsSourceFileNames )
 	];
 	return jsFileNamesEntries;
+}
+
+/**
+ * @param {string} filename Source file's path and name.
+ * @param {string} suffix To add before file extension.
+ */
+function computeBuildFilename( filename, suffix ) {
+	const nameWithoutSuffix = path.parse( filename ).name.split( '.' )[0];
+	suffix = suffix ? '.' + suffix : '';
+
+	return `${ path.parse( filename ).dir }/${ nameWithoutSuffix + suffix }.js`; // phpcs:ignore WordPress.WhiteSpace.OperatorSpacing
+}
+
+/**
+ * Prepare webpack configuration to minify js files to source folder as target folder and suffix file name with .min.js extension.
+ * @param {string[]} jsFileNames Source files to build.
+ * @param {boolean} minimize True to generate minified files.
+ */
+ function mapJsFiles( jsFileNames, minimize = false ) {
+	return jsFileNames.map( 
+		( filename ) => {
+			const entry = {};
+			entry[ path.parse( filename ).name ] = filename;
+			const output = {
+				filename: computeBuildFilename( filename, minimize ? 'min' : '' ),
+				path: process.cwd(),
+				iife: false, // Avoid Webpack to wrap files into a IIFE which is not needed for this kind of javascript files.
+			};
+			const config = {
+				entry: entry,
+				output: output,
+				optimization: { minimize: minimize }
+			};
+			return config;
+		},
+		{}
+	);
 }
 
 module.exports = getJsFileNamesEntries;
