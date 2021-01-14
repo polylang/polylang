2579ef588618715ddf75df47c53ff99d29196743
raaaahman <sylvain@devindetails.com>
2021-01-14 16:21:29 +0100

    tool(webpack): (Fix) Import build tools from package

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,7 @@
 
 const path = require( 'path' );
 const glob = require( 'glob' ).sync;
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const CssMinimizerPlugin = require( 'css-minimizer-webpack-plugin' );
-const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
-
-/**
- * Internal dependencies
- */
-
- const getJsFileNamesEntries = require( './webpack/FileNames' );
+const { transformJsEntry, transformCssEntry } = require( '@wpsyntex/polylang-build-scripts' );
 
 function configureWebpack( options ){
 	const mode = options.mode;
@@ -30,69 +22,31 @@ function configureWebpack( options ){
 		'vendor/**',
 		'tmp/**',
 		'webpack/**',
-		'js/build/**',
-		'js/lib/**',
+		'**/build/**',
 	];
 
 	const jsFileNamesToIgnore = [
+		'js/lib/**',
 		'*.config.js',
 		'*.min.js',
 	];
-	
-	const jsFileNamesEntries = getJsFileNamesEntries( commonFoldersToIgnore, jsFileNamesToIgnore );
+
+	const jsFileNames = glob( '**/*.js', { 'ignore': [ ...commonFoldersToIgnore, ...jsFileNamesToIgnore ] } ).map( filename => `./${ filename }`);
+	console.log( 'js files to minify:', jsFileNames );
+
+	const jsFileNamesEntries = [
+		...jsFileNames.map( transformJsEntry( path.resolve( __dirname ), true ) ),
+		...jsFileNames.map( transformJsEntry( path.resolve( __dirname ), false ) )
+	]
 
 	const cssFileNames = glob( '**/*.css', { 'ignore': [ ...commonFoldersToIgnore, '**/*.min.css' ] } ).map( filename => `./${ filename }`);
 	console.log( 'css files to minify:', cssFileNames );
 
 	// Prepare webpack configuration to minify css files to source folder as target folder and suffix file name with .min.js extension.
-	const cssFileNamesEntries = cssFileNames.map( ( filename ) => {
-			const entry = {};
-			entry[ path.parse( filename ).name ] = filename;
-			const output = {
-				filename: `${path.parse( filename ).dir}/[name].work`,
-				path: path.resolve( __dirname ), // Output folder as project root to put files in the same folder as source files.
-			}
-			const config = {
-				entry: entry,
-				output: output,
-				plugins: [
-					new MiniCssExtractPlugin(
-						{
-							filename: `${path.parse( filename ).dir}/[name].min.css`
-						}
-					),
-					new CleanWebpackPlugin(
-						{
-							dry: false,
-							verbose: false,
-							cleanOnceBeforeBuildPatterns: [], // Disable to clean nothing before build.
-							cleanAfterEveryBuildPatterns: [
-								'**/*.work',
-								'**/*.LICENSE.txt'
-							],
-						}
-					)
-				],
-				module: {
-					rules: [
-						{
-							test: /\.css$/i,
-							use: [ MiniCssExtractPlugin.loader, 'css-loader'],
-						},
-					],
-				},
-				devtool: ! isProduction ? 'source-map' : false,
-				optimization: {
-					minimize: true,
-					minimizer: [
-						new CssMinimizerPlugin(),
-					],
-				},
-			};
-			return config;
-		},
-		{}
-	);
+	const cssFileNamesEntries = [
+		...cssFileNames.map( transformCssEntry( path.resolve( __dirname ), isProduction, true ) ),
+		...cssFileNames.map( transformCssEntry( path.resolve( __dirname ), isProduction, false ) )
+	];
 
 	// Make webpack configuration.
 	const config = [
