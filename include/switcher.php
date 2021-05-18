69fc8556bf13ddc8e6e6002d3a7c3b32df45e338
raaaahman <sylvain@devindetails.com>
2021-05-18 10:13:39 +0200

    feat(switcher): Use classes passed in the args

--- include/switcher.php
+++ include/switcher.php
@@ -126,7 +126,13 @@ class PLL_Switcher {
 			$order = (int) $language->term_group;
 			$slug = $language->slug;
 			$locale = $language->get_locale( 'display' );
-			$classes = array( 'lang-item', 'lang-item-' . $id, 'lang-item-' . esc_attr( $slug ) );
+			$item_classes = array( 'lang-item', 'lang-item-' . $id, 'lang-item-' . esc_attr( $slug ) );
+			$classes = isset( $args['classes'] ) && is_array( $args['classes' ] ) ?
+				array_merge(
+					$item_classes,
+					$args['classes']
+				) :
+				$item_classes;
 			$link_classes = isset( $args['link_classes'] ) ? $args['link_classes'] : array();
 			$current_lang = $this->get_current_language( $args ) === $slug;
 
