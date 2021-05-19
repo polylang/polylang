9644997f9a6444ed28ff7152a98161426c2a8c1c
Frédéric Demarle <frederic.demarle@yahoo.fr>
2021-05-19 15:20:22 +0200

    Remove the WPML compatibility settings module

--- modules/wpml/load.php
+++ modules/wpml/load.php
@@ -6,20 +6,12 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Don't access directly
+	exit; // Don't access directly.
 };
 
 if ( $polylang->model->get_languages_list() ) {
 	if ( ! defined( 'PLL_WPML_COMPAT' ) || PLL_WPML_COMPAT ) {
-		PLL_WPML_Compat::instance(); // WPML API
-		PLL_WPML_Config::instance(); // wpml-config.xml
+		PLL_WPML_Compat::instance(); // WPML API.
+		PLL_WPML_Config::instance(); // wpml-config.xml.
 	}
-
-	add_filter(
-		'pll_settings_modules',
-		function( $modules ) {
-			$modules[] = 'PLL_Settings_WPML';
-			return $modules;
-		}
-	);
 }
