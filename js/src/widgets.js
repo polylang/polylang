ac5f5da108d960e7d8ce6394cee0d866312bc4bb
Emmanuel Hesry <ehesry@gmail.com>
2021-05-21 14:49:02 +0200

    Move flag initialisation and condition to make it in the block-based widgets screen

--- js/src/widgets.js
+++ js/src/widgets.js
@@ -46,62 +46,62 @@ jQuery(
 			}
 		}
 
-		if ( 'undefined' !== typeof wp.customize ) {
-
-			widgets_container = $( '#customize-controls' );
-			widgets_selector  = '.customize-control .widget';
-
-			/**
-			 * WP Customizer add control listener.
-			 *
-			 * @link https://wordpress.stackexchange.com/questions/256536/callback-after-wordpress-customizer-complete-loading
-			 *
-			 * @param {object} control The control type.
-			 * @return {void} Nothing.
-			 */
-			function customize_add_flag( control ) {
-				if ( ! control.extended( wp.customize.Widgets.WidgetControl ) ) {
-					return;
-				}
-
-				/*
-				* Make sure the widget's contents are embedded; normally this is done
-				* when the control is expanded, for DOM performance reasons.
-				*/
-				control.embedWidgetContent();
-
-				// Now we know for sure the widget is fully embedded.
-				add_flag( control.container.find( '.widget' ) );
-			}
-			wp.customize.control.each( customize_add_flag );
-			wp.customize.control.bind( 'add', customize_add_flag );
-
-		} else if ( isBlockEditor ) {
+		if ( isBlockEditor ) {
 
 			widgets_container = $( '.edit-widgets-main-block-list' );
 			widgets_selector  = '.widget';
 
-		} else {
-
-			widgets_container = $( '#widgets-right' );
-			widgets_selector  = '.widget';
-
-		}
-
-		if ( ! isBlockEditor ) {
-			// Add flags on load.
-			$( widgets_selector, widgets_container ).each(
-				function() {
-					add_flag( this );
-				}
-			);
-		} else {
 			// Update flags when we click on the legacy widget to display its form.
 			widgets_container.on(
 				'click',
 				'.wp-block-legacy-widget',
 				function() {
 					add_flag( $( this ).find( '.widget' ) );
+				}
+			);
+
+		} else {
+			if ( 'undefined' !== typeof wp.customize ) {
+
+				widgets_container = $( '#customize-controls' );
+				widgets_selector  = '.customize-control .widget';
+
+				/**
+				 * WP Customizer add control listener.
+				 *
+				 * @link https://wordpress.stackexchange.com/questions/256536/callback-after-wordpress-customizer-complete-loading
+				 *
+				 * @param {object} control The control type.
+				 * @return {void} Nothing.
+				 */
+				function customize_add_flag( control ) {
+					if ( ! control.extended( wp.customize.Widgets.WidgetControl ) ) {
+						return;
+					}
+
+					/*
+					* Make sure the widget's contents are embedded; normally this is done
+					* when the control is expanded, for DOM performance reasons.
+					*/
+					control.embedWidgetContent();
+
+					// Now we know for sure the widget is fully embedded.
+					add_flag( control.container.find( '.widget' ) );
+				}
+				wp.customize.control.each( customize_add_flag );
+				wp.customize.control.bind( 'add', customize_add_flag );
+
+			} else {
+
+				widgets_container = $( '#widgets-right' );
+				widgets_selector  = '.widget';
+
+			}
+
+			// Add flags on load.
+			$( widgets_selector, widgets_container ).each(
+				function() {
+					add_flag( this );
 				}
 			);
 		}
