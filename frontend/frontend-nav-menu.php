838c90f1b6c416b1b3a60d2b0d3e067f308a00a6
Hug0-Drelon <hugo.drelon@gmail.com>
2021-06-02 16:10:42 +0200

    move back index in loop

--- frontend/frontend-nav-menu.php
+++ frontend/frontend-nav-menu.php
@@ -101,8 +101,8 @@ class PLL_Frontend_Nav_Menu extends PLL_Nav_Menu {
 		$offset = 0;
 
 		foreach ( $items as $item ) {
-			$i = 0;
 			if ( $options = get_post_meta( $item->ID, '_pll_menu_item', true ) ) {
+				$i = 0;
 
 				/** This filter is documented in include/switcher.php */
 				$options = apply_filters( 'pll_the_languages_args', $options ); // Honor the filter here for 'show_flags', 'show_names' and 'dropdown'.
