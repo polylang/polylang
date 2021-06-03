df38abbf72936f558a776f4fa47a4f927436261f
raaaahman <sylvain@devindetails.com>
2021-06-03 15:32:03 +0200

    refactor(admin): Coding standards

--- admin/block-editor-filter-preload-paths.php
+++ admin/block-editor-filter-preload-paths.php
@@ -6,8 +6,8 @@
 /**
  * Class PLL_Block_Editor_Filter_Preload_Paths
  */
-class PLL_Block_Editor_Filter_Preload_Paths
-{
+class PLL_Block_Editor_Filter_Preload_Paths {
+
 
 	/**
 	 * @var int
@@ -28,31 +28,28 @@ class PLL_Block_Editor_Filter_Preload_Paths
 	 * @param int      $priority         Priority of execution for the given callback. Default 10.
 	 * @param int      $arguments_number Number of arguments to pass to the callback. Default 1.
 	 */
-	public function __construct( $callback, $priority = 10, $arguments_number = 1 )
-	{
+	public function __construct( $callback, $priority = 10, $arguments_number = 1 ) {
 		$this->callback = $callback;
 		$this->arguments_number = $arguments_number;
 
 		if ( version_compare( $GLOBALS['wp_version'], '5.8-alpha', '<' ) ) {
 			add_filter( 'block_editor_preload_paths', $callback, $priority, $arguments_number );
 		} else {
-			add_filter( 'block_editor_rest_api_preload_paths', array( $this, 'block_editor_rest_api_preload_paths' ), $priority, 2);
+			add_filter( 'block_editor_rest_api_preload_paths', array( $this, 'block_editor_rest_api_preload_paths' ), $priority, 2 );
 		}
 	}
 
 	/**
-	* Filters the preload REST requests by the current language of the post
-	* Necessary otherwise subsequent REST requests filtered by the language
-	* would not hit the preloaded requests
-	*
- 	* @since 3.1
- 	* @param $preload_paths
-	* @param WP_Block_Editor_Context $block_editor_context The post resource data.
-	* @return array|mixed|string[] (string|string[])[]
-	*
-	*/
-	public function block_editor_rest_api_preload_paths( $preload_paths, $block_editor_context )
-	{
+	 * Filters the preload REST requests by the current language of the post
+	 * Necessary otherwise subsequent REST requests filtered by the language
+	 * would not hit the preloaded requests
+	 *
+	 * @since 3.1
+	 * @param string[]                $preload_paths        The preload paths loaded by the Block Editor.
+	 * @param WP_Block_Editor_Context $block_editor_context The post resource data.
+	 * @return array|mixed|string[] (string|string[])[]
+	 */
+	public function block_editor_rest_api_preload_paths( $preload_paths, $block_editor_context ) {
 		if ( $this->arguments_number > 1 && null === $block_editor_context->post ) {
 			return $preload_paths;
 		}
