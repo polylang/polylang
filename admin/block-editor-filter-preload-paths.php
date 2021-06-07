aa4995b023fe68c7034756248cfc2d283138baec
raaaahman <sylvain@devindetails.com>
2021-06-07 14:43:06 +0200

    refactor(admin): Preload paths check for the existence of WP_Block_Editor_Context

--- admin/block-editor-filter-preload-paths.php
+++ admin/block-editor-filter-preload-paths.php
@@ -30,12 +30,11 @@ class PLL_Block_Editor_Filter_Preload_Paths {
 	 */
 	public function __construct( $callback, $priority = 10, $arguments_number = 1 ) {
 		$this->callback = $callback;
-		$this->arguments_number = $arguments_number;
 
-		if ( version_compare( $GLOBALS['wp_version'], '5.8-alpha', '<' ) ) {
-			add_filter( 'block_editor_preload_paths', $callback, $priority, $arguments_number );
+		if ( class_exists( 'WP_Block_Editor_Context' ) ) {
+			add_filter( 'block_editor_rest_api_preload_paths', array( $this, 'block_editor_rest_api_preload_paths' ), $priority, $arguments_number );
 		} else {
-			add_filter( 'block_editor_rest_api_preload_paths', array( $this, 'block_editor_rest_api_preload_paths' ), $priority, 2 );
+			add_filter( 'block_editor_preload_paths', $callback, $priority, $arguments_number );
 		}
 	}
 
@@ -49,13 +48,8 @@ class PLL_Block_Editor_Filter_Preload_Paths {
 	 * @param WP_Block_Editor_Context $block_editor_context The post resource data.
 	 * @return array|mixed|string[] (string|string[])[]
 	 */
-	public function block_editor_rest_api_preload_paths( $preload_paths, $block_editor_context ) {
-		if ( $this->arguments_number > 1 && null === $block_editor_context->post ) {
-			return $preload_paths;
-		}
-
-		// Only two cases: one or two arguments
-		$args = 1 === $this->arguments_number ? array( $preload_paths ) : array( $preload_paths, $block_editor_context->post );
+	public function block_editor_rest_api_preload_paths( $preload_paths, $block_editor_context = null ) {
+		$args = null === $block_editor_context ? array( $preload_paths ) : array( $preload_paths, $block_editor_context->post );
 
 		return call_user_func_array( $this->callback, $args );
 	}
