79491f839867863c30826201a9107331b3ed1b1d
Chouby <frederic.demarle@yahoo.fr>
2021-06-07 10:13:42 +0200

    Fix wp screen notice (#856)
    * Fix Trying to get property 'base' of non-object
    
    * Remove wrong current screen from ajax test
    
    * Add test to check that the columns are not added in term.php

--- admin/admin-filters-columns.php
+++ admin/admin-filters-columns.php
@@ -249,11 +249,12 @@ class PLL_Admin_Filters_Columns {
 	public function add_term_column( $columns ) {
 		$screen = get_current_screen();
 
-		if ( 'edit-tags' === $screen->base ) { // Don't add our columns when editing a term.
-			return $this->add_column( $columns, 'posts' );
+		// Avoid displaying languages in screen options when editing a term.
+		if ( $screen instanceof WP_Screen && 'term' === $screen->base ) {
+			return $columns;
 		}
 
-		return $columns;
+		return $this->add_column( $columns, 'posts' );
 	}
 
 	/**
