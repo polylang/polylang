8769b896a8e8047c449f45b3e887fb7bcdb16a24
Git I Hate You <git@screenfeed.fr>
2021-06-22 11:57:07 +0200

    Added integration tests

---
+++ tests/phpunit/tests/test-rest-request.php
@@ -0,0 +1,89 @@
+<?php
+
+class Rest_Request_Test extends PLL_UnitTestCase {
+	/**
+	 * @var string
+	 */
+	public $structure = '/%postname%/';
+
+	/**
+	 * Initialization before all tests run.
+	 *
+	 * @param  WP_UnitTest_Factory $factory WP_UnitTest_Factory object.
+	 * @return void
+	 */
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		parent::wpSetUpBeforeClass( $factory );
+
+		self::create_language( 'en_US' );
+		self::create_language( 'fr_FR' );
+	}
+
+	/**
+	 * @return void
+	 */
+	public function setUp() {
+		global $wp_rewrite;
+
+		parent::setUp();
+
+		$wp_rewrite->extra_rules_top = array();
+		$wp_rewrite->set_permalink_structure( $this->structure );
+		$wp_rewrite->flush_rules();
+
+		$links_model         = self::$model->get_links_model();
+		$this->frontend      = new PLL_REST_Request( $links_model );
+		$GLOBALS['polylang'] = &$this->frontend;
+	}
+
+	/**
+	 * @return void
+	 */
+	public function tearDown() {
+		parent::tearDown();
+
+		unset( $GLOBALS['polylang'] );
+	}
+
+	public function test_should_define_language_when_language_is_valid() {
+		self::$model->options['default_lang'] = 'en';
+
+		$_REQUEST['lang'] = 'fr';
+
+		$this->frontend->init();
+
+		$this->assertInstanceOf( 'PLL_Language', $this->frontend->curlang );
+		$this->assertSame( 'fr', $this->frontend->curlang->slug );
+	}
+
+	public function test_should_define_default_language_when_language_is_invalid() {
+		self::$model->options['default_lang'] = 'en';
+
+		$_REQUEST['lang'] = 'it';
+
+		$this->frontend->init();
+
+		$this->assertInstanceOf( 'PLL_Language', $this->frontend->curlang );
+		$this->assertSame( 'en', $this->frontend->curlang->slug );
+	}
+
+	public function test_should_not_define_default_language_when_default_language_is_invalid() {
+		self::$model->options['default_lang'] = 'es';
+
+		$_REQUEST['lang'] = 'it';
+
+		$this->frontend->init();
+
+		$this->assertFalse( $this->frontend->curlang );
+	}
+
+	public function test_should_not_define_language_when_not_sent() {
+		self::$model->options['default_lang'] = 'en';
+
+		unset( $_REQUEST['lang'] );
+
+		$this->frontend->init();
+
+		$this->assertNull( $this->frontend->curlang );
+	}
+}
