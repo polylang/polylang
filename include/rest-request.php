708f942786a60eac8282636afcd98eaf270bbcab
Git I Hate You <git@screenfeed.fr>
2021-06-22 10:56:08 +0200

    Defined a current language on REST request If a non-empty `lang` query arg is sent during a REST request, define the current language (fall back to default language if invalid). Don't define a language if no `lang` are sent.

--- include/rest-request.php
+++ include/rest-request.php
@@ -9,34 +9,39 @@
  * @since 2.6
  */
 class PLL_REST_Request extends PLL_Base {
+	/**
+	 * @var PLL_Language|false|null A `PLL_Language` when defined, `false` otherwise. `null` until the language
+	 *                              definition process runs.
+	 */
+	public $curlang;
 
 	/**
-	 * @var PLL_Filters
+	 * @var PLL_Filters|null
 	 */
 	public $filters;
 
 	/**
-	 * @var PLL_Filters_Links
+	 * @var PLL_Filters_Links|null
 	 */
 	public $filters_links;
 
 	/**
-	 * @var PLL_Admin_Links
+	 * @var PLL_Admin_Links|null
 	 */
 	public $links;
 
 	/**
-	 * @var PLL_Nav_Menu
+	 * @var PLL_Nav_Menu|null
 	 */
 	public $nav_menu;
 
 	/**
-	 * @var PLL_Static_Pages
+	 * @var PLL_Static_Pages|null
 	 */
 	public $static_pages;
 
 	/**
-	 * @var PLL_Filters_Widgets_Options
+	 * @var PLL_Filters_Widgets_Options|null
 	 */
 	public $filters_widgets_options;
 
@@ -50,23 +55,41 @@ class PLL_REST_Request extends PLL_Base {
 	public function init() {
 		parent::init();
 
-		if ( $this->model->get_languages_list() ) {
+		if ( ! $this->model->get_languages_list() ) {
+			return;
+		}
 
-			/** This action is documented in include/class-polylang.php */
-			do_action( 'pll_no_language_defined' ); // To load overridden textdomains.
-
-			$this->filters_links = new PLL_Filters_Links( $this );
-			$this->filters = new PLL_Filters( $this );
-			$this->filters_widgets_options = new PLL_Filters_Widgets_Options( $this );
-
-			// Static front page and page for posts.
-			if ( 'page' === get_option( 'show_on_front' ) ) {
-				$this->static_pages = new PLL_Static_Pages( $this );
+		if ( ! empty( $_REQUEST['lang'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification
+			if ( is_string( $_REQUEST['lang'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification
+				$this->curlang = $this->model->get_language( sanitize_key( $_REQUEST['lang'] ) ); // phpcs:ignore WordPress.Security.NonceVerification
 			}
 
-			$this->links = new PLL_Admin_Links( $this );
-
-			$this->nav_menu = new PLL_Nav_Menu( $this ); // For auto added pages to menu.
+			if ( empty( $this->curlang ) && ! empty( $this->options['default_lang'] ) && is_string( $this->options['default_lang'] ) ) {
+				// A lang has been requested but it is invalid, let's fall back to the default one.
+				$this->curlang = $this->model->get_language( sanitize_key( $this->options['default_lang'] ) );
+			}
 		}
+
+		if ( ! empty( $this->curlang ) ) {
+			$GLOBALS['text_direction'] = $this->curlang->is_rtl ? 'rtl' : 'ltr';
+
+			/** This action is documented in frontend/choose-lang.php */
+			do_action( 'pll_language_defined', $this->curlang->slug, $this->curlang );
+		} else {
+			/** This action is documented in include/class-polylang.php */
+			do_action( 'pll_no_language_defined' ); // To load overridden textdomains.
+		}
+
+		$this->filters_links           = new PLL_Filters_Links( $this );
+		$this->filters                 = new PLL_Filters( $this );
+		$this->filters_widgets_options = new PLL_Filters_Widgets_Options( $this );
+
+		// Static front page and page for posts.
+		if ( 'page' === get_option( 'show_on_front' ) ) {
+			$this->static_pages = new PLL_Static_Pages( $this );
+		}
+
+		$this->links    = new PLL_Admin_Links( $this );
+		$this->nav_menu = new PLL_Nav_Menu( $this ); // For auto added pages to menu.
 	}
 }
