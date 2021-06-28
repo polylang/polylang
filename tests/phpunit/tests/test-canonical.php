247a3e3f3036ba3f5fb78245ba59d56d7508f8cd
Hug0-Drelon <hugo.drelon@gmail.com>
2021-06-28 19:35:17 +0200

    create test

--- tests/phpunit/tests/test-canonical.php
+++ tests/phpunit/tests/test-canonical.php
@@ -459,4 +459,26 @@ class Canonical_Test extends PLL_Canonical_UnitTestCase {
 			)
 		);
 	}
+
+	public function test_category_feed() {
+		// Create an untranslated category.
+		$cat_id = $this->factory->category->create(
+			array(
+				'name' => 'Test Category',
+				'slug' => 'test-category',
+				)
+		);
+		self::$model->term->set_language( $cat_id, 'en' );
+		// Create a post with the previous category.
+		$post_id = $this->factory->post->create(
+			array(
+				'post_title' => 'test',
+				'post_type'  => 'page',
+				'category'   => $cat_id,
+			)
+		);
+		self::$model->post->set_language( $post_id, 'en' );
+
+		$this->assertCanonical( '/fr/category/test-category/feed/', '/en/category/test-category/feed/' );
+	}
 }
