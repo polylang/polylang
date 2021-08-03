60b9d6b8966ef34dd33d42fdf929c45eecd95296
Hug0-Drelon <hugo.drelon@gmail.com>
2021-08-03 11:44:31 +0200

    merge tests for tax and feed queries

--- frontend/frontend-filters-links.php
+++ frontend/frontend-filters-links.php
@@ -392,26 +392,18 @@ class PLL_Frontend_Filters_Links extends PLL_Filters_Links {
 			}
 		}
 
-		elseif ( is_feed() && ( is_category() || is_tag() || is_tax() ) ) {
+		elseif ( is_category() || is_tag() || is_tax() ) {
 			if ( $this->model->is_translated_taxonomy( $this->get_queried_taxonomy( $this->wp_query()->tax_query ) ) ) {
-				$term_id = $this->get_queried_term_id( $this->wp_query()->tax_query );
-				$language = $this->model->term->get_language( $term_id );
-				if ( $this->links_model->using_permalinks ) {
-					$redirect_url = $this->maybe_add_page_to_redirect_url( get_term_feed_link( $term_id, '' ) );
+				if ( $this->links_model->using_permalinks && ( ! empty( $this->wp_query()->query['cat'] ) || ! empty( $this->wp_query()->query['tag'] ) ) ) {
+					// When we receive a plain permalink with a cat or tag query var, we need to redirect to the pretty permalink.
+					$term_id = $this->get_queried_term_id( $this->wp_query()->tax_query );
+					if( is_feed() ) {
+						$redirect_url = $this->maybe_add_page_to_redirect_url( get_term_feed_link( $term_id, '' ) );
+					} else {
+						$redirect_url = $this->maybe_add_page_to_redirect_url( get_term_link( $term_id ) );
+					}
 				}
 			}
-		}
-
-		elseif ( $this->links_model->using_permalinks && ( is_category() || is_tag() || is_tax() ) && ( ! empty( $this->wp_query()->query['cat'] ) || ! empty( $this->wp_query()->query['tag'] ) ) ) {
-			// When we receive a plain permalink with a cat or tag query var, we need to redirect to the pretty permalink.
-			if ( $this->model->is_translated_taxonomy( $this->get_queried_taxonomy( $this->wp_query()->tax_query ) ) ) {
-				$term_id = $this->get_queried_term_id( $this->wp_query()->tax_query );
-				$language = $this->model->term->get_language( $term_id );
-				$redirect_url = $this->maybe_add_page_to_redirect_url( get_term_link( $term_id ) );
-			}
-		}
-
-		elseif ( is_category() || is_tag() || is_tax() ) {
 			// We need to switch the language when there is no language provided in a pretty permalink.
 			$obj = get_queried_object();
 			if ( ! empty( $obj ) && $this->model->is_translated_taxonomy( $obj->taxonomy ) ) {
