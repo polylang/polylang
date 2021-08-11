091dbc62cec2127fbbbe67d25f1f2f921e81e518
Chouby <frederic.demarle@yahoo.fr>
2021-08-11 10:30:14 +0200

    Allow to remove the cookie with the pll_cookie expiration filter (#905)

--- integrations/cache/cache-compat.php
+++ integrations/cache/cache-compat.php
@@ -45,9 +45,9 @@ class PLL_Cache_Compat {
 		$samesite = ( 3 === PLL()->options['force_lang'] ) ? 'None' : 'Lax';
 
 		/** This filter is documented in include/cookie.php */
-		$expiration = apply_filters( 'pll_cookie_expiration', YEAR_IN_SECONDS );
+		$expiration = (int) apply_filters( 'pll_cookie_expiration', YEAR_IN_SECONDS );
 
-		if ( $expiration > 0 ) {
+		if ( 0 !== $expiration ) {
 			$format = 'var expirationDate = new Date();
 				expirationDate.setTime( expirationDate.getTime() + %7$d * 1000 );
 				document.cookie = "%1$s=%2$s; expires=" + expirationDate.toUTCString() + "; path=%3$s%4$s%5$s%6$s";';
