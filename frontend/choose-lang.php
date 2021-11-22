61c68afeb7ffa82a26c1ad4707a853f71f1c00e5
Dominik Schilling <dominikschilling+git@gmail.com>
2021-11-22 21:26:46 +0100

    Set to 404 only when searching an empty string

--- frontend/choose-lang.php
+++ frontend/choose-lang.php
@@ -313,8 +313,14 @@ abstract class PLL_Choose_Lang {
 		elseif ( ( count( $query->query ) == 1 || ( is_paged() && count( $query->query ) == 2 ) || ( isset( $query->query['s'] ) && ! $query->query['s'] ) ) && $lang = get_query_var( 'lang' ) ) {
 			$lang = $this->model->get_language( $lang );
 			$this->set_language( $lang ); // sets the language now otherwise it will be too late to filter sticky posts !
-			$query->is_home = $query->is_404 = true;
-			$query->is_archive = $query->is_tax = $query->is_page = $query->is_singular = false;
+			$query->is_home = true;
+			$query->is_archive = $query->is_tax = false;
+
+			// Also set to 404 when searching an empty string.
+			if ( isset( $query->query['s'] ) && ! $query->query['s'] ) {
+				$query->is_404 = true;
+				$query->is_page = $query->is_singular = false;
+			}
 		}
 	}
 
