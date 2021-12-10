5e39c2f1a82b5b84350ceffc9770e9577dd8ed1e
Chouby <frederic.demarle@yahoo.fr>
2021-12-10 13:48:57 +0100

    Add Yoast SEO columns when updating the post list table rows (#943)

--- integrations/wpseo/wpseo.php
+++ integrations/wpseo/wpseo.php
@@ -50,6 +50,11 @@ class PLL_WPSEO {
 		} else {
 			add_filter( 'pll_copy_post_metas', array( $this, 'copy_post_metas' ), 10, 2 );
 			add_filter( 'pll_translate_post_meta', array( $this, 'translate_post_meta' ), 10, 3 );
+
+			// Yoast SEO adds the columns hooks only for the 'inline-save' action. We need them for 'pll_update_post_rows' too.
+			if ( wp_doing_ajax() && isset( $_POST['action'] ) && 'pll_update_post_rows' === $_POST['action'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
+				$GLOBALS['wpseo_meta_columns'] = new WPSEO_Meta_Columns();
+			}
 		}
 	}
 
