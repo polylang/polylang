37c490daadc0e12557f5f01c70a116f67de1fca1
Marianne <marianne.honczaruk@gmail.com>
2022-02-09 13:18:36 +0100

    Clean language cache on language creation, and check variable isset to avoid undefined index

--- include/model.php
+++ include/model.php
@@ -58,6 +58,7 @@ class PLL_Model {
 		add_action( 'update_option_permalink_structure', array( $this, 'clean_languages_cache' ) );
 		add_action( 'update_option_siteurl', array( $this, 'clean_languages_cache' ) );
 		add_action( 'update_option_home', array( $this, 'clean_languages_cache' ) );
+		add_action( 'create_language', array( $this, 'clean_languages_cache' ) );
 
 		add_filter( 'get_terms_args', array( $this, 'get_terms_args' ) );
 
@@ -92,7 +93,8 @@ class PLL_Model {
 
 				if ( ! empty( $languages ) && ! empty( $term_languages ) ) {
 					foreach ( $languages as $k => $v ) {
-						$languages[ $k ] = new PLL_Language( $v, $term_languages[ 'pll_' . $v->slug ] );
+						$term_language   = isset( $term_languages[ 'pll_' . $v->slug ] ) ? $term_languages[ 'pll_' . $v->slug ] : null;
+						$languages[ $k ] = new PLL_Language( $v, $term_language );
 					}
 
 					// We will need the languages list to allow its access in the filter below.
