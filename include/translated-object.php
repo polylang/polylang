7701e70b9e02b28428a6ad016afd98f3018414d1
Chouby <frederic.demarle@yahoo.fr>
2022-02-15 18:17:41 +0100

    Improve performance of the get_pages() filter (#980)

--- include/translated-object.php
+++ include/translated-object.php
@@ -398,37 +398,6 @@ abstract class PLL_Translated_Object {
 	}
 
 	/**
-	 * Returns ids of objects in a language similarly to get_objects_in_term() for a taxonomy.
-	 * It is faster than get_objects_in_term() as it avoids a JOIN.
-	 *
-	 * @since 1.4
-	 *
-	 * @param PLL_Language $lang PLL_Language object.
-	 * @return int[] Object ids.
-	 */
-	public function get_objects_in_language( $lang ) {
-		global $wpdb;
-		$tt_id = $this->tax_tt;
-
-		$last_changed = wp_cache_get_last_changed( 'terms' );
-		$cache_key    = "polylang:get_objects_in_language:{$lang->$tt_id}:{$last_changed}";
-		$cache        = wp_cache_get( $cache_key, 'terms' );
-
-		if ( false === $cache ) {
-			$object_ids = $wpdb->get_col( $wpdb->prepare( "SELECT object_id FROM $wpdb->term_relationships WHERE term_taxonomy_id = %d", $lang->$tt_id ) );
-			wp_cache_set( $cache_key, $object_ids, 'terms' );
-		} else {
-			$object_ids = (array) $cache;
-		}
-
-		if ( ! $object_ids ) {
-			return array();
-		}
-
-		return $object_ids;
-	}
-
-	/**
 	 * Check if a user can synchronize translations.
 	 *
 	 * @since 2.6
