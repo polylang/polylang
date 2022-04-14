5c54bc1484cd8457ba49953201922854ff4e5cbe
Grégory Viguier <Screenfeed@users.noreply.github.com>
2022-04-14 15:12:54 +0200

    Use the ::class notation

--- modules/lingotek/load.php
+++ modules/lingotek/load.php
@@ -10,6 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 };
 
 if ( ! defined( 'POLYLANG_PRO' ) && ( ! defined( 'PLL_LINGOTEK_AD' ) || PLL_LINGOTEK_AD ) && $polylang instanceof PLL_Admin_Base ) {
-	$polylang->add_shared( 'lingotek', 'PLL_Lingotek' );
+	$polylang->add_shared( 'lingotek', PLL_Lingotek::class );
 	add_action( 'wp_loaded', array( $polylang->get( 'lingotek' ), 'init' ) );
 }
