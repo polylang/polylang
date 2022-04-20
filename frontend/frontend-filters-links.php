a3d1b454ec2d0ceeb8cfa17c02f92c526846845a
Hug0-Drelon <69580439+Hug0-Drelon@users.noreply.github.com>
2022-04-20 16:29:43 +0200

    test that $term_lang is not falsy
    Co-authored-by: Grégory Viguier <Screenfeed@users.noreply.github.com>

--- frontend/frontend-filters-links.php
+++ frontend/frontend-filters-links.php
@@ -540,7 +540,7 @@ class PLL_Frontend_Filters_Links extends PLL_Filters_Links {
 			function ( $term ) use ( $lang ) {
 				$term_lang = $this->model->term->get_language( $term );
 
-				return $term_lang->slug === $lang;
+				return ! empty( $term_lang ) && $term_lang->slug === $lang;
 			}
 		);
 
