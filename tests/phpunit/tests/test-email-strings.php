f0f27b6c06085c98881a567711bd8701e793f1f9
Hug0-Drelon <hugo.drelon@gmail.com>
2022-05-05 16:46:28 +0200

    import email test from test-filters.php

--- tests/phpunit/tests/test-email-strings.php
+++ tests/phpunit/tests/test-email-strings.php
@@ -29,7 +29,7 @@ class Email_Strings_Test extends PLL_UnitTestCase {
 
 	public function test_retrieve_password_default_language() {
 		global $wp_version;
-		if ( version_compare( $wp_version, '5.7', '>' ) ) {
+		if ( version_compare( $wp_version, '5.7', '<' ) ) {
 			$this->markTestSkipped( 'This test requires WordPress version 5.7 or higher.' );
 		}
 
@@ -66,7 +66,7 @@ class Email_Strings_Test extends PLL_UnitTestCase {
 
 	public function test_retrieve_password_secondary_language() {
 		global $wp_version;
-		if ( version_compare( $wp_version, '5.7', '>' ) ) {
+		if ( version_compare( $wp_version, '5.7', '<' ) ) {
 			$this->markTestSkipped( 'This test requires WordPress version 5.7 or higher.' );
 		}
 
@@ -98,6 +98,62 @@ class Email_Strings_Test extends PLL_UnitTestCase {
 		$this->assertTrue( $result, 'No mail has been sent to retrieve password.' );
 		$this->assertNotFalse( strpos( $mailer->get_sent()->subject, 'Mi Sitio' ), 'Blogname string has not been translated in mail subject.' );
 		$this->assertNotFalse( strpos( $mailer->get_sent()->body, 'Mi Sitio' ), 'Blogname string has not been translated in mail body.' );
+		reset_phpmailer_instance();
+	}
+
+	public function test_site_title_in_password_change_email() {
+		// Important to use a language available in DIR_TESTDATA . '/languages/', otherwise switch_to_locale() doesn't switch.
+		$language = self::$model->get_language( 'es' );
+		$_mo = new PLL_MO();
+		$_mo->add_entry( $_mo->make_entry( get_bloginfo( 'name' ), 'Mi sitio' ) );
+		$_mo->export_to_db( $language );
+
+		reset_phpmailer_instance();
+		$user_id = self::factory()->user->create();
+		update_user_meta( $user_id, 'locale', 'es_ES' );
+
+		// Set PLL environment.
+		$admin = new PLL_Admin( $this->links_model );
+		$admin->init();
+		$frontend_filters = new PLL_Admin_Filters( $admin );
+
+		$userdata = array(
+			'ID'        => $user_id,
+			'user_pass' => 'new password',
+		);
+		wp_update_user( $userdata );
+
+		$mailer = tests_retrieve_phpmailer_instance();
+		$this->assertNotFalse( strpos( $mailer->get_sent()->subject, 'Mi sitio' ) );
+		$this->assertNotFalse( strpos( $mailer->get_sent()->body, 'Mi sitio' ) );
+		reset_phpmailer_instance();
+	}
+
+	public function test_site_title_in_email_change_confirmation_email() {
+		$language = self::$model->get_language( 'es' );
+		$_mo = new PLL_MO();
+		$_mo->add_entry( $_mo->make_entry( get_bloginfo( 'name' ), 'Mi sitio' ) );
+		$_mo->export_to_db( $language );
+
+		reset_phpmailer_instance();
+		$user_id = self::factory()->user->create();
+		update_user_meta( $user_id, 'locale', 'es_ES' );
+		wp_set_current_user( $user_id );
+		set_current_screen( 'profile.php' );
+
+		// Set PLL environment.
+		$admin = new PLL_Admin( $this->links_model );
+		$admin->init();
+
+		$_POST = array(
+			'user_id' => $user_id,
+			'email'   => 'my_new_email@example.org',
+		);
+		do_action( 'personal_options_update', $user_id );
+
+		$mailer = tests_retrieve_phpmailer_instance();
+		$this->assertNotFalse( strpos( $mailer->get_sent()->subject, 'Mi sitio' ) );
+		$this->assertNotFalse( strpos( $mailer->get_sent()->body, 'Mi sitio' ) );
 		reset_phpmailer_instance();
 	}
 }
