4ab63a12a57b34ef36030a54fb8c7937b771b386
Hug0-Drelon <hugo.drelon@gmail.com>
2022-05-13 17:46:45 +0200

    remove log

--- js/src/post.js
+++ js/src/post.js
@@ -28,7 +28,6 @@ jQuery(
 				if ( 0 < mutation.addedNodes.length && ( form.classList.contains( 'inline-edit-post' ) || form.classList.contains( 'inline-edit-product' ) ) ) {
 					// WordPress has inserted the quick edit form.
 					const post_id = Number( form.id.substring( 5 ) );
-					console.log(post_id);
 
 					if ( post_id > 0 ) {
 						// Get the language dropdown.
