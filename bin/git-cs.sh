41703d4f5d951235e56e5f0772b05fa3236f6d44
Hug0-Drelon <hugo.drelon@gmail.com>
2022-08-11 22:25:34 +0200

    Echo some information.

--- bin/git-cs.sh
+++ bin/git-cs.sh
@@ -3,4 +3,10 @@
 # If there is none unstaged or uncommitted files, 'MODIFIED_FILES' will be empty (i.e. PHPCS will analyze all files).
 MODIFIED_FILES=$(git ls-files -om --exclude-standard)
 
+if [ ! -z "$MODIFIED_FILES" ]; then
+	echo -e "Running PHPCS on files:\n$MODIFIED_FILES"
+else
+	echo "Running PHPCS on all files"
+fi
+
 vendor/bin/phpcs $MODIFIED_FILES
