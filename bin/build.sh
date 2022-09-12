dd2dd00ba1e106531ff9dcf8082a290c2f2d268d
Emmanuel Hesry <ehesry@gmail.com>
2022-09-12 11:31:43 +0200

    Use npm update instead (#1108)

--- bin/build.sh
+++ bin/build.sh
@@ -9,9 +9,6 @@ echo "Installing PHP packages..."
 composer $COMPOSER_COMMAND # Need update to ensure to have the latest version of Polylang dependencies.
 
 echo "Running Polylang build..."
-npm install && npm run build
+npm update && npm run build
 
 echo "Build done!"
-
-echo "Cleanup" # Discard composer.lock and package-lock.json changings to ensure they'll never be pushed on repository.
-git checkout -- composer.lock package-lock.json
--- bin/distribute.sh
+++ bin/distribute.sh
@@ -4,7 +4,7 @@ rm -rf vendor/ # Make sure to remove all traces of development dependencies
 
 composer update --no-dev # composer.lock file is always present because pushed on repository.
 
-npm install && npm run build # minify js and css files
+npm update && npm run build # minify js and css files
 
 rsync -rc --exclude-from=.distignore . polylang/ --delete --delete-excluded
 zip -r polylang.zip polylang/*
