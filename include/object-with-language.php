b6c8c7ddbae8f9620f30e950f65c981898eb8801
Grégory Viguier <Screenfeed@users.noreply.github.com>
2022-11-08 17:06:11 +0100

    Don't register existing taxonomies

--- include/object-with-language.php
+++ include/object-with-language.php
@@ -84,6 +84,10 @@ abstract class PLL_Object_With_Language implements PLL_Object_With_Language_Inte
 		$this->model          = &$model;
 		$this->tax_to_cache[] = $this->tax_language;
 
+		if ( taxonomy_exists( $this->tax_language ) ) {
+			return;
+		}
+
 		/*
 		 * Register our taxonomy as soon as possible.
 		 * This is early registration, not ready for rewrite rules as $wp_rewrite will be setup later.
