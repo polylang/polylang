e7ddc238e14d411bc258b2e51eab0c220917d29a
Marianne380 <marianne.honczaruk@gmail.com>
2022-12-12 16:58:20 +0100

    API and types improvement (#1161)

--- modules/sync/sync.php
+++ modules/sync/sync.php
@@ -208,7 +208,7 @@ class PLL_Sync {
 				$translations = $this->model->term->get_translations( $term_id );
 
 				foreach ( $translations as $lang => $tr_id ) {
-					if ( ! empty( $tr_id ) && $tr_id !== $term_id ) {
+					if ( $tr_id !== $term_id ) {
 						$tr_parent = $this->model->term->get_translation( $term->parent, $lang );
 						$tr_term   = get_term( (int) $tr_id, $taxonomy );
 
--- tests/phpunit/tests/test-create-delete-languages.php
+++ tests/phpunit/tests/test-create-delete-languages.php
@@ -147,10 +147,7 @@ class Create_Delete_Languages_Test extends PLL_UnitTestCase {
 			'slug',
 			'term_group',
 			'term_taxonomy_id',
-			'count',
-			'tl_term_id',
-			'tl_term_taxonomy_id',
-			'tl_count',
+			'term_props',
 			'locale',
 			'is_rtl',
 			'w3c',
@@ -169,7 +166,18 @@ class Create_Delete_Languages_Test extends PLL_UnitTestCase {
 		);
 
 		$languages = get_transient( 'pll_languages_list' );
-		$this->assertEqualSets( $properties, array_keys( reset( $languages ) ) );
+		$language  = reset( $languages );
+		$this->assertEqualSets( $properties, array_keys( $language ) );
+
+		// Let's check PLL_Language::$term_props.
+		$this->assertArrayHasKey( 'language', $language['term_props'] );
+		$this->assertArrayHasKey( 'term_id', $language['term_props']['language'] );
+		$this->assertArrayHasKey( 'term_taxonomy_id', $language['term_props']['language'] );
+		$this->assertArrayHasKey( 'count', $language['term_props']['language'] );
+		$this->assertArrayHasKey( 'term_language', $language['term_props'] );
+		$this->assertArrayHasKey( 'term_id', $language['term_props']['term_language'] );
+		$this->assertArrayHasKey( 'term_taxonomy_id', $language['term_props']['term_language'] );
+		$this->assertArrayHasKey( 'count', $language['term_props']['term_language'] );
 	}
 
 	/**
