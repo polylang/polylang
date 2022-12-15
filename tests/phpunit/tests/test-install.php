6f9ea6624c08cf04fd4d97579d5df3c864f47f09
Marianne <marianne@wp-syntex.fr>
2022-12-15 12:23:33 +0100

    Get rid of deprecated PLL_Language tl_term_taxonomy_id property  in pll

--- tests/phpunit/tests/test-install.php
+++ tests/phpunit/tests/test-install.php
@@ -105,7 +105,7 @@ class Install_Test extends PLL_UnitTestCase {
 
 		// No languages for posts and terms
 		$this->assertEmpty( $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->term_relationships} WHERE term_taxonomy_id=%d", $english->get_tax_prop( 'language', 'term_taxonomy_id' ) ) ) );
-		$this->assertEmpty( $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->term_relationships} WHERE term_taxonomy_id=%d", $english->tl_term_taxonomy_id ) ) );
+		$this->assertEmpty( $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->term_relationships} WHERE term_taxonomy_id=%d", $english->get_tax_prop( 'language', 'tl_term_taxonomy_id' ) ) ) );
 
 		// No translations for posts and terms
 		$this->assertEmpty( get_terms( array( 'taxonomy' => 'post_translations' ) ) );
