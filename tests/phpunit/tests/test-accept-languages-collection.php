2df8e116be2c4a0ecc5f42d63b0c830600d2274a
Hug0-Drelon <hugo.drelon@gmail.com>
2022-12-20 10:54:57 +0100

    Use PLL_UnitTestCase_Trait::create_language() in Accept_Languages_Collection_Test.

--- tests/phpunit/tests/test-accept-languages-collection.php
+++ tests/phpunit/tests/test-accept-languages-collection.php
@@ -1,52 +1,6 @@
 <?php
 
-class Accept_Languages_Collection_Test extends WP_UnitTestCase {
-	/**
-	 * Polylang pre-registered languages.
-	 *
-	 * @see settings/languages.php
-	 * @var array
-	 */
-	protected static $known_languages;
-
-	/**
-	 * Polylang Admin Model, used to handle languages.
-	 *
-	 * @var PLL_Admin_Model
-	 */
-	protected static $model;
-
-	/**
-	 * @param WP_UnitTest_Factory $factory
-	 */
-	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
-		self::$known_languages = include POLYLANG_DIR . '/settings/languages.php';
-		$options               = PLL_Install::get_default_options();
-		self::$model           = new PLL_Admin_Model( $options );
-	}
-
-	/**
-	 * Returns a pre-registered language.
-	 *
-	 * @param string $locale
-	 * @return PLL_Language
-	 */
-	protected function get_known_language( $locale ) {
-		$language               = self::$known_languages[ $locale ];
-		$language['locale']     = $locale;
-		$language['slug']       = $language['code'];
-		$language['w3c']        = isset( $language['w3c'] ) ? $language['w3c'] : str_replace( '_', '-', $language['locale'] );
-		$language['rtl']        = 'rtl' === $language['dir'] ? 1 : 0;
-		$language['term_group'] = 0;
-		$result = self::$model->add_language( $language );
-
-		$this->assertNotInstanceOf( WP_Error::class, $result, "{$locale} language is not created." );
-
-		self::$model->clean_languages_cache();
-
-		return self::$model->get_language( $language['slug'] );
-	}
-
+class Accept_Languages_Collection_Test extends PLL_UnitTestCase {
 	/**
 	 * Use reflection to access PLL_Accept_Language values from PLL_Accept_Languages_Collection.
 	 *
@@ -117,7 +71,8 @@ class Accept_Languages_Collection_Test extends WP_UnitTestCase {
 
 	public function test_pick_matching_language_with_different_region() {
 		$accept_languages = PLL_Accept_Languages_Collection::from_accept_language_header( 'en-US' );
-		$en = $this->get_known_language( 'en_GB' );
+		self::create_language( 'en_GB' );
+		$en = self::$model->get_language( 'en_GB' );
 		$languages = array( $en );
 
 		$best_match = $accept_languages->find_best_match( $languages );
@@ -127,7 +82,8 @@ class Accept_Languages_Collection_Test extends WP_UnitTestCase {
 
 	public function test_pick_matching_language_and_region_when_script_is_missing() {
 		$accept_languages = PLL_Accept_Languages_Collection::from_accept_language_header( 'zh-Hant-HK' );
-		$zh_hk = $this->get_known_language( 'zh_HK' );
+		self::create_language( 'zh_HK' );
+		$zh_hk = self::$model->get_language( 'zh_HK' );
 		$languages = array( $zh_hk );
 
 		$best_match = $accept_languages->find_best_match( $languages );
@@ -137,7 +93,8 @@ class Accept_Languages_Collection_Test extends WP_UnitTestCase {
 
 	public function test_pick_matching_language_and_variant_when_region_is_missing() {
 		$accept_languages = PLL_Accept_Languages_Collection::from_accept_language_header( 'de-formal' );
-		$de_de_formal = $this->get_known_language( 'de_DE_formal' );
+		self::create_language( 'de_DE_formal' );
+		$de_de_formal = self::$model->get_language( 'de_DE_formal' );
 		$languages = array( $de_de_formal );
 
 		$best_match = $accept_languages->find_best_match( $languages );
@@ -147,21 +104,11 @@ class Accept_Languages_Collection_Test extends WP_UnitTestCase {
 
 	public function test_pick_matching_language_and_region_with_custom_slug() {
 		$accept_languages    = PLL_Accept_Languages_Collection::from_accept_language_header( 'zh-HK' );
-		$zh_cn               = self::$known_languages['zh_CN'];
-		$zh_cn['locale']     = 'zh_CN';
 		$zh_cn['slug']       = 'zh-cn'; // Custom slug.
-		$zh_cn['w3c']        = 'zh-CN';
-		$zh_cn['rtl']        = 'rtl' === $zh_cn['dir'] ? 1 : 0;
-		$zh_cn['term_group'] = 0;
-		$result              = self::$model->add_language( $zh_cn );
-
-		$this->assertNotInstanceOf( WP_Error::class, $result, 'zh_CN language is not created.' );
-
-		self::$model->clean_languages_cache();
-
-		$zh_cn = self::$model->get_language( $zh_cn['slug'] );
-
+		self::create_language( 'zh_CN', $zh_cn );
+		$zh_cn = self::$model->get_language( 'zh_CN' );
 		$languages = array( $zh_cn );
+
 		$best_match = $accept_languages->find_best_match( $languages );
 
 		$this->assertSame( $zh_cn->slug, $best_match );
