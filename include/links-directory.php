d4d1176e94369d3f238189f93e9f18ccbdbd1139
Hug0-Drelon <hugo.drelon@gmail.com>
2023-01-02 11:31:02 +0100

    Add a comment.

--- include/links-directory.php
+++ include/links-directory.php
@@ -214,7 +214,9 @@ class PLL_Links_Directory extends PLL_Links_Permalinks {
 
 		foreach ( $rules as $key => $rule ) {
 			if ( ! is_string( $rule ) || ! is_string( $key ) ) {
-				// Someone broke a rule...
+				/**
+				 * Protection agains Sendinblue for WooCommerce. See: https://wordpress.org/support/topic/bug-introduced-in-rewrite-rules/
+				 */
 				continue;
 			}
 
