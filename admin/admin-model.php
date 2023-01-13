043c9eafd078dd0e6115f9467a4f96080001215c
Hug0-Drelon <hugo.drelon@gmail.com>
2023-01-13 14:52:59 +0100

    Clean languages cache as before when adding a language.

--- admin/admin-model.php
+++ admin/admin-model.php
@@ -51,6 +51,8 @@ class PLL_Admin_Model extends PLL_Model {
 			update_option( 'polylang', $this->options );
 		}
 
+		$this->clean_languages_cache(); // Update the languages list now !
+
 		// Init a mo_id for this language
 		$mo = new PLL_MO();
 		$mo->export_to_db( $this->get_language( $args['slug'] ) );
