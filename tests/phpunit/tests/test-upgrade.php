6080692eb5fa3e78d2749f51ab8755afba674a06
Hug0-Drelon <hugo.drelon@gmail.com>
2023-05-09 17:43:14 +0200

    Do not test Polylang::init() but PLL_Upgrade instead. Because PLL_WPML_Config singleton cannot be reloaded...

--- tests/phpunit/tests/test-upgrade.php
+++ tests/phpunit/tests/test-upgrade.php
@@ -1,6 +1,8 @@
 <?php
 
 class Upgrade_Test extends PLL_UnitTestCase {
+	protected $options_backup;
+
 	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
 		parent::wpSetUpBeforeClass( $factory );
 
@@ -8,11 +10,15 @@ class Upgrade_Test extends PLL_UnitTestCase {
 		self::create_language( 'fr_FR' );
 	}
 
-	public function test_upgrade() {
-		// Fake old version.
-		$options = get_option( 'polylang' );
-		$options['version'] = '3.3';
-		update_option( 'polylang', $options );
+	/**
+	 * @ticket #1664 see {https://github.com/polylang/polylang-pro/issues/1664}.
+	 */
+	public function test_upgrade_to_3_4() {
+		wp_set_current_user( 1 );
+		update_user_meta( get_current_user_id(), 'pll_filter_content', 'en' );
+		$links_model = self::$model->get_links_model();
+		$admin = new PLL_Admin( $links_model );
+		$admin->init();
 
 		// Fake old transient.
 		self::$model->set_languages_ready();
@@ -22,14 +28,15 @@ class Upgrade_Test extends PLL_UnitTestCase {
 			unset( $language['term_props'] );
 			$languages[ $i ] = $language;
 		}
+		$admin->model->clean_languages_cache();
 		set_transient( 'pll_languages_list', $languages );
 
-		$pll = new Polylang();
+		new PLL_Upgrade( $admin->options );
 
 		try {
-			$pll->init();
+			do_action( 'setup_theme' ); // See the issue, `PLL_Admin_Base::init_user()` being hooked to `setup_theme`.
 		} catch ( \Throwable $th ) {
-			$this->assertTrue( false, "Polylang upgrade failed with the error {$th}" );
+			$this->assertTrue( false, "Polylang admin failed with error: {$th}" );
 		}
 
 		$this->assertTrue( true );
