f9a9c44b80ad07a4ad4b0f5248cba1b1aecf40fa
Grégory Viguier <git@screenfeed.fr>
2023-05-26 16:34:04 +0200

    Added a test

--- tests/phpunit/tests/test-model.php
+++ tests/phpunit/tests/test-model.php
@@ -270,5 +270,30 @@ class Model_Test extends PLL_UnitTestCase {
 		);
 		$this->assertSameSetsWithIndex( $slugs, $terms );
 	}
+
+	public function test_dont_use_old_format_languages_list() {
+		// Build cache.
+		self::$model->set_languages_ready();
+		self::$model->get_languages_list();
+		// Modify the transient.
+		$languages = get_transient( 'pll_languages_list' );
+		foreach ( $languages as &$language ) {
+			unset( $language['term_props'] );
+		}
+		// Inject our transient.
+		self::$model->clean_languages_cache();
+		set_transient( 'pll_languages_list', $languages );
+
+		// Test the list.
+		$languages = self::$model->get_languages_list();
+
+		$this->assertCount( 2, $languages, 'There should be 2 languages.' );
+
+		foreach ( $languages as $language ) {
+			$this->assertIsInt( $language->term_id, 'The language term_id should be an integer.' );
+			$this->assertGreaterThan( 0, $language->term_id, 'The language term_id should be a positive integer.' );
+			$this->assertSame( $language->term_id, $language->get_tax_prop( 'language', 'term_id' ), 'The tax prop term_id should contain the language term_id' );
+		}
+	}
 }
 
