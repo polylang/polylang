aefaa523bd113b15c959998a20cc78a0287fb2b6
Hug0-Drelon <hugo.drelon@gmail.com>
2023-06-22 10:13:58 +0200

    Pass var to closure.

--- tests/phpunit/includes/links-trait.php
+++ tests/phpunit/includes/links-trait.php
@@ -12,27 +12,20 @@ trait PLL_Test_Links_Trait {
 	protected $structure = '/%postname%/';
 
 	/**
-	 * Original `siteurl` option value.
-	 *
-	 * @var string
-	 */
-	protected $orig_siteurl = '';
-
-	/**
 	 * Filters `plugins_url` because `WP_CONTENT_URL` is already defined before the option `siteurl` is changed during tests for directory.
 	 * Also remove full path `POLYLANG_DIR` added to flag URL.
 	 *
 	 * @return void
 	 */
 	protected function filter_plugins_url() {
-		$this->orig_siteurl = get_option( 'siteurl' );
+		$orig_siteurl = get_option( 'siteurl' );
 
 		add_filter(
 			'plugins_url',
-			function ( $url ) {
+			function ( $url ) use ( $orig_siteurl ) {
 				$siteurl = get_option( 'siteurl' );
 				if ( false === strpos( $url, $siteurl ) ) {
-					$url = str_replace( $this->orig_siteurl, $siteurl, $url );
+					$url = str_replace( $orig_siteurl, $siteurl, $url );
 				}
 
 				return str_replace( POLYLANG_DIR . '/', '/polylang/', $url );
