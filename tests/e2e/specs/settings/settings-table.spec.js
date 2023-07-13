59171a352e3f095e08950f5ae5917a68e0a5e942
Hug0-Drelon <hugo.drelon@gmail.com>
2023-07-13 11:17:38 +0200

    Tests all settings modules.

--- tests/e2e/specs/settings/settings-table.spec.js
+++ tests/e2e/specs/settings/settings-table.spec.js
@@ -12,4 +12,42 @@ test.describe( 'Plugin Settings UI Test', () => {
 		await expect( page.getByText('The language is set from different domains') ).toBeVisible();
 		await expect( page.getByText('Hide URL language information for default language') ).toBeVisible();
 	});
+
+	test( 'Detect browser language module', async ( { page } ) => {
+		await page.goto('/wp-admin/admin.php?page=mlang_settings');
+		await page.getByRole('cell', { name: 'Detect browser language Activate' }).getByTitle('Activate this module').click();
+		await expect( page.locator('#pll-module-browser').getByText('Deactivate') ).toBeVisible();
+	});
+
+	test( 'Media module', async ( { page } ) => {
+		await page.goto('/wp-admin/admin.php?page=mlang_settings');
+		await page.getByRole('cell', { name: 'Media Activate' }).getByTitle('Activate this module').click();
+		await expect( page.locator('#pll-module-media').getByText('Deactivate') ).toBeVisible();
+	});
+
+	test( 'Custom post types and Taxonomies module', async ( { page } ) => {
+		await page.goto('/wp-admin/admin.php?page=mlang_settings');
+		await expect( page.getByText('Custom post types and Taxonomies', { exact: true }) ).toBeVisible();
+		await expect( page.locator('#pll-module-cpt').getByText('Deactivated') ).toBeVisible();
+	});
+
+	test( 'Share slugs module', async ( { page } ) => {
+		await page.goto('/wp-admin/admin.php?page=mlang_settings');
+		await expect( page.getByText('Share slugs') ).toBeVisible();
+		await expect( page.locator('#pll-module-share-slugs').getByText('Deactivated') ).toBeVisible();
+		await expect( page.locator('div').filter({ hasText: /^To enable this feature, you need Polylang Pro\. Upgrade now\.$/ }).first() ).toBeVisible();
+	});
+
+	test( 'Translate slugs module', async ( { page } ) => {
+		await page.goto('/wp-admin/admin.php?page=mlang_settings');
+		await expect( page.getByText('Translate slugs') ).toBeVisible();
+		await expect( page.locator('#pll-module-translate-slugs').getByText('Deactivated') ).toBeVisible();
+		await expect( page.locator('div').filter({ hasText: /^To enable this feature, you need Polylang Pro\. Upgrade now\.$/ }).nth(1) ).toBeVisible();
+	});
+
+	test( 'License keys module', async ( { page } ) => {
+		await page.goto('/wp-admin/admin.php?page=mlang_settings');
+		await expect( page.getByText('License keys') ).toBeVisible();
+		await expect( page.locator('#pll-module-licenses').getByText('Deactivated') ).toBeVisible();
+	});
 });
