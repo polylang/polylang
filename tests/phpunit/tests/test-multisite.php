d35b73ebebbb6ee64ec40a1abe03b31921b6016c
Grégory Viguier <git@screenfeed.fr>
2023-07-17 12:29:46 +0200

    Skip the test if not multisite

--- tests/phpunit/tests/test-multisite.php
+++ tests/phpunit/tests/test-multisite.php
@@ -1,15 +1,20 @@
 <?php
 
-if ( is_multisite() ) :
+class Multisite_Test extends PLL_UnitTestCase {
 
-	class Multisite_Test extends PLL_UnitTestCase {
-		public function test_new_site() {
-			$site_id = self::factory()->blog->create();
-			switch_to_blog( $site_id );
-			$options = get_option( 'polylang' );
-			restore_current_blog();
-			$this->assertNotFalse( $options );
+	public function set_up() {
+		if ( ! is_multisite() ) {
+			$this->markTestSkipped( 'This test requires WordPress multisite.' );
 		}
+
+		parent::set_up();
 	}
 
-endif;
+	public function test_new_site() {
+		$site_id = self::factory()->blog->create();
+		switch_to_blog( $site_id );
+		$options = get_option( 'polylang' );
+		restore_current_blog();
+		$this->assertNotFalse( $options );
+	}
+}
