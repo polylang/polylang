472e5ea747eb1532d7b928ff95dbfc4aeb5a7453
Hugo Drelon <69580439+Hug0-Drelon@users.noreply.github.com>
2023-07-19 16:55:38 +0200

    Migrate CI to Github Actions (#1314)

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 $_root_dir = dirname( dirname( dirname( __DIR__ ) ) );
-$_tests_dir = $_root_dir . '/tmp/wordpress-tests-lib';
+$_tests_dir = ! empty( getenv( 'WP_TESTS_DIR' ) ) ? getenv( 'WP_TESTS_DIR' ) : $_root_dir . '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 // load the plugin however *no* Polylang instance is created as no languages exist in DB
