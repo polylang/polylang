a2e0d44599a317db02f54ee472e9d85c667b5672
Grégory Viguier <git@screenfeed.fr>
2023-09-04 11:23:34 +0200

    Prevent Patchwork to print text during init

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -4,6 +4,22 @@ $_root_dir = dirname( dirname( dirname( __DIR__ ) ) );
 $_tests_dir = ! empty( getenv( 'WP_TESTS_DIR' ) ) ? getenv( 'WP_TESTS_DIR' ) : $_root_dir . '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
+// Temporary fix: prevent Patchwork to print `;\Patchwork\CodeManipulation\Stream::reinstateWrapper();` in CLI.
+$db_file_path = $_root_dir . '/tmp/wordpress/wp-content/db.php';
+
+if ( is_readable( $db_file_path ) && is_writable( $db_file_path ) ) {
+	$db_file_content = file_get_contents( $db_file_path );
+
+	if ( is_string( $db_file_content ) && trim( $db_file_content ) === '' ) {
+		$fp = @fopen( $db_file_path, 'wb' );
+
+		if ( $fp ) {
+			fwrite( $fp, '<?php' );
+			fclose( $fp );
+		}
+	}
+}
+
 // load the plugin however *no* Polylang instance is created as no languages exist in DB
 tests_add_filter(
 	'muplugins_loaded',
