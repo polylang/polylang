0cc8c1fb3b612ec92d644df37963f3f91c6ae50f
Hug0-Drelon <hugo.drelon@gmail.com>
2023-09-08 16:19:35 +0200

    Filter REST request in our client middleware according to translatable entities defined server side.

---
+++ js/src/lib/filter-path-middleware.js
@@ -0,0 +1,22 @@
+/**
+ * @package Polylang
+ */
+
+/**
+ * Filters requests for translatable entities.
+ * This logic is shared accross all Polylang plugins.
+ *
+ * @since 3.5
+ *
+ * @param {APIFetchOptions} options
+ * @param {Array} filteredRoutes
+ * @param {CallableFunction} filter
+ * @returns {APIFetchOptions}
+ */
+const filterPathMiddleware = ( options, filteredRoutes, filter ) => {
+	const cleanPath = options.path.split( '?' )[0].replace(/^\/+|\/+$/g, ''); // Get path without query parameters and trim '/'.
+
+	return Object.values( filteredRoutes ).find( ( path ) => cleanPath === path ) ? filter( options ) : options;
+}
+
+export default filterPathMiddleware;
