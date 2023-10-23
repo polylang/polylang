924300d8f2a48a98060cebce20b1afee8fd676c0
Chouby <frederic.demarle@yahoo.fr>
2023-10-23 10:30:09 +0200

    Test wpml config locations (#1373)

---
+++ tests/phpunit/data/plugins/best-plugin/best-plugin.php
@@ -0,0 +1,5 @@
+<?php
+/*
+Plugin Name: Dummy Plugin
+Description: For testing purposes only.
+*/
---
+++ tests/phpunit/tests/test-wpml-config-locations.php
@@ -0,0 +1,116 @@
+<?php
+
+class WPML_Config_Locations_Test extends PLL_UnitTestCase {
+
+	protected static $dirs = array(
+		'themes/best-theme',
+		'themes/best-child',
+		'plugins/best-plugin',
+	);
+
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		parent::wpSetUpBeforeClass( $factory );
+
+		// Copy themes and plugins from tests/phpunit/data to wp-content.
+		foreach ( self::$dirs as $path ) {
+			$source_dir = PLL_TEST_DATA_DIR . $path;
+			$dest_dir   = WP_CONTENT_DIR . "/{$path}";
+
+			@mkdir( $dest_dir );
+
+			foreach ( glob( $source_dir . '/*.*' ) as $file ) {
+				copy( $file, $dest_dir . '/' . basename( $file ) );
+			}
+		}
+	}
+
+	public static function wpTearDownAfterClass() {
+		parent::wpTearDownAfterClass();
+
+		// Remove previously copied themes and plugins from wp-content.
+		foreach ( self::$dirs as $type => $path ) {
+			$dest_dir = WP_CONTENT_DIR . "/{$path}";
+
+			foreach ( glob( $dest_dir . '/*.*' ) as $file ) {
+				unlink( $file );
+			}
+
+			rmdir( $dest_dir );
+		}
+	}
+
+	public function test_in_polylang() {
+		@mkdir( WP_CONTENT_DIR . '/polylang' );
+		copy( PLL_TEST_DATA_DIR . 'wpml-config.xml', WP_CONTENT_DIR . '/polylang/wpml-config.xml' );
+
+		$files    = ( new PLL_WPML_Config() )->get_files();
+		$expected = array( 'Polylang' => WP_CONTENT_DIR . '/polylang/wpml-config.xml' );
+
+		unlink( WP_CONTENT_DIR . '/polylang/wpml-config.xml' );
+		rmdir( WP_CONTENT_DIR . '/polylang' );
+
+		$this->assertSameSetsWithIndex( $expected, $files );
+	}
+
+	public function test_in_theme() {
+		switch_theme( 'best-theme' );
+
+		$files    = ( new PLL_WPML_Config() )->get_files();
+		$expected = array( 'themes/best-theme' => get_theme_root() . '/best-theme/wpml-config.xml' );
+
+		switch_theme( 'default' );
+
+		$this->assertSameSetsWithIndex( $expected, $files );
+	}
+
+	public function test_in_child_theme() {
+		switch_theme( 'best-child' );
+
+		$files    = ( new PLL_WPML_Config() )->get_files();
+		$expected = array(
+			'themes/best-theme' => get_theme_root() . '/best-theme/wpml-config.xml',
+			'themes/best-child' => get_theme_root() . '/best-child/wpml-config.xml',
+		);
+
+		switch_theme( 'default' );
+
+		$this->assertSameSetsWithIndex( $expected, $files );
+	}
+
+	public function test_in_active_plugin() {
+		activate_plugin( 'best-plugin/best-plugin.php' );
+		$files    = ( new PLL_WPML_Config() )->get_files();
+		$expected = array( 'plugins/best-plugin' => WP_PLUGIN_DIR . '/best-plugin/wpml-config.xml' );
+
+		$this->assertSameSetsWithIndex( $expected, $files );
+	}
+
+	public function test_in_mu_plugin() {
+		@mkdir( WPMU_PLUGIN_DIR );
+		$filename_1 = WPMU_PLUGIN_DIR . '/wpml-config.xml';
+		copy( PLL_TEST_DATA_DIR . 'wpml-config.xml', $filename_1 );
+
+		@mkdir( WPMU_PLUGIN_DIR . '/must-use' );
+		$filename_2 = WPMU_PLUGIN_DIR . '/must-use/wpml-config.xml';
+		copy( PLL_TEST_DATA_DIR . 'wpml-config.xml', $filename_2 );
+
+		@symlink( PLL_TEST_DATA_DIR . 'plugins/best-plugin', WPMU_PLUGIN_DIR . '/best-plugin' );
+
+		$files    = ( new PLL_WPML_Config() )->get_files();
+		$expected = array(
+			'mu-plugins'             => $filename_1,
+			'mu-plugins/must-use'    => $filename_2,
+			'mu-plugins/best-plugin' => WPMU_PLUGIN_DIR . '/best-plugin/wpml-config.xml',
+		);
+
+		unlink( WPMU_PLUGIN_DIR . '/best-plugin' );
+
+		unlink( $filename_2 );
+		rmdir( WPMU_PLUGIN_DIR . '/must-use' );
+
+		unlink( $filename_1 );
+		rmdir( WPMU_PLUGIN_DIR );
+
+		$this->assertSameSetsWithIndex( $expected, $files );
+	}
+}
