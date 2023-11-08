cba1352b041399fbd45f12fa9afe0a03c3ff8285
Marianne <marianne@wp-syntex.fr>
2023-11-08 17:44:48 +0100

    Sets up the strings directly in the test

--- tests/phpunit/tests/test-switch-blog.php
+++ tests/phpunit/tests/test-switch-blog.php
@@ -37,16 +37,6 @@ if ( is_multisite() ) :
 			),
 		);
 
-		public function set_up() {
-			parent::set_up();
-
-			$pll_admin = $this->get_pll_admin_env();
-
-			$mo = new PLL_MO();
-			$mo->add_entry( $mo->make_entry( 'Test en', 'Test fr' ) );
-			$mo->export_to_db( $pll_admin->model->get_language( 'fr' ) );
-		}
-
 		/**
 		 * Checks that the `pll__` function gets the right string translation when two languages are sharing the same locale, with multisite.
 		 *
@@ -54,6 +44,12 @@ if ( is_multisite() ) :
 		 * @see https://github.com/polylang/polylang-pro/issues/1870.
 		 */
 		public function test_strings_translation_with_same_language_locale_with_multisite() {
+			$pll_admin = $this->get_pll_admin_env();
+
+			$mo = new PLL_MO();
+			$mo->add_entry( $mo->make_entry( 'Test en', 'Test fr' ) );
+			$mo->export_to_db( $pll_admin->model->get_language( 'fr' ) );
+
 			$pll_frontend          = $this->get_pll_frontend_env( array( 'force_lang' => 1 ) );
 			$pll_frontend->curlang = $pll_frontend->model->get_language( 'en' );
 			do_action( 'pll_language_defined' );
