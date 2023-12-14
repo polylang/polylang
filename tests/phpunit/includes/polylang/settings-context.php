2e487c4070a64fd9a259d4df5147229bafe7c480
Marianne <marianne@wp-syntex.fr>
2023-12-14 16:27:49 +0100

    Adds do_wordpress_actions for PLL_Settings_Context

--- tests/phpunit/includes/polylang/settings-context.php
+++ tests/phpunit/includes/polylang/settings-context.php
@@ -25,4 +25,26 @@ class PLL_Settings_Context extends PLL_Admin_Context {
 	protected function get_model( array $options ): PLL_Model {
 		return new PLL_Admin_Model( $options );
 	}
+
+	/**
+	 * Executes Polylang actions on filters that need to be run according to context.
+	 * Also refresh WordPress’ rewrite rule.
+	 *
+	 * @since 3.6
+	 *
+	 * @return void
+	 */
+	protected function do_wordpress_actions() {
+		global $wp_rewrite;
+
+		$this->do_pll_actions( 'setup_theme' );
+		$this->do_pll_actions( 'after_setup_theme' );
+		$this->do_pll_actions( 'init' );
+		$this->do_pll_actions( 'widgets_init' );
+		$this->do_pll_actions( 'wp_loaded' );
+
+		$wp_rewrite->flush_rules();
+
+		$this->do_pll_actions( 'admin_init' );
+	}
 }
