5f957ad906911b4795c8a63f65dc2a13deeb6415
Marianne <marianne@wp-syntex.fr>
2023-12-19 16:22:51 +0100

    PLL_Settings_Context::do_wordpress_actions() was duplicate from parent

--- tests/phpunit/includes/polylang/settings-context.php
+++ tests/phpunit/includes/polylang/settings-context.php
@@ -25,25 +25,4 @@ class PLL_Settings_Context extends PLL_Admin_Context {
 	protected function get_model( array $options ): PLL_Model {
 		return new PLL_Admin_Model( $options );
 	}
-
-	/**
-	 * Executes Polylang actions on filters that need to be run according to context.
-	 * Also refreshes WordPress’ rewrite rules.
-	 *
-	 * @since 3.6
-	 *
-	 * @return void
-	 */
-	protected function do_wordpress_actions() {
-		$this->do_pll_actions( 'setup_theme' );
-		$this->do_pll_actions( 'after_setup_theme' );
-		$this->do_pll_actions( 'init' );
-		$this->do_pll_actions( 'widgets_init' );
-		$this->do_pll_actions( 'wp_loaded' );
-
-		flush_rewrite_rules();
-
-		$this->do_pll_actions( 'admin_init' );
-		$this->do_pll_actions( 'current_screen' );
-	}
 }
