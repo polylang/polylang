e4f8644b4fb45189f472c259ef89fdd0eedd3573
Hug0-Drelon <hugo.drelon@gmail.com>
2024-04-09 17:18:59 +0200

    Clean globals to remove previously loaded domains and cache and load correctly custom domain in tests.

--- tests/phpunit/tests/test-olt-manager.php
+++ tests/phpunit/tests/test-olt-manager.php
@@ -5,6 +5,12 @@ class OLT_Manager_Test extends PLL_UnitTestCase {
 		$factory->language->create_many( 2 );
 	}
 
+	public static function wpTearDownAfterClass() {
+		parent::wpTearDownAfterClass();
+
+		unlink( WP_LANG_DIR . '/plugins/foo-fr_FR.mo' );
+	}
+
 	public function test_polylang_first() {
 		$plugins = array(
 			'jetpack/jetpack.php',
@@ -26,22 +32,37 @@ class OLT_Manager_Test extends PLL_UnitTestCase {
 	}
 
 	public function test_load_textdomains() {
+		// Clean up!
+		unset( $GLOBALS['wp_themes'], $GLOBALS['l10n'], $GLOBALS['l10n_unloaded'] );
+
+		/** @var WP_Textdomain_Registry $wp_textdomain_registry */
+		global $wp_textdomain_registry;
+
+		$wp_textdomain_registry = new WP_Textdomain_Registry();
+
 		update_option( 'WPLANG', '' );
 
+		// Copy language file.
+		@mkdir( DIR_TESTDATA );
+		@mkdir( WP_LANG_DIR );
+		@mkdir( WP_LANG_DIR . '/plugins' );
+		copy( PLL_TEST_DATA_DIR . 'fr_FR.mo', WP_LANG_DIR . '/plugins/foo-fr_FR.mo' );
+
 		new PLL_OLT_Manager();
-
-		load_textdomain( 'foo', PLL_TEST_DATA_DIR . 'fr_FR.mo' );
-
 		$options                 = PLL_Install::get_default_options();
 		$options['force_lang']   = 0;
 		$options['default_lang'] = 'en';
 		$model                   = new PLL_Model( $options );
 		$links_model             = $model->get_links_model();
 		$frontend                = new PLL_Frontend( $links_model );
-		$fr                      = $frontend->model->get_language( 'fr' );
-		$frontend->curlang       = $fr;
 
-		do_action( 'pll_language_defined', 'fr', $fr );
+		$is_loaded = load_plugin_textdomain( 'foo' );
+
+		$this->assertFalse( $is_loaded, 'textdomain should be bypassed by PLL_OLT_Manager.' );
+
+		$frontend->curlang = $frontend->model->get_language( 'fr' );
+
+		do_action( 'pll_language_defined', 'fr', $frontend->curlang );
 
 		$locale = get_locale();
 
