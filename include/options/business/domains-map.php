73227ee0ff74f302b700a9971e597946208003ee
Grégory Viguier <git@screenfeed.fr>
2024-04-16 14:12:23 +0200

    Extend `PLL_Map_Option` and use parent's `create_schema()`

--- include/options/business/domains-map.php
+++ include/options/business/domains-map.php
@@ -8,7 +8,7 @@
  *
  * @since 3.7
  */
-class PLL_Domains_Map_Option extends PLL_List_Option {
+class PLL_Domains_Map_Option extends PLL_Map_Option {
 	/**
 	 * Creates JSON schema of the option.
 	 *
@@ -17,20 +17,14 @@ class PLL_Domains_Map_Option extends PLL_List_Option {
 	 * @return array The schema.
 	 */
 	protected function create_schema(): array {
-		return array(
-			'$schema'     => 'http://json-schema.org/draft-04/schema#',
-			'title'       => $this->key(),
-			'description' => $this->description,
-			'type'        => 'object', // Correspond to associative array in PHP, @see{https://developer.wordpress.org/rest-api/extending-the-rest-api/schema/#primitive-types}.
-			'context'     => array( 'edit' ),
-			'patternProperties'    => array(
-				'^[a-z_-]+$' => array( // Language slug as key.
-					'type'   => $this->type,
-					'format' => 'uri',
-				),
+		$map_schema                      = parent::create_schema();
+		$map_schema['patternProperties'] = array(
+			'^[a-z_-]+$' => array( // Language slug as key.
+				'type'   => $this->type,
+				'format' => 'uri',
 			),
-			'additionalProperties' => false,
 		);
+		return $map_schema;
 	}
 
 	/**
