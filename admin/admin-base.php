25115f6f9299ba39abc6b0f64149a68d07669b01
Grégory Viguier <git@screenfeed.fr>
2024-04-17 14:58:19 +0200

    Introduced `PLL_Admin_Base::languages_page()` This allows to remove a PHPStan rule from the baseline.

--- admin/admin-base.php
+++ admin/admin-base.php
@@ -155,6 +155,16 @@ abstract class PLL_Admin_Base extends PLL_Base {
 	}
 
 	/**
+	 * Dummy method to display the 3 tabs pages: languages, strings translations, settings.
+	 * Must be overwritten in child class.
+	 *
+	 * @since 3.7
+	 *
+	 * @return void
+	 */
+	public function languages_page() {}
+
+	/**
 	 * Setup js scripts & css styles ( only on the relevant pages )
 	 *
 	 * @since 0.6
