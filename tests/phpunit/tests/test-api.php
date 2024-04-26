570be6b26c9ce1bc692deff8e785c8bf07051e06
Hug0-Drelon <hugo.drelon@gmail.com>
2024-04-26 10:44:19 +0200

    Add some tests on pll_get_post() and pll_get_term() to ensure returned types.

---
+++ tests/phpunit/tests/test-api.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Test class for Polylang public API.
+ */
+class API_Test extends PLL_UnitTestCase {
+	public static function pllSetUpBeforeClass( PLL_UnitTest_Factory $factory ) {
+		$factory->language->create_many( 3 );
+	}
+
+	public function set_up() {
+		parent::set_up();
+
+		$this->pll_env       = ( new PLL_Context_Frontend() )->get();
+		$GLOBALS['polylang'] = $this->pll_env;
+	}
+
+	public function test_pll_get_post() {
+		$posts = $this->factory()->post->create_translated(
+			array( 'lang' => 'en' ),
+			array( 'lang' => 'fr' )
+		);
+		$this->pll_env->curlang = $this->pll_env->model->get_language( 'fr' );
+
+		$this->assertSame( $posts['en'], pll_get_post( $posts['en'], 'en' ) );
+		$this->assertSame( $posts['fr'], pll_get_post( $posts['en'], 'fr' ) );
+		$this->assertSame( 0, pll_get_post( $posts['en'], 'de' ) );
+		$this->assertSame( $posts['fr'], pll_get_post( $posts['en'] ) );
+	}
+
+	public function test_pll_get_term() {
+		$terms = $this->factory()->term->create_translated(
+			array( 'lang' => 'en' ),
+			array( 'lang' => 'fr' )
+		);
+		$this->pll_env->curlang = $this->pll_env->model->get_language( 'fr' );
+
+		$this->assertSame( $terms['en'], pll_get_term( $terms['en'], 'en' ) );
+		$this->assertSame( $terms['fr'], pll_get_term( $terms['en'], 'fr' ) );
+		$this->assertSame( 0, pll_get_term( $terms['en'], 'de' ) );
+		$this->assertSame( $terms['fr'], pll_get_term( $terms['en'] ) );
+	}
+}
