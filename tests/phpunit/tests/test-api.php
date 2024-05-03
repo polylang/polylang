d7fd62944525b24621ddbb1397cf547b4e412872
Hug0-Drelon <hugo.drelon@gmail.com>
2024-05-03 11:01:21 +0200

    Add tests for PLL_Translated_object::get_translation.

--- tests/phpunit/tests/test-api.php
+++ tests/phpunit/tests/test-api.php
@@ -15,6 +15,9 @@ class API_Test extends PLL_UnitTestCase {
 		$GLOBALS['polylang'] = $this->pll_env;
 	}
 
+	/**
+	 * @covers ::pll_get_post
+	 */
 	public function test_pll_get_post() {
 		$posts = $this->factory()->post->create_translated(
 			array( 'lang' => 'en' ),
@@ -29,6 +32,9 @@ class API_Test extends PLL_UnitTestCase {
 		$this->assertSame( $posts['fr'], pll_get_post( $posts['en'] ) );
 	}
 
+	/**
+	 * @covers ::pll_get_term
+	 */
 	public function test_pll_get_term() {
 		$terms = $this->factory()->term->create_translated(
 			array( 'lang' => 'en' ),
@@ -41,5 +47,22 @@ class API_Test extends PLL_UnitTestCase {
 		$this->assertSame( 0, pll_get_term( $terms['en'], 'chti' ) );
 		$this->assertSame( 0, pll_get_term( $terms['en'], 'de' ) );
 		$this->assertSame( $terms['fr'], pll_get_term( $terms['en'] ) );
+	}
+
+	/**
+	 * @testWith ["post"]
+	 *           ["term"]
+	 *
+	 * @covers PLL_Translated_Object::get_translation
+	 */
+	public function test_translated_objects_get_translation( $type ) {
+		$objects = $this->factory()->$type->create_translated(
+			array( 'lang' => 'en' ),
+			array( 'lang' => 'fr' )
+		);
+
+		$this->assertSame( $objects['fr'], PLL()->model->$type->get_translation( $objects['en'], 'fr' ) );
+		$this->assertSame( 0, PLL()->model->$type->get_translation( $objects['en'], 'de' ) );
+		$this->assertSame( 0, PLL()->model->$type->get_translation( $objects['en'], 'chti' ) );
 	}
 }
