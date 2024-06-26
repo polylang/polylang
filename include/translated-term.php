a669752b7a475294f199d152bd963a6ba1bdb300
Marianne <marianne@wp-syntex.fr>
2024-06-26 15:37:23 +0200

    Fix doc

--- include/translated-term.php
+++ include/translated-term.php
@@ -342,7 +342,7 @@ class PLL_Translated_Term extends PLL_Translated_Object implements PLL_Translata
 	}
 
 	/**
-	 * Wrapper of `wp_insert_term` with the language.
+	 * Wraps `wp_insert_term` with language feature.
 	 *
 	 * @since 3.7
 	 *
@@ -374,7 +374,7 @@ class PLL_Translated_Term extends PLL_Translated_Object implements PLL_Translata
 	}
 
 	/**
-	 * Wrapper of `wp_update_term` with the language.
+	 * Wraps `wp_update_term` with language feature.
 	 *
 	 * @since 3.7
 	 *
