93f5619b1d07d17a26d639d796acc8b20e2500b7
Frédéric Demarle <frederic.demarle@yahoo.fr>
2024-07-30 15:37:53 +0200

    Improvements proposal for the default term in backend

--- admin/admin-default-term.php
+++ admin/admin-default-term.php
@@ -10,18 +10,6 @@
  * @since 3.7 Extends `PLL_Default_Term`, most of the code is moved to it.
  */
 class PLL_Admin_Default_Term extends PLL_Default_Term {
-	/**
-	 * Constructor: setups properties.
-	 *
-	 * @since 3.1
-	 *
-	 * @param object $polylang The Polylang object.
-	 */
-	public function __construct( &$polylang ) {
-		$this->model      = &$polylang->model;
-		$this->curlang    = &$polylang->pref_lang;
-		$this->taxonomies = $this->model->get_translated_taxonomies();
-	}
 
 	/**
 	 * Setups filters and actions needed.
@@ -36,7 +24,7 @@ class PLL_Admin_Default_Term extends PLL_Default_Term {
 		foreach ( $this->taxonomies as $taxonomy ) {
 			if ( 'category' === $taxonomy ) {
 				// Adds the language column in the 'Terms' table.
-				add_filter( 'manage_' . $taxonomy . '_custom_column', array( $this, 'term_column' ), 10, 3 );
+				add_filter( 'pll_first_language_term_column', array( $this, 'first_language_column' ), 10, 2 );
 			}
 		}
 	}
@@ -44,35 +32,17 @@ class PLL_Admin_Default_Term extends PLL_Default_Term {
 	/**
 	 * Identifies the default term in the terms list table to disable the language dropdown in JS.
 	 *
-	 * @since 3.1
+	 * @since 3.7
 	 *
 	 * @param  string $out     The output.
-	 * @param  string $column  The custom column's name.
 	 * @param  int    $term_id The term id.
 	 * @return string          The HTML string.
 	 */
-	public function term_column( $out, $column, $term_id ) {
-		if ( $column === $this->get_first_language_column() && $this->is_default_term( $term_id ) ) {
+	public function first_language_column( $out, $term_id ) {
+		if ( $this->is_default_term( $term_id ) ) {
 			$out .= sprintf( '<div class="hidden" id="default_cat_%1$d">%1$d</div>', intval( $term_id ) );
 		}
 
 		return $out;
-	}
-
-	/**
-	 * Returns the first language column in the posts, pages and media library tables.
-	 *
-	 * @since 0.9
-	 *
-	 * @return string First language column name.
-	 */
-	protected function get_first_language_column() {
-		$columns = array();
-
-		foreach ( $this->model->get_languages_list() as $language ) {
-			$columns[] = 'language_' . $language->slug;
-		}
-
-		return empty( $columns ) ? '' : reset( $columns );
 	}
 }
