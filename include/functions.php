750f731179fec3c1798f4478b31c2b1ec0134747
Hug0-Drelon <hugo.drelon@gmail.com>
2024-08-22 00:12:41 +0200

    Use new Abstract_Screen in pll_use_block_editor_plugin().

--- include/functions.php
+++ include/functions.php
@@ -126,7 +126,7 @@ function pll_use_block_editor_plugin() {
 	 *
 	 * @param bool $use_plugin True when loading the block editor plugin.
 	 */
-	return class_exists( 'PLL_Block_Editor_Plugin' ) && apply_filters( 'pll_use_block_editor_plugin', ! defined( 'PLL_USE_BLOCK_EDITOR_PLUGIN' ) || PLL_USE_BLOCK_EDITOR_PLUGIN );
+	return class_exists( 'Abstract_Screen' ) && apply_filters( 'pll_use_block_editor_plugin', ! defined( 'PLL_USE_BLOCK_EDITOR_PLUGIN' ) || PLL_USE_BLOCK_EDITOR_PLUGIN );
 }
 
 /**
