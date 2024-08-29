5a07fb4b122138d908768613a7cb1ebfdb324bbc
sebastienserre <sebastien@thivinfo.com>
2024-08-29 16:02:06 +0200

    premium plugins translations are detected.

--- modules/site-health/admin-site-health.php
+++ modules/site-health/admin-site-health.php
@@ -461,43 +461,43 @@ class PLL_Admin_Site_Health {
 		$fields = array();
 
 			$translation_updates_list = $this->get_translations_update_list( $translation_updates );
-			if ( ! empty( $translation_updates_list ) ) {
-				foreach ( $translation_updates_list as $type => $values ) {
-					$type_label = __( 'WordPress', 'polylang' );
+		if ( ! empty( $translation_updates_list ) ) {
+			foreach ( $translation_updates_list as $type => $values ) {
+				$type_label = __( 'WordPress', 'polylang' );
 
-					if ( 'plugin' === $type ) {
-						$type = 'plugins';
-						$type_label = __( 'Plugins', 'polylang' );
-					}
-					if ( 'theme' === $type ) {
-						$type = 'themes';
-						$type_label = __( 'Themes', 'polylang' );
+				if ( 'plugin' === $type ) {
+					$type = 'plugins';
+					$type_label = __( 'Plugins', 'polylang' );
+				}
+				if ( 'theme' === $type ) {
+					$type = 'themes';
+					$type_label = __( 'Themes', 'polylang' );
 
-					}
-					$fields[ 'translation_' . $type ]['label'] = '=== ' . $type_label . ' ===';
-					$fields[ 'translation_' . $type ]['value'] = ' '; // needed to avoid a "undefined" when copy to clipboard. Empty string is skipped.
-					foreach ( $values as $name => $value ) {
-						$fields[ 'translation_' . $name ]['label'] = $name;
-						$is_locales_installed = $this->is_wp_language_installed( $value, $type, $name );
-						if ( ! $is_locales_installed ) {
-							$locales = implode( ', ', $value );
-							$fields[ 'translation_' . $name ]['value'] = sprintf(
-							/* translators: the placeholder is a WordPress locale */
-								__( 'A translation is missing for %s .', 'polylang' ),
-								$locales
-							);
-						} else {
-							$locales = implode( ', ', $value );
+				}
+				$fields[ 'translation_' . $type ]['label'] = '=== ' . $type_label . ' ===';
+				$fields[ 'translation_' . $type ]['value'] = ' '; // needed to avoid a "undefined" when copy to clipboard. Empty string is skipped.
+				foreach ( $values as $name => $value ) {
+					$fields[ 'translation_' . $name ]['label'] = $name;
+					$is_locales_installed = $this->is_wp_language_installed( $value, $type, $name );
+					if ( ! $is_locales_installed ) {
+						$locales = implode( ', ', $value );
+						$fields[ 'translation_' . $name ]['value'] = sprintf(
+						/* translators: the placeholder is a WordPress locale */
+							__( 'A translation is missing for %s .', 'polylang' ),
+							$locales
+						);
+					} else {
+						$locales = implode( ', ', $value );
 
-							$fields[ 'translation_' . $name ]['value'] = sprintf(
-							/* translators: the placeholder is a WordPress locale */
-								__( 'A translation is updatable for %s .', 'polylang' ),
-								$locales
-							);
-						}
+						$fields[ 'translation_' . $name ]['value'] = sprintf(
+						/* translators: the placeholder is a WordPress locale */
+							__( 'A translation is updatable for %s .', 'polylang' ),
+							$locales
+						);
 					}
 				}
 			}
+		}
 
 		// Create the section.
 		if ( ! empty( $fields ) ) {
@@ -511,9 +511,6 @@ class PLL_Admin_Site_Health {
 		return $debug_info;
 	}
 
-	public function detect_non_wporg_translations(){
-
-	}
 	/**
 	 * Is the language pack already installed ?
 	 *
@@ -522,19 +519,17 @@ class PLL_Admin_Site_Health {
 	 * @param array  $locales array of WordPress locales.
 	 * @param string $type type of update (may be core, plugin or theme and sometimes core, plugins or themes ).
 	 * @param string $name name of the element (plugin/theme) currently updated. In case of Core update, it's "default"
-	 * @return boolean
+	 * @param array|bool  $already_installed array of already installed language packs.
+	 * @param array  $update_list array of wp.org translation pack update
+	 * @return boolean|array
 	 */
-	public function is_wp_language_installed( $locales, $type, $name, $already_installed = array()) {
-		// fucking inconsistency !
-		if ( 'plugin' === $type ) {
-			$type = 'plugins';
-		}
-		if ( 'theme' === $type ) {
-			$type = 'themes';
-		}
+	public function is_wp_language_installed( $locales, $type, $name, $already_installed = array(), $update_list = array() ) {
 
 		$installed_translations = wp_get_installed_translations( $type );
 		foreach ( $locales as $locale ) {
+			if ( $update_list['plugin'][ $name ][ $locale ] ) {
+				break;
+			}
 			if ( ! empty( $installed_translations[ $name ] ) && $installed_translations[ $name ][ $locale ] ) {
 				$already_installed[ $type ][ $name ][ $locale ] = $locale;
 			}
@@ -546,7 +541,6 @@ class PLL_Admin_Site_Health {
 
 		return false;
 	}
-
 
 	/**
 	 * Returns all available translation updates.
@@ -575,24 +569,26 @@ class PLL_Admin_Site_Health {
 			}
 		}
 
+		// Premiums
 		$activated_plugins = get_option( 'active_plugins' );
 		if ( ! empty( $activated_plugins ) ) {
 			foreach ( $activated_plugins as $activated_plugin ) {
-				$plugin_data       = get_plugin_data( WP_PLUGIN_DIR . '/' . $activated_plugin );
-				$already_installed = $this->is_wp_language_installed( $locales, 'plugin', strtolower( $plugin_data['Name']) , $already_installed );
+				$plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . $activated_plugin );
+
+				$already_installed = $this->is_wp_language_installed( $locales, 'plugins', strtolower( $plugin_data['Name'] ), $already_installed, $update_list );
 				foreach ( $pll_locales as $locale ) {
 
 					if ( ! $update_list['plugin'][ strtolower( $plugin_data['Name'] ) ][ $locale->get_locale() ] ) { // Polylang/Polylang Pro case maybe others.
-						$po_path     = dirname( $activated_plugin ) . $plugin_data['DomainPath'] . '/' . $plugin_data['TextDomain'] . '-' . $locale->get_locale() . '.po';
-						$file_exists = file_exists( WP_PLUGIN_DIR . '/' . $po_path );
-						if ( ! file_exists( $file_exists ) ) {
+						$po_path     = trailingslashit( WP_PLUGIN_DIR ) . dirname( $activated_plugin ) . trailingslashit( $plugin_data['DomainPath'] ) . $plugin_data['TextDomain'] . '-' . $locale->get_locale() . '.po';
+						$file_exists = file_exists( $po_path );
+						if ( ! $file_exists ) {
 							$update_list['plugin'][ strtolower( $plugin_data['Name'] ) ][ $locale->get_locale() ] = $locale->get_locale();
 						}
 					}
 				}
 			}
 		}
-		//$update_list = array_diff( $already_installed, $update_list );
+
 		return array_filter( $update_list );
 	}
 
