87e878c8ccf588500bec5da33dc88c76ab823daf
Grégory Viguier <Screenfeed@users.noreply.github.com>
2024-10-03 12:00:43 +0200

    Don't use textdomains until `after_setup_theme` (#1550)

--- include/translated-post.php
+++ include/translated-post.php
@@ -81,11 +81,6 @@ class PLL_Translated_Post extends PLL_Translated_Object implements PLL_Translata
 			$this->tax_language,
 			(array) $this->get_translated_object_types(),
 			array(
-				'labels' => array(
-					'name'          => __( 'Languages', 'polylang' ),
-					'singular_name' => __( 'Language', 'polylang' ),
-					'all_items'     => __( 'All languages', 'polylang' ),
-				),
 				'public'             => false,
 				'show_ui'            => false, // Hide the taxonomy on admin side, needed for WP 4.4.x.
 				'show_in_nav_menus'  => false, // No metabox for nav menus, needed for WP 4.4.x.
