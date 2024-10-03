79c2b0c84d306f6e4c051245aaa8e9af045766a9
Grégory Viguier <git@screenfeed.fr>
2024-10-03 11:14:54 +0200

    Removed labels entirely

--- include/translated-post.php
+++ include/translated-post.php
@@ -81,11 +81,6 @@ class PLL_Translated_Post extends PLL_Translated_Object implements PLL_Translata
 			$this->tax_language,
 			(array) $this->get_translated_object_types(),
 			array(
-				'labels' => array(
-					'name'          => 'Languages',
-					'singular_name' => 'Language',
-					'all_items'     => 'All languages',
-				),
 				'public'             => false,
 				'show_ui'            => false, // Hide the taxonomy on admin side, needed for WP 4.4.x.
 				'show_in_nav_menus'  => false, // No metabox for nav menus, needed for WP 4.4.x.
