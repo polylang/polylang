53f74d5ebcacf29138b226d5dbd9df492fce4288
Frédéric Demarle <frederic.demarle@yahoo.fr>
2024-10-10 16:21:01 +0200

    DRY

--- admin/admin-links.php
+++ admin/admin-links.php
@@ -256,25 +256,7 @@ class PLL_Admin_Links extends PLL_Links {
 
 		// Capability check already done in post-new.php.
 		check_admin_referer( 'new-post-translation' );
-
-		$post_id   = (int) $_GET['from_post'];
-		$lang_slug = sanitize_key( $_GET['new_lang'] );
-
-		if ( $post_id <= 0 || empty( $lang_slug ) ) {
-			return array();
-		}
-
-		$post = get_post( $post_id );
-		$lang = $this->model->get_language( $lang_slug );
-
-		if ( empty( $post ) || empty( $lang ) ) {
-			return array();
-		}
-
-		return array(
-			'from_post' => $post,
-			'new_lang'  => $lang,
-		);
+		return $this->get_objects_from_new_post_translation_request( (int) $_GET['from_post'], sanitize_key( $_GET['new_lang'] ) );
 	}
 
 	/**
@@ -283,13 +265,13 @@ class PLL_Admin_Links extends PLL_Links {
 	 * @since 3.7
 	 *
 	 * @return array {
-	 *     @type WP_Post      $from_post The source post.
+	 *     @type WP_Post      $from_post The source media.
 	 *     @type PLL_Language $new_lang  The target language.
 	 * }
 	 *
 	 * @phpstan-return array{}|array{from_post: WP_Post, new_lang: PLL_Language}|never
 	 */
-	public function get_data_from_new_media_translation_request() {
+	public function get_data_from_new_media_translation_request(): array {
 		if ( ! $this->options['media_support'] ) {
 			return array();
 		}
@@ -299,10 +281,24 @@ class PLL_Admin_Links extends PLL_Links {
 		}
 
 		check_admin_referer( 'translate_media' );
+		return $this->get_objects_from_new_post_translation_request( (int) $_GET['from_media'], sanitize_key( $_GET['new_lang'] ) );
+	}
 
-		$post_id   = (int) $_GET['from_media'];
-		$lang_slug = sanitize_key( $_GET['new_lang'] );
-
+	/**
+	 * Returns the objects given the post ID and language slug provided in the new post translation request.
+	 *
+	 * @since 3.7
+	 *
+	 * @param int    $post_id   The original Post ID provided.
+	 * @param string $lang_slug The new translation language provided
+	 * @return array {
+	 *     @type WP_Post      $from_post The source post.
+	 *     @type PLL_Language $new_lang  The target language.
+	 * }
+	 *
+	 * @phpstan-return array{}|array{from_post: WP_Post, new_lang: PLL_Language}|never
+	 */
+	private function get_objects_from_new_post_translation_request( int $post_id, string $lang_slug ): array {
 		if ( $post_id <= 0 || empty( $lang_slug ) ) {
 			return array();
 		}
