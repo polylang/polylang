57a49c6bbe920bdf9a710640053a6c3c7373b4d6
Hugo Drelon <69580439+Hug0-Drelon@users.noreply.github.com>
2024-11-20 14:21:13 +0100

    Use empty string instead of duplicate for non-translated strings. (#1574)

--- include/mo.php
+++ include/mo.php
@@ -104,4 +104,24 @@ class PLL_MO extends MO {
 	public function delete_entry( $string ) {
 		unset( $this->entries[ $string ] );
 	}
+
+	/**
+	 * Translates a string or returns false if the translation is not found.
+	 * Contrary to `self::translate()`, this method doesn't fallback to the source string but returns empty string instead.
+	 *
+	 * @since 3.7
+	 *
+	 * @param string $source The source string to translate.
+	 * @return string The translated string or empty string if not found.
+	 */
+	public function translate_if_any( string $source ) {
+		$entry = new Translation_Entry( array( 'singular' => $source ) );
+		$entry = $this->translate_entry( $entry );
+
+		if ( ! $entry instanceof Translation_Entry || empty( $entry->translations ) ) {
+			return '';
+		}
+
+		return $entry->translations[0];
+	}
 }
--- tests/phpunit/tests/test-upgrade.php
+++ tests/phpunit/tests/test-upgrade.php
@@ -109,4 +109,54 @@ class Upgrade_Test extends PLL_UnitTestCase {
 		$this->assertSame( 'yes', get_option( 'pll_language_from_content_available' ) );
 		$this->assertSame( 0, $options->get( 'force_lang' ) );
 	}
+
+	public function test_should_clean_up_strings_translations_on_upgrade_to_3_7() {
+		$fr = $this->factory()->language->create_and_get(
+			array( 'locale' => 'fr_FR' )
+		);
+		update_term_meta(
+			$fr->term_id,
+			'_pll_strings_translations',
+			array(
+				array( 'Hello', 'Hello' ),
+				array( 'World', 'World' ),
+			)
+		);
+
+		$en = $this->factory()->pll_model->languages->get( 'en' );
+		update_term_meta(
+			$en->term_id,
+			'_pll_strings_translations',
+			array(
+				array( 'Hello', 'Hello' ),
+				array( 'World', 'World' ),
+			)
+		);
+
+		$options = self::create_options(
+			array( 'version' => '3.6' )
+		);
+
+		( new PLL_Upgrade( $options ) )->_upgrade();
+
+		$raw_strings_fr = get_term_meta( $fr->term_id, '_pll_strings_translations', true );
+		$raw_strings_en = get_term_meta( $en->term_id, '_pll_strings_translations', true );
+
+		$this->assertSame(
+			array(
+				array( 'Hello', '' ),
+				array( 'World', '' ),
+			),
+			$raw_strings_fr,
+			'Strings translations should have been cleaned up.'
+		);
+		$this->assertSame(
+			array(
+				array( 'Hello', '' ),
+				array( 'World', '' ),
+			),
+			$raw_strings_en,
+			'Strings translations should have been cleaned up.'
+		);
+	}
 }
