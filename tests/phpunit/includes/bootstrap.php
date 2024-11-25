7d672b1e9386244098684857927511d3d2b10451
Grégory Viguier <git@screenfeed.fr>
2024-11-25 16:47:04 +0100

    Add colors to the "Testing xxx" message

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+use function WP_Syntex\Polylang_Phpunit\getCliColors;
 
 $_root_dir = dirname( __DIR__, 3 );
 $_tests_dir = ! empty( getenv( 'WP_TESTS_DIR' ) ) ? getenv( 'WP_TESTS_DIR' ) : $_root_dir . '/tmp/wordpress-tests-lib';
@@ -21,6 +22,7 @@ tests_add_filter(
 define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_root_dir . '/vendor/yoast/phpunit-polyfills/' );
 require_once $_root_dir . '/vendor/antecedent/patchwork/Patchwork.php';
 require_once $_tests_dir . '/includes/bootstrap.php';
+require_once $_root_dir . '/vendor/wpsyntex/wp-phpunit/UnitTests/functions.php';
 
 if ( ! defined( 'DIR_TESTROOT' ) ) {
 	define( 'DIR_TESTROOT', $_tests_dir );
@@ -30,8 +32,10 @@ if ( ! defined( 'PLL_TEST_DATA_DIR' ) ) {
 	define( 'PLL_TEST_DATA_DIR', dirname( __DIR__ ) . '/data/' );
 }
 
+$colors = getCliColors();
+
 printf(
-	'Testing Polylang%1$s %2$s with WordPress %3$s...' . PHP_EOL,
+	"{$colors['info']}Testing Polylang%1\$s %2\$s with WordPress %3\$s...{$colors['no_color']}\n",
 	defined( 'POLYLANG_PRO' ) && POLYLANG_PRO ? ' Pro' : '',
 	POLYLANG_VERSION,
 	$GLOBALS['wp_version']
