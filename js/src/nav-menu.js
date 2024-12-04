c1f54479eb7e021f14fd927065de6ea70ceb8bab
Grégory Viguier <git@screenfeed.fr>
2024-12-04 18:20:47 +0100

    Cache the wrapper element

--- js/src/nav-menu.js
+++ js/src/nav-menu.js
@@ -5,110 +5,110 @@
  */
 
 const pllNavMenu = {
+	/* global pll_data */
+	/**
+	 * The element wrapping the menu elements.
+	 *
+	 * @member {HTMLElement|null}
+	 */
+	wrapper: null,
+
 	/**
 	 * Init.
 	 */
 	init: () => {
-		const handlers = [
-			pllNavMenu.printMetabox.attachEvent,
-			pllNavMenu.ensureContent.attachEvent,
-			pllNavMenu.showHideRows.attachEvent,
-		];
-
 		if ( document.readyState !== 'loading' ) {
-			handlers.forEach( ( handler ) => handler() );
+			pllNavMenu.ready();
 		} else {
-			handlers.forEach( ( handler ) =>
-				document.addEventListener( 'DOMContentLoaded', handler )
-			);
+			document.addEventListener( 'DOMContentLoaded', pllNavMenu.ready );
 		}
 	},
 
 	/**
-	 * Attaches an event to an element.
-	 *
-	 * @param {string} elemId   The HTML id of the element to attach the event to.
-	 * @param {string} type     A case-sensitive string representing the event type to listen for.
-	 * @param {Object} listener The callback to fire on that event.
+	 * Called when the DOM is ready. Attaches the events to the wrapper.
 	 */
-	maybeAttachEvent: ( elemId, type, listener ) => {
-		const wrapper = document.getElementById( elemId );
+	ready: () => {
+		pllNavMenu.wrapper = document.getElementById( 'menu-to-edit' );
 
-		if ( wrapper ) {
-			wrapper.addEventListener( type, listener );
+		if ( ! pllNavMenu.wrapper ) {
+			return;
 		}
+
+		pllNavMenu.wrapper.addEventListener( 'click', pllNavMenu.printMetabox );
+		pllNavMenu.wrapper.addEventListener( 'change', pllNavMenu.ensureContent );
+		pllNavMenu.wrapper.addEventListener( 'change', pllNavMenu.showHideRows );
 	},
 
 	printMetabox: {
 		/**
-		 * Attaches an event to `#menu-to-edit` to print our checkboxes in the language switcher.
+		 * Event callback that prints our checkboxes in the language switcher.
+		 *
+		 * @param {Event} event The event.
 		 */
-		attachEvent: () => {
-			pllNavMenu.maybeAttachEvent( 'menu-to-edit', 'click', ( event ) => {
-				if ( ! event.target.classList.contains( 'item-edit' ) ) {
-					// Not clicking on a Edit arrow button.
-					return;
+		handleEvent: ( event ) => {
+			if ( ! event.target.classList.contains( 'item-edit' ) ) {
+				// Not clicking on a Edit arrow button.
+				return;
+			}
+
+			const metabox = event.target.closest( '.menu-item' ).querySelector( '.menu-item-settings' );
+
+			if ( ! metabox?.id ) {
+				// Should not happen.
+				return;
+			}
+
+			if ( ! metabox.querySelectorAll( 'input[value="#pll_switcher"][type=text]' ).length ) {
+				// Not our metabox, or already replaced.
+				return;
+			}
+
+			// Remove default fields we don't need.
+			for ( const el of metabox.querySelectorAll( 'p:not(.field-move)' ) ) {
+				el.remove();
+			}
+
+			const t      = pllNavMenu.printMetabox;
+			const itemId = Number( metabox.id.replace( 'menu-item-settings-', '' ) );
+
+			metabox.append( t.createHiddenInput( 'title', itemId, pll_data.title ) ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.append
+			metabox.append( t.createHiddenInput( 'url', itemId, '#pll_switcher' ) ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.append
+			metabox.append( t.createHiddenInput( 'detect', itemId, 1 ) ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.append
+
+			const ids          = Array( 'hide_if_no_translation', 'hide_current', 'force_home', 'show_flags', 'show_names', 'dropdown' ); // Reverse order.
+			const isValDefined = typeof( pll_data.val[ itemId ] ) !== 'undefined';
+
+			ids.forEach( ( optionName ) => {
+				// Create the checkbox's wrapper.
+				const inputWrapper = t.createElement( 'p', { class: 'description' } );
+
+				if ( 'hide_current' === optionName && isValDefined && 1 === pll_data.val[ itemId ].dropdown ) {
+					// Hide the `hide_current` checkbox if `dropdown` is checked.
+					inputWrapper.classList.add( 'hidden' );
 				}
 
-				const metabox = event.target.closest( '.menu-item' ).querySelector( '.menu-item-settings' );
+				metabox.prepend( inputWrapper ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.prepend
 
-				if ( ! metabox?.id ) {
-					// Should not happen.
-					return;
-				}
+				// Create the checkbox's label.
+				const inputId = `edit-menu-item-${ optionName }-${ itemId }`;
+				const label   = t.createElement( 'label', { 'for': inputId } );
+				label.innerText = ` ${ pll_data.strings[ optionName ] }`;
 
-				if ( ! metabox.querySelectorAll( 'input[value="#pll_switcher"][type=text]' ).length ) {
-					// Not our metabox, or already replaced.
-					return;
-				}
+				inputWrapper.append( label ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.append
 
-				// Remove default fields we don't need.
-				for ( const el of metabox.querySelectorAll( 'p:not(.field-move)' ) ) {
-					el.remove();
-				}
-
-				const t      = pllNavMenu.printMetabox;
-				const itemId = Number( metabox.id.replace( 'menu-item-settings-', '' ) );
-
-				metabox.append( t.createHiddenInput( 'title', itemId, pll_data.title ) ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.append
-				metabox.append( t.createHiddenInput( 'url', itemId, '#pll_switcher' ) ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.append
-				metabox.append( t.createHiddenInput( 'detect', itemId, 1 ) ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.append
-
-				const ids          = Array( 'hide_if_no_translation', 'hide_current', 'force_home', 'show_flags', 'show_names', 'dropdown' ); // Reverse order.
-				const isValDefined = typeof( pll_data.val[ itemId ] ) !== 'undefined';
-
-				ids.forEach( ( optionName ) => {
-					// Create the checkbox's wrapper.
-					const inputWrapper = t.createElement( 'p', { class: 'description' } );
-
-					if ( 'hide_current' === optionName && isValDefined && 1 === pll_data.val[ itemId ].dropdown ) {
-						// Hide the `hide_current` checkbox if `dropdown` is checked.
-						inputWrapper.classList.add( 'hidden' );
-					}
-
-					metabox.prepend( inputWrapper ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.prepend
-
-					// Create the checkbox's label.
-					const inputId = `edit-menu-item-${ optionName }-${ itemId }`;
-					const label   = t.createElement( 'label', { 'for': inputId } );
-					label.innerText = ` ${ pll_data.strings[ optionName ] }`;
-
-					inputWrapper.append( label ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.append
-
-					// Create the checkbox.
-					const cb = t.createElement( 'input', {
-						type:  'checkbox',
-						id:    inputId,
-						name:  `menu-item-${ optionName }[${ itemId }]`,
-						value: 1,
-					} );
-
-					if ( ( isValDefined && 1 === pll_data.val[ itemId ][ optionName ] ) || ( ! isValDefined && 'show_names' === optionName ) ) { // `show_names` as default value.
-						cb.checked = true;
-					}
-
-					label.prepend( cb ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.prepend
+				// Create the checkbox.
+				const cb = t.createElement( 'input', {
+					type:  'checkbox',
+					id:    inputId,
+					name:  `menu-item-${ optionName }[${ itemId }]`,
+					value: 1,
 				} );
+
+				if ( ( isValDefined && 1 === pll_data.val[ itemId ][ optionName ] ) || ( ! isValDefined && 'show_names' === optionName ) ) { // `show_names` as default value.
+					cb.checked = true;
+				}
+
+				label.prepend( cb ); // phpcs:ignore WordPressVIPMinimum.JS.HTMLExecutingFunctions.prepend
 			} );
 		},
 
@@ -146,71 +146,71 @@ const pllNavMenu = {
 	},
 
 	ensureContent: {
+		regExpr: new RegExp( /^edit-menu-item-show_(names|flags)-(\d+)$/ ),
+
 		/**
-		 * Attaches an event to `#menu-to-edit` to disallow unchecking both `show_names` and `show_flags`.
+		 * Event callback that disallows unchecking both `show_names` and `show_flags`.
+		 *
+		 * @param {Event} event The event.
 		 */
-		attachEvent: () => {
-			const regExpr = new RegExp( /^edit-menu-item-show_(names|flags)-(\d+)$/ );
+		handleEvent: ( event ) => {
+			if ( ! event.target.id || event.target.checked ) {
+				// Now checked, nothing to do.
+				return;
+			}
 
-			pllNavMenu.maybeAttachEvent( 'menu-to-edit', 'change', ( event ) => {
-				if ( ! event.target.id || event.target.checked ) {
-					// Now checked, nothing to do.
-					return;
-				}
+			const matches = event.target.id.match( pllNavMenu.ensureContent.regExpr );
 
-				const matches = event.target.id.match( regExpr );
+			if ( ! matches ) {
+				// Not the checkbox we want.
+				return;
+			}
 
-				if ( ! matches ) {
-					// Not the checkbox we want.
-					return;
-				}
-
-				// Check the other checkbox.
-				const [ , type, id ] = matches;
-				const otherType = 'names' === type ? 'flags' : 'names';
-				document.getElementById( `edit-menu-item-show_${ otherType }-${ id }` ).checked = true;
-			} );
+			// Check the other checkbox.
+			const [ , type, id ] = matches;
+			const otherType = 'names' === type ? 'flags' : 'names';
+			document.getElementById( `edit-menu-item-show_${ otherType }-${ id }` ).checked = true;
 		},
 	},
 
 	showHideRows: {
+		regExpr: new RegExp( /^edit-menu-item-dropdown-(\d+)$/ ),
+
 		/**
-		 * Attaches an event to `#menu-to-edit` to show or hide the `hide_current` checkbox when `dropdown` is checked.
+		 * Event callback that shows or hides the `hide_current` checkbox when `dropdown` is checked.
+		 *
+		 * @param {Event} event The event.
 		 */
-		attachEvent: () => {
-			const regExpr = new RegExp( /^edit-menu-item-dropdown-(\d+)$/ );
+		handleEvent: ( event ) => {
+			if ( ! event.target.id ) {
+				// Not the checkbox we want.
+				return;
+			}
 
-			pllNavMenu.maybeAttachEvent( 'menu-to-edit', 'change', ( event ) => {
-				if ( ! event.target.id ) {
-					// Not the checkbox we want.
-					return;
-				}
+			const matches = event.target.id.match( pllNavMenu.showHideRows.regExpr );
 
-				const matches = event.target.id.match( regExpr );
+			if ( ! matches ) {
+				// Not the checkbox we want.
+				return;
+			}
 
-				if ( ! matches ) {
-					// Not the checkbox we want.
-					return;
-				}
+			const hideCb = document.getElementById( `edit-menu-item-hide_current-${ matches[1] }` );
 
-				const hideCb = document.getElementById( `edit-menu-item-hide_current-${ matches[1] }` );
+			if ( ! hideCb ) {
+				// Should not happen.
+				return;
+			}
 
-				if ( ! hideCb ) {
-					// Should not happen.
-					return;
-				}
+			const description = hideCb.closest( '.description' );
 
-				const description = hideCb.closest( '.description' );
+			// Hide or show.
+			description.classList.toggle( 'hidden', event.target.checked );
 
-				// Hide or show.
-				description.classList.toggle( 'hidden', event.target.checked );
-
-				if ( event.target.checked ) {
-					// Uncheck after hiding.
-					hideCb.checked = false;
-					hideCb.dispatchEvent( new Event( 'change' ) );
-				}
-			} );
+			if ( event.target.checked ) {
+				// Uncheck after hiding.
+				hideCb.checked = false;
+				hideCb.dispatchEvent( new Event( 'change' ) );
+			}
 		},
 	},
 };
