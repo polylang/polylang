1ba0d01ec84ada8fe9078f7e37a6aa87398b850d
Grégory Viguier <git@screenfeed.fr>
2025-03-03 17:01:06 +0100

    Settings menu

--- admin/admin-base.php
+++ admin/admin-base.php
@@ -3,6 +3,8 @@
  * @package Polylang
  */
 
+use WP_Syntex\Polylang\Capabilities\Tools;
+
 /**
  * Setup features available on all admin pages.
  *
@@ -120,36 +122,19 @@ abstract class PLL_Admin_Base extends PLL_Base {
 	public function add_menus() {
 		global $admin_page_hooks;
 
-		// Prepare the list of tabs
-		$tabs = array( 'lang' => __( 'Languages', 'polylang' ) );
+		$tabs   = $this->get_tabs();
+		$parent = false;
 
-		// Only if at least one language has been created
-		if ( $this->model->has_languages() ) {
-			$tabs['strings'] = __( 'Translations', 'polylang' );
-		}
-
-		$tabs['settings'] = __( 'Settings', 'polylang' );
-
-		/**
-		 * Filter the list of tabs in Polylang settings
-		 *
-		 * @since 1.5.1
-		 *
-		 * @param array $tabs list of tab names
-		 */
-		$tabs = apply_filters( 'pll_settings_tabs', $tabs );
-
-		$parent = '';
-
-		foreach ( $tabs as $tab => $title ) {
+		foreach ( $tabs as $tab => $tab_data ) {
 			$page = 'lang' === $tab ? 'mlang' : "mlang_$tab";
+
 			if ( empty( $parent ) ) {
 				$parent = $page;
-				add_menu_page( $title, __( 'Languages', 'polylang' ), 'manage_options', $page, '__return_null', 'dashicons-translation' );
+				add_menu_page( $tab_data['label'], __( 'Languages', 'polylang' ), $tab_data['capability'], $page, '__return_null', 'dashicons-translation' );
 				$admin_page_hooks[ $page ] = 'languages'; // Hack to avoid the localization of the hook name. See: https://core.trac.wordpress.org/ticket/18857
 			}
 
-			add_submenu_page( $parent, $title, $title, 'manage_options', $page, array( $this, 'languages_page' ) );
+			add_submenu_page( $parent, $tab_data['label'], $tab_data['label'], $tab_data['capability'], $page, array( $this, 'languages_page' ) );
 		}
 	}
 
@@ -526,5 +511,84 @@ abstract class PLL_Admin_Base extends PLL_Base {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Returns the list of tabs.
+	 *
+	 * @since 3.8
+	 *
+	 * @return array[] {
+	 *     List of tabs with page slugs as array keys, and arrays as follow as array values:
+	 *
+	 *     @type string $label      Tab title.
+	 *     @type string $capability Optional. User capability. Default is `manage_options`.
+	 * }
+	 *
+	 * @phpstan-return array<non-empty-string, array{label: string, capability: non-empty-string}>
+	 */
+	protected function get_tabs(): array {
+		$tabs = array(
+			'lang' => __( 'Languages', 'polylang' ),
+		);
+
+		// Only if at least one language has been created.
+		if ( $this->model->has_languages() ) {
+			$tabs['strings'] = __( 'Translations', 'polylang' );
+		}
+
+		$tabs['settings'] = __( 'Settings', 'polylang' );
+
+		/**
+		 * Filter the list of tabs in Polylang settings.
+		 *
+		 * @since 1.5.1
+		 * @since 3.8 Added the possibility to pass user capabilities.
+		 *
+		 * @param string[]|string[][] $tabs {
+		 *     List of tabs with page slugs as array keys. Can be an array of titles, or an array of arrays as follow:
+		 *
+		 *     @type string $label      Tab title.
+		 *     @type string $capability Optional. User capability. Default is `manage_options`.
+		 * }
+		 */
+		$tabs = apply_filters( 'pll_settings_tabs', $tabs );
+
+		if ( isset( $tabs['strings'] ) && is_string( $tabs['strings'] ) ) {
+			$tabs['strings'] = array(
+				'label'      => $tabs['strings'],
+				'capability' => Tools::serialize_capabilities( array( 'manage_options', 'translate_strings' ) ),
+			);
+		}
+
+		/** @phpstan-var array<non-empty-string, string|array{label: string, capability: non-empty-string}> $tabs */
+		$return = array();
+
+		foreach ( $tabs as $tab => $label_or_data ) {
+			if ( is_array( $label_or_data ) ) {
+				$menu_data = array_merge(
+					array(
+						'label'      => '',
+						'capability' => 'manage_options',
+					),
+					$label_or_data
+				);
+			} elseif ( is_string( $label_or_data ) ) {
+				// Backard compatibility.
+				$menu_data = array(
+					'label'      => $label_or_data,
+					'capability' => 'manage_options',
+				);
+			} else {
+				continue;
+			}
+
+			if ( current_user_can( $menu_data['capability'] ) ) {
+				// Keep only useful tabs to ease `add_menu_page()` in `PLL_Admin_Base::add_menus()`.
+				$return[ $tab ] = $menu_data;
+			}
+		}
+
+		return $return;
 	}
 }
