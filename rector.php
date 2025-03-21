0504875f4cd00be3192704bcd409720665bc73aa
Grégory Viguier <git@screenfeed.fr>
2025-03-21 18:02:28 +0100

    Shorter lists

--- rector.php
+++ rector.php
@@ -11,13 +11,7 @@ use Rector\Php71\Rector\List_\ListToArrayDestructRector;
 return RectorConfig::configure()
 	->withPaths(
 		[
-			__DIR__ . '/src/admin',
-			__DIR__ . '/src/frontend',
-			__DIR__ . '/src/include',
-			__DIR__ . '/src/install',
-			__DIR__ . '/src/integrations',
-			__DIR__ . '/src/modules',
-			__DIR__ . '/src/settings',
+			__DIR__ . '/src',
 			__DIR__ . '/tests',
 			__DIR__ . '/polylang.php',
 			__DIR__ . '/uninstall.php',
