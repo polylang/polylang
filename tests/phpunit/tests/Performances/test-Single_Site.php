25c4216ed76454985e47303166e6a82dd72f12b8
Hug0-Drelon <hugo.drelon@gmail.com>
2025-03-24 15:39:03 +0100

    Skip new test on multisite you dumb ass...

--- tests/phpunit/tests/Performances/test-Single_Site.php
+++ tests/phpunit/tests/Performances/test-Single_Site.php
@@ -12,6 +12,10 @@ class Single_Site_Test extends PLL_UnitTestCase {
 	 * @see https://github.com/polylang/polylang/issues/2557
 	 */
 	public function test_active_sitewide_plugins_is_not_called() {
+		if ( is_multisite() ) {
+			$this->markTestSkipped( 'This test is not relevant on multisite.' );
+		}
+
 		add_action( 'pll_init_options_for_blog', array( Registry::class, 'register' ) );
 		new Options();
 
