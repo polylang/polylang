c6b3f093441eed5a1468e52c85d435ecd498c7db
Grégory Viguier <git@screenfeed.fr>
2025-04-07 16:12:58 +0200

    Added a test

--- tests/phpunit/tests/test-frontend.php
+++ tests/phpunit/tests/test-frontend.php
@@ -49,4 +49,45 @@ class Frontend_Test extends PLL_UnitTestCase {
 
 		$this->assertInstanceOf( stdClass::class, $wp_admin_bar->get_node( 'customize' ) );
 	}
+
+	/**
+	 * Tests that the main query doesn't do extra queries to fetch the language's `term_taxonomy_id`.
+	 *
+	 * @see https://github.com/polylang/polylang-pro/issues/2562
+	 *
+	 * @return void
+	 */
+	public function test_optimize_query(): void {
+		self::create_language( 'en_US' );
+
+		$options = self::create_options(
+			array(
+				'hide_default' => false,
+				'default_lang' => 'en',
+			)
+		);
+
+		$model = new PLL_Model( $options );
+		$links_model = $model->get_links_model();
+		$links_model->init();
+
+		new PLL_Frontend( $links_model );
+
+		$model->get_language( 'en' ); // Put the query in cache before the following filter.
+		$queries = array();
+
+		add_filter(
+			'query',
+			function ( $query ) use ( &$queries ) {
+				$queries[] = $query;
+				return $query;
+			}
+		);
+
+		$GLOBALS['wp_object_cache']->flush_group( 'term-queries' );
+		$GLOBALS['wp_object_cache']->flush_group( 'terms' );
+		new WP_Query( array( 'lang' => 'en' ) );
+
+		$this->assertCount( 1, $queries );
+	}
 }
