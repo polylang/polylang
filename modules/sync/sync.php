f758c7e174fdbfdf0ff2b6eaa4ac320c99fad8f6
Marianne380 <marianne.honczaruk@gmail.com>
2025-05-22 14:45:28 +0200

    Avoids syncing term parent on term creation if parent term isn't translated. (#1671)

--- modules/sync/sync.php
+++ modules/sync/sync.php
@@ -188,7 +188,7 @@ class PLL_Sync {
 	}
 
 	/**
-	 * Synchronize term parent in translations
+	 * Synchronize term parent in translations.
 	 * Calling clean_term_cache *after* this is mandatory otherwise the $taxonomy_children option is not correctly updated
 	 *
 	 * @since 2.3
@@ -201,28 +201,38 @@ class PLL_Sync {
 	public function sync_term_parent( $term_id, $tt_id, $taxonomy ) {
 		global $wpdb;
 
-		if ( is_taxonomy_hierarchical( $taxonomy ) && $this->model->is_translated_taxonomy( $taxonomy ) ) {
-			$term = get_term( $term_id );
+		if ( ! is_taxonomy_hierarchical( $taxonomy ) || ! $this->model->is_translated_taxonomy( $taxonomy ) ) {
+			return;
+		}
 
-			if ( $term instanceof WP_Term ) {
-				$translations = $this->model->term->get_translations( $term_id );
+		$term = get_term( $term_id );
+		if ( ! $term instanceof WP_Term ) {
+			return;
+		}
 
-				foreach ( $translations as $lang => $tr_id ) {
-					if ( $tr_id !== $term_id ) {
-						$tr_parent = $this->model->term->get_translation( $term->parent, $lang );
-						$tr_term   = get_term( (int) $tr_id, $taxonomy );
+		$translations = $this->model->term->get_translations( $term_id );
 
-						if ( $tr_term instanceof WP_Term && ! ( $term->parent && empty( $tr_parent ) ) ) {
-							$wpdb->update(
-								$wpdb->term_taxonomy,
-								array( 'parent' => $tr_parent ?: 0 ),
-								array( 'term_taxonomy_id' => $tr_term->term_taxonomy_id )
-							);
+		foreach ( $translations as $lang => $tr_id ) {
+			if ( $tr_id === $term_id ) {
+				continue;
+			}
+			
+			$tr_parent = $this->model->term->get_translation( $term->parent, $lang );
+			$tr_term   = get_term( (int) $tr_id, $taxonomy );
 
-							clean_term_cache( $tr_id, $taxonomy ); // OK since WP 3.9.
-						}
-					}
-				}
+			if ( str_starts_with( current_filter(), 'created_' ) && 0 === $tr_parent ) {
+				// Do not remove the existing hierarchy of translations when creating new term without parent.
+				continue;
+			}
+
+			if ( $tr_term instanceof WP_Term && ! ( $term->parent && empty( $tr_parent ) ) ) {
+				$wpdb->update(
+					$wpdb->term_taxonomy,
+					array( 'parent' => $tr_parent ?: 0 ),
+					array( 'term_taxonomy_id' => $tr_term->term_taxonomy_id )
+				);
+
+				clean_term_cache( $tr_id, $taxonomy ); // OK since WP 3.9.
 			}
 		}
 	}
