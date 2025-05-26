125bbbad20b20b9dcd30b59fa37b7602fe92d801
Hug0-Drelon <hugo.drelon@gmail.com>
2025-05-26 16:59:31 +0200

    Proposal: do not enqueue post.js for media.

---
+++ js/src/media.js
@@ -0,0 +1,15 @@
+/**
+ * Media list table
+ * When clicking on attach link, filters find post list per media language
+ */
+jQuery(
+	function ( $ ) {
+		$.ajaxPrefilter(
+			function ( options, originalOptions, jqXHR ) {
+				if ( 'string' === typeof options.data && -1 !== options.data.indexOf( 'action=find_posts' ) ) {
+					options.data = 'pll_post_id=' + $( '#affected' ).val() + '&' + options.data;
+				}
+			}
+		);
+	}
+);
--- js/src/post.js
+++ js/src/post.js
@@ -170,19 +170,3 @@ jQuery(
 		);
 	}
 );
-
-/**
- * Media list table
- * When clicking on attach link, filters find post list per media language
- */
-jQuery(
-	function ( $ ) {
-		$.ajaxPrefilter(
-			function ( options, originalOptions, jqXHR ) {
-				if ( 'string' === typeof options.data && -1 !== options.data.indexOf( 'action=find_posts' ) ) {
-					options.data = 'pll_post_id=' + $( '#affected' ).val() + '&' + options.data;
-				}
-			}
-		);
-	}
-);
