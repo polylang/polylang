00588e77c8e84d81053f9a559682fa396143a46e
Emmanuel Hesry <ehesry@gmail.com>
2025-05-27 09:57:41 +0200

    Makes jQuery.ajaxPrefilter() work

--- js/src/lib/ajax-filter/index.js
+++ js/src/lib/ajax-filter/index.js
@@ -15,35 +15,31 @@ export function ajaxFilter( data ) {
 		return;
 	}
 
-	jQuery(
-		function ( $ ) {
-			const dataStr = $.param( data );
+	const dataStr = jQuery.param( data );
 
-			$.ajaxPrefilter( function ( options ) {
-				if ( -1 === options.url.indexOf( ajaxurl ) && -1 === ajaxurl.indexOf( options.url ) ) {
-					return;
-				}
-
-				if (
-					'undefined' === typeof options.data ||
-					null === options.data ||
-					'string' === typeof options.data && '' === options.data.trim()
-				) {
-					// An empty string or null/undefined.
-					options.data = dataStr;
-				} else if ( 'string' === typeof options.data ) {
-					// A non-empty string: can be a JSON string or a query string.
-					try {
-						options.data = JSON.stringify( Object.assign( JSON.parse( options.data ), data ) );
-					} catch ( exception ) {
-						// A non-empty non-JSON string is considered a query string.
-						options.data = `${ options.data }&${ dataStr }`;
-					}
-				} else if ( $.isPlainObject( options.data ) ) {
-					// An object.
-					options.data = Object.assign( options.data, data );
-				}
-			} );
+	jQuery.ajaxPrefilter( function ( options ) {
+		if ( -1 === options.url.indexOf( ajaxurl ) && -1 === ajaxurl.indexOf( options.url ) ) {
+			return;
 		}
-	);
+
+		if (
+			'undefined' === typeof options.data ||
+			null === options.data ||
+			'string' === typeof options.data && '' === options.data.trim()
+		) {
+			// An empty string or null/undefined.
+			options.data = dataStr;
+		} else if ( 'string' === typeof options.data ) {
+			// A non-empty string: can be a JSON string or a query string.
+			try {
+				options.data = JSON.stringify( Object.assign( JSON.parse( options.data ), data ) );
+			} catch ( exception ) {
+				// A non-empty non-JSON string is considered a query string.
+				options.data = `${ options.data }&${ dataStr }`;
+			}
+		} else if ( jQuery.isPlainObject( options.data ) ) {
+			// An object.
+			options.data = Object.assign( options.data, data );
+		}
+	} );
 }
