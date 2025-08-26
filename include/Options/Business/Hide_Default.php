aaf348c5d3db2e15fdcf2a0dcb5d51ed3a5b3aa4
Sébastien SERRE <sebastienserre@wp-syntex.com>
2025-08-26 14:42:31 +0200

    Update include/Options/Business/Hide_Default.php
    Co-authored-by: Hugo Drelon <69580439+Hug0-Drelon@users.noreply.github.com>

--- include/Options/Business/Hide_Default.php
+++ include/Options/Business/Hide_Default.php
@@ -42,7 +42,7 @@ class Hide_Default extends Abstract_Boolean {
 	 * @return array The updated site health information.
 	 */
 	public function add_to_site_health_info( array $info, Options $options ): array {
-		if ( $options->get( self::key() ) ) {
+		if ( $this->get() ) {
 			$value = '1: ' . __( 'Hide URL language information for default language', 'polylang' );
 		}
 		$value = '0: ' . __( 'Display URL language information for default language', 'polylang' );
