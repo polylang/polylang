cd9656650eb1fba6a3d27b443dccd9be259d917e
Emmanuel Hesry <ehesry@gmail.com>
2025-09-01 11:12:37 +0200

    Moves get_translations() outside loop

--- include/translated-object.php
+++ include/translated-object.php
@@ -162,14 +162,7 @@ abstract class PLL_Translated_Object extends PLL_Translatable_Object {
 		// Sanitize and validate the translations array.
 		$translations = $this->validate_translations( $translations, $id );
 
-		/*
-		 * Gather all existing translations from objects we're about to link together.
-		 * This is needed to clean up old translation groups when merging them.
-		*/
-		$old_translations = array();
-		foreach ( $translations as $tr_id ) {
-			$old_translations = array_merge( $old_translations, $this->get_translations( $tr_id ) );
-		}
+		$old_translations = $this->get_old_translations( $translations );
 
 		foreach ( array_diff_assoc( $old_translations, $translations ) as $tr_id ) {
 			$this->delete_translation( $tr_id );
@@ -505,6 +498,41 @@ abstract class PLL_Translated_Object extends PLL_Translatable_Object {
 		/** @phpstan-var array<non-empty-string, positive-int> $translations */
 		return array_merge( array( $lang->slug => $id ), $translations );
 	}
+
+	/**
+	 * Retrieves old translations from the description field of translation terms.
+	 * This is used to clean up old translation groups when merging them.
+	 *
+	 * @since 3.8
+	 *
+	 * @param int[] $translations An array of object IDs.
+	 * @return int[] An array of object IDs of translations to be removed.
+	 */
+	protected function get_old_translations( $translations ) {
+		$old_translations = array();
+
+		$terms = wp_get_object_terms( $translations, $this->tax_translations );
+
+		if ( ! is_array( $terms ) ) {
+			return $old_translations;
+		}
+
+		foreach ( $terms as $term ) {
+			if ( empty( $term->description ) ) {
+				continue;
+			}
+
+			$trans = maybe_unserialize( $term->description );
+			if ( ! is_array( $trans ) ) {
+				continue;
+			}
+
+			$old_translations = array_merge( $old_translations, $trans );
+		}
+
+		return $old_translations;
+	}
+
 	/**
 	 * Creates translations groups in mass.
 	 *
