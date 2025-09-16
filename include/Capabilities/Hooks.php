f5362c51a534b092b2c30988e29e703e47a97931
Grégory Viguier <git@screenfeed.fr>
2025-09-16 15:19:13 +0200

    Remove the hook `pll_map_meta_cap` It doesn't bring any value.

--- include/Capabilities/Hooks.php
+++ include/Capabilities/Hooks.php
@@ -19,7 +19,7 @@ class Hooks {
 	 * @since 3.8
 	 */
 	public function __construct() {
-		add_filter( 'map_meta_cap', array( $this, 'map_custom_caps' ), 1, 4 );
+		add_filter( 'map_meta_cap', array( $this, 'map_custom_caps' ), 1, 2 );
 	}
 
 	/**
@@ -27,31 +27,15 @@ class Hooks {
 	 *
 	 * @since 3.8
 	 *
-	 * @param string[] $caps    Primitive capabilities required by the user.
-	 * @param string   $cap     Capability being checked.
-	 * @param int      $user_id The user ID.
-	 * @param array    $args    Adds context to the capability check, typically
-	 *                          starting with an object ID.
+	 * @param string[] $caps Primitive capabilities required by the user.
+	 * @param string   $cap  Capability being checked.
 	 * @return string[]
 	 */
-	public function map_custom_caps( $caps, $cap, $user_id, $args ) {
-		switch ( $cap ) {
-			case 'manage_translations':
-				$caps = array( 'manage_options' );
-				break;
+	public function map_custom_caps( $caps, $cap ) {
+		if ( 'manage_translations' === $cap ) {
+			return array( 'manage_options' );
 		}
 
-		/**
-		 * Filter the user capabilities to handle PLL's custom capabilities.
-		 *
-		 * @since 3.8
-		 *
-		 * @param string[] $caps    Primitive capabilities required by the user.
-		 * @param string   $cap     Capability being checked.
-		 * @param int      $user_id The user ID.
-		 * @param array    $args    Adds context to the capability check, typically
-		 *                          starting with an object ID.
-		 */
-		return apply_filters( 'pll_map_meta_cap', $caps, $cap, $user_id, $args );
+		return $caps;
 	}
 }
