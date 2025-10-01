1a5768e9db846f87d1d2b1d70fc5d00e9dfa7b12
Emmanuel Hesry <ehesry@gmail.com>
2025-10-01 18:28:04 +0200

    - Isolates completely the test in a test suite to ensure Polylang will be never loaded by another test - Reverts using factory()->pll_model which breaks a lot of other tests

--- tests/phpunit/tests/plugins/test-model-maybe-create-language-terms.php
+++ tests/phpunit/tests/plugins/test-model-maybe-create-language-terms.php
@@ -1,10 +1,12 @@
 <?php
 
 /**
- * This test was moved from `Model_Test` class to avoid to load Polylang API
- * before registering the custom table as it's done in production.
+ * This test was moved from `Model_Test` class and isoloted in its own test suite
+ * to avoid to load Polylang API before registering the custom table as it's done in production.
+ * Polylang API could be required by other tests by calling `self::require_api()` method.
+ * /!\ This make it loaded for the whole running PHPUnit process.
  * The goal is to check any use of Polylang API functions which will trigger a fatal error.
- * 
+ *
  * @ticket #2713 {@see https://github.com/polylang/polylang-pro/issues/2713}
  */
 class Model_Maybe_Create_Language_Terms_Test extends PLL_UnitTestCase {
@@ -13,17 +15,17 @@ class Model_Maybe_Create_Language_Terms_Test extends PLL_UnitTestCase {
 		// Translatable custom table.
 		require_once PLL_TEST_DATA_DIR . 'translatable.php';
 
-		$foo = new PLLTest_Translatable( self::factory()->pll_model );
+		$foo = new PLLTest_Translatable( self::$model );
 		$tax = $foo->get_tax_language();
-		self::factory()->pll_model->translatable_objects->register( $foo );
+		self::$model->translatable_objects->register( $foo );
 
 		// Languages we'll work with.
-		self::factory()->language->create( array( 'locale' => 'es_ES' ) );
-		self::factory()->language->create( array( 'locale' => 'de_DE' ) );
+		self::create_language( 'es_ES' );
+		self::create_language( 'de_DE' );
 
 		// Get the term_ids to delete.
 		$term_ids = array();
-		foreach ( self::factory()->pll_model->get_languages_list() as $language ) {
+		foreach ( self::$model->get_languages_list() as $language ) {
 			if ( 'es' === $language->slug || 'de' === $language->slug ) {
 				$term_ids[] = $language->get_tax_prop( $tax, 'term_id' );
 			}
@@ -36,12 +38,12 @@ class Model_Maybe_Create_Language_Terms_Test extends PLL_UnitTestCase {
 			wp_delete_term( $term_id, $tax );
 		}
 
-		self::factory()->pll_model->clean_languages_cache();
-		$links_model = self::factory()->pll_model->get_links_model();
+		self::$model->clean_languages_cache();
+		$links_model = self::$model->get_links_model();
 		$GLOBALS['polylang'] = new PLL_Admin( $links_model );
 
 		// Make sure the terms are deleted.
-		foreach ( self::factory()->pll_model->get_languages_list() as $language ) {
+		foreach ( self::$model->get_languages_list() as $language ) {
 			if ( 'es' === $language->slug || 'de' === $language->slug ) {
 				$this->assertSame( 0, $language->get_tax_prop( $tax, 'term_id' ), "Expected to have no '$tax' term_ids for the new languages." );
 				$this->assertSame( 0, $language->get_tax_prop( $tax, 'term_taxonomy_id' ), "Expected to have no '$tax' term_taxonomy_ids for the new languages." );
@@ -49,14 +51,14 @@ class Model_Maybe_Create_Language_Terms_Test extends PLL_UnitTestCase {
 		}
 
 		// Re-create missing terms.
-		self::factory()->pll_model->maybe_create_language_terms();
+		self::$model->maybe_create_language_terms();
 
 		$this->assertSame( array( 'foo_language' ), get_option( 'pll_language_taxonomies' ) );
 
 		// Make sure the terms are re-created.
 		$tt_ids = array();
 		$slugs  = array();
-		foreach ( self::factory()->pll_model->get_languages_list() as $language ) {
+		foreach ( self::$model->get_languages_list() as $language ) {
 			if ( 'es' === $language->slug || 'de' === $language->slug ) {
 				$tt_id             = $language->get_tax_prop( $tax, 'term_taxonomy_id' );
 				$term_id           = $language->get_tax_prop( $tax, 'term_id' );
