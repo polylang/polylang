0b19dc2c9c0c612ff1337e4638fa27354dd52b96
sebastienserre <sebastien@thivinfo.com>
2025-10-09 11:52:21 +0200

    add `get_site_health_info` to `Previous_Version` for site health data management

--- include/Options/Business/Previous_Version.php
+++ include/Options/Business/Previous_Version.php
@@ -5,6 +5,8 @@
 
 namespace WP_Syntex\Polylang\Options\Business;
 
+use WP_Syntex\Polylang\Options\Options;
+
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -35,5 +37,22 @@ class Previous_Version extends Version {
 	 */
 	protected function get_description(): string {
 		return __( "Polylang's previous version.", 'polylang' );
+	}
+
+	/**
+	 * Adds information to the site health info array.
+	 *
+	 * @since 3.8
+	 *
+	 * @param Options $options An instance of the Options class providing additional configuration.
+	 *
+	 * @return array The updated site health information.
+	 */
+	public function get_site_health_info( Options $options ): array { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+		if ( ! $this->get() ) {
+			$value = __( 'This is the first activation', 'polylang' );
+		}
+
+		return $this->format_single_value_for_site_health_info( $value );
 	}
 }
