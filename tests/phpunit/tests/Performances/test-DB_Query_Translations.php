0a8f69f6e1ed1092cc7cbddb13290760c2819cd6
Emmanuel Hesry <ehesry@gmail.com>
2025-10-09 16:32:54 +0200

    Adds expected queries comments for assertions

--- tests/phpunit/tests/Performances/test-DB_Query_Translations.php
+++ tests/phpunit/tests/Performances/test-DB_Query_Translations.php
@@ -32,6 +32,18 @@ class DB_Query_Translations_Test extends PLL_UnitTestCase {
 			}
 		);
 
+		/*
+		 * Expected queries:
+		 *
+		 * `wp_get_object_terms()` call:
+		 * --------------------------------------------------------------------
+		 * SELECT DISTINCT t.term_id, tr.object_id
+		 *    FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id INNER JOIN wptests_term_relationships AS tr ON tr.term_taxonomy_id = tt.term_taxonomy_id
+		 *    WHERE tt.taxonomy IN ('language', 'post_translations') AND tr.object_id IN (4)
+		 *    ORDER BY t.name ASC
+		 *
+		 * SELECT t.*, tt.* FROM wptests_terms AS t INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id WHERE t.term_id IN (2,8)
+		 */
 		$this->assertSame( 2, $query_count, 'Number of queries when getting post translations should be 2.' );
 	}
 
@@ -59,6 +71,108 @@ class DB_Query_Translations_Test extends PLL_UnitTestCase {
 			}
 		);
 
+		/*
+		 * Expected queries:
+		 *
+		 * `wp_get_object_terms()` call:
+		 * --------------------------------------------------------------------
+		 * SELECT DISTINCT t.term_id, tr.object_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id INNER JOIN wptests_term_relationships AS tr ON tr.term_taxonomy_id = tt.term_taxonomy_id
+		 *     WHERE tt.taxonomy IN ('language', 'post_translations') AND tr.object_id IN (7)
+		 *     ORDER BY t.name ASC
+		 *
+		 * SELECT DISTINCT t.term_id, tr.object_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id INNER JOIN wptests_term_relationships AS tr ON tr.term_taxonomy_id = tt.term_taxonomy_id
+		 *     WHERE tt.taxonomy IN ('language', 'post_translations') AND tr.object_id IN (8)
+		 *     ORDER BY t.name ASC
+		 *
+		 * SELECT DISTINCT t.term_id, tr.object_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id INNER JOIN wptests_term_relationships AS tr ON tr.term_taxonomy_id = tt.term_taxonomy_id
+		 *     WHERE tt.taxonomy IN ('language', 'post_translations') AND tr.object_id IN (9)
+		 *     ORDER BY t.name ASC
+		 *
+		 * SELECT t.*, tt.* FROM wptests_terms AS t INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id WHERE t.term_id IN (2)
+		 * SELECT t.*, tt.* FROM wptests_terms AS t INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id WHERE t.term_id IN (4)
+		 * SELECT t.*, tt.* FROM wptests_terms AS t INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id WHERE t.term_id IN (6)
+
+		 * `wp_insert_term()` call:
+		 * --------------------------------------------------------------------
+		 * SELECT  t.term_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id
+		 *     WHERE tt.taxonomy IN ('post_translations') AND t.name IN ('pll_68e7b3e475f0c') AND tt.parent = '0'
+		 *     ORDER BY t.name ASC
+		 *
+		 * SELECT  t.term_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id
+		 *     WHERE t.slug IN ('pll_68e7b3e475f0c')
+		 *     ORDER BY t.term_id ASC
+		 *     LIMIT 1
+
+		 * SELECT  t.term_id
+		 *    FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id
+		 *    WHERE t.name IN ('pll_68e7b3e475f0c')
+		 *    ORDER BY t.term_id ASC
+		 *    LIMIT 1
+		 *
+		 * INSERT INTO `wptests_terms` (`name`, `slug`, `term_group`) VALUES ('pll_68e7b3e475f0c', 'pll_68e7b3e475f0c', 0)
+		 * SELECT tt.term_taxonomy_id FROM wptests_term_taxonomy AS tt INNER JOIN wptests_terms AS t ON tt.term_id = t.term_id WHERE tt.taxonomy = 'post_translations' AND t.term_id = 9
+		 * INSERT INTO `wptests_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`) VALUES (9, 'post_translations', 'a:3:{s:2:\"en\";i:7;s:2:\"fr\";i:8;s:2:\"de\";i:9;}', 0, 0)
+		 * SELECT t.term_id, t.slug, tt.term_taxonomy_id, tt.taxonomy FROM wptests_terms AS t INNER JOIN wptests_term_taxonomy AS tt ON ( tt.term_id = t.term_id ) WHERE t.slug = 'pll_68e7b3e475f0c' AND tt.parent = 0 AND tt.taxonomy = 'post_translations' AND t.term_id < 9 AND tt.term_taxonomy_id != 9
+		 * SELECT autoload FROM wptests_options WHERE option_name = 'post_translations_children'
+		 *
+		 * `wp_set_object_terms()` call:
+		 * --------------------------------------------------------------------
+		 *.SELECT DISTINCT t.term_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id INNER JOIN wptests_term_relationships AS tr ON tr.term_taxonomy_id = tt.term_taxonomy_id
+		 *     WHERE tt.taxonomy IN ('post_translations') AND tr.object_id IN (7)
+		 *
+		 * SELECT  t.term_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id
+		 *     WHERE tt.taxonomy IN ('post_translations') AND t.slug IN ('pll_68e7b3e475f0c')
+		 *     ORDER BY t.term_id ASC
+		 *     LIMIT 1
+		 *
+		 * SELECT t.*, tt.* FROM wptests_terms AS t INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id WHERE t.term_id IN (9)
+		 * SELECT term_taxonomy_id FROM wptests_term_relationships WHERE object_id = 7 AND term_taxonomy_id = 9
+		 * INSERT INTO `wptests_term_relationships` (`object_id`, `term_taxonomy_id`) VALUES (7, 9)
+		 * SELECT COUNT(*) FROM wptests_term_relationships WHERE term_taxonomy_id = 9
+		 * UPDATE `wptests_term_taxonomy` SET `count` = 1 WHERE `term_taxonomy_id` = 9
+		 * SELECT term_id, taxonomy FROM wptests_term_taxonomy WHERE term_taxonomy_id IN (9)
+		 *
+		 * SELECT DISTINCT t.term_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id INNER JOIN wptests_term_relationships AS tr ON tr.term_taxonomy_id = tt.term_taxonomy_id
+		 *     WHERE tt.taxonomy IN ('post_translations') AND tr.object_id IN (8)
+		 *
+		 * SELECT  t.term_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id
+		 *     WHERE tt.taxonomy IN ('post_translations') AND t.slug IN ('pll_68e7b3e475f0c')
+		 *     ORDER BY t.term_id ASC
+		 *     LIMIT 1
+		 *
+		 * SELECT t.*, tt.* FROM wptests_terms AS t INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id WHERE t.term_id IN (9)
+		 * SELECT term_taxonomy_id FROM wptests_term_relationships WHERE object_id = 8 AND term_taxonomy_id = 9
+		 * INSERT INTO `wptests_term_relationships` (`object_id`, `term_taxonomy_id`) VALUES (8, 9)
+		 * SELECT COUNT(*) FROM wptests_term_relationships WHERE term_taxonomy_id = 9
+		 * UPDATE `wptests_term_taxonomy` SET `count` = 2 WHERE `term_taxonomy_id` = 9
+		 * SELECT term_id, taxonomy FROM wptests_term_taxonomy WHERE term_taxonomy_id IN (9)
+		 *
+		 * SELECT DISTINCT t.term_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id INNER JOIN wptests_term_relationships AS tr ON tr.term_taxonomy_id = tt.term_taxonomy_id
+		 *     WHERE tt.taxonomy IN ('post_translations') AND tr.object_id IN (9)
+		 *
+		 * SELECT  t.term_id
+		 *     FROM wptests_terms AS t  INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id
+		 *     WHERE tt.taxonomy IN ('post_translations') AND t.slug IN ('pll_68e7b3e475f0c')
+		 *     ORDER BY t.term_id ASC
+		 *     LIMIT 1
+		 *
+		 * SELECT t.*, tt.* FROM wptests_terms AS t INNER JOIN wptests_term_taxonomy AS tt ON t.term_id = tt.term_id WHERE t.term_id IN (9)
+		 * SELECT term_taxonomy_id FROM wptests_term_relationships WHERE object_id = 9 AND term_taxonomy_id = 9
+		 * INSERT INTO `wptests_term_relationships` (`object_id`, `term_taxonomy_id`) VALUES (9, 9)
+		 * SELECT COUNT(*) FROM wptests_term_relationships WHERE term_taxonomy_id = 9
+		 * UPDATE `wptests_term_taxonomy` SET `count` = 3 WHERE `term_taxonomy_id` = 9
+		 * SELECT term_id, taxonomy FROM wptests_term_taxonomy WHERE term_taxonomy_id IN (9)
+		 */
 		$this->assertSame( 38, $query_count, 'Number of queries when getting post translations should be 2.' );
 	}
 
