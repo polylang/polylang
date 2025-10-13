a8096b0418787307a09bced8dfd46e0f34242884
Hug0-Drelon <hugo.drelon@gmail.com>
2025-10-13 17:20:37 +0200

    Move language context logic from PLL_CRUD_Posts and PLL_CRUD_Terms.

--- include/crud-posts.php
+++ include/crud-posts.php
@@ -321,46 +321,6 @@ class PLL_CRUD_Posts {
 	}
 
 	/**
-	 * Returns the language to set for a post creation.
-	 *
-	 * @since 3.8
-	 *
-	 * @param User $user    The user object.
-	 * @param int  $post_id The post ID for which to set the language. Default `0`.
-	 * @return PLL_Language The language context.
-	 */
-	public function get_language_from_context( User $user, int $post_id = 0 ): PLL_Language {
-		/** @var PLL_Language $default_language The default language is always defined. */
-		$default_language = $this->model->get_default_language();
-		$language         = null;
-		if ( ! empty( $_GET['new_lang'] ) && $lang = $this->model->get_language( sanitize_key( $_GET['new_lang'] ) ) ) { // phpcs:ignore WordPress.Security.NonceVerification
-			// Defined only on admin.
-			$language = $lang;
-		} elseif ( ! isset( $this->pref_lang ) && ! empty( $_REQUEST['lang'] ) && $lang = $this->model->get_language( sanitize_key( $_REQUEST['lang'] ) ) ) { // phpcs:ignore WordPress.Security.NonceVerification
-			// Testing $this->pref_lang makes this test pass only on frontend.
-			$language = $lang;
-		} elseif ( $this->request && $lang = $this->request->get_language() ) {
-			// REST request.
-			$language = $lang;
-		} elseif ( ( $parent_id = wp_get_post_parent_id( $post_id ) ) && $parent_lang = $this->model->post->get_language( $parent_id ) ) {
-			// Use parent if exists.
-			$language = $parent_lang;
-		} elseif ( isset( $this->pref_lang ) && $user->can_translate( $this->pref_lang ) ) {
-			// Always defined on admin, never defined on frontend.
-			$language = $this->pref_lang;
-		} elseif ( ! empty( $this->curlang ) ) {
-			// Only on frontend due to the previous test always true on admin.
-			$language = $this->curlang;
-		} elseif ( $user->is_translator() ) {
-			// Use default language if user can translate into it or its preferred one.
-			$language = $user->can_translate( $default_language ) ? $default_language : $user->get_preferred_language( $this->model );
-		}
-
-		// In all other cases use default language because we must have a language to set.
-		return $language ?? $default_language;
-	}
-
-	/**
 	 * Makes sure that all terms in the given list are in the given language.
 	 * If not the case, the terms are translated or created (for a hierarchical taxonomy, terms are created recursively).
 	 *
--- include/crud-terms.php
+++ include/crud-terms.php
@@ -304,49 +304,4 @@ class PLL_CRUD_Terms {
 
 		return $term_slug->get_suffixed_slug( '-' );
 	}
-
-	/**
-	 * Returns the language to set for a post creation.
-	 *
-	 * @since 3.8
-	 *
-	 * @param User   $user     The user object.
-	 * @param int    $term_id  The term ID for which to set the language. Default `0`.
-	 * @param string $taxonomy The taxonomy for which to set the language. Default `''`.
-	 * @return PLL_Language The language context.
-	 */
-	public function get_language_from_context( User $user, int $term_id = 0, string $taxonomy = '' ): PLL_Language {
-		/** @var PLL_Language $default_language The default language is always defined. */
-		$default_language = $this->model->get_default_language();
-		$language         = null;
-		if ( ! empty( $_GET['new_lang'] ) && $lang = $this->model->get_language( sanitize_key( $_GET['new_lang'] ) ) ) { // phpcs:ignore WordPress.Security.NonceVerification
-			// Defined only on admin.
-			$language = $lang;
-		} elseif ( ! empty( $_POST['term_lang_choice'] ) && is_string( $_POST['term_lang_choice'] ) && $lang = $this->model->get_language( sanitize_key( $_POST['term_lang_choice'] ) ) ) { // phpcs:ignore WordPress.Security.NonceVerification
-			$language = $lang;
-		} elseif ( ! empty( $_POST['inline_lang_choice'] ) && is_string( $_POST['inline_lang_choice'] ) && $lang = $this->model->get_language( sanitize_key( $_POST['inline_lang_choice'] ) ) ) { // phpcs:ignore WordPress.Security.NonceVerification
-			$language = $lang;
-		} elseif ( ! isset( $this->pref_lang ) && ! empty( $_REQUEST['lang'] ) && $lang = $this->model->get_language( sanitize_key( $_REQUEST['lang'] ) ) ) { // phpcs:ignore WordPress.Security.NonceVerification
-			// Testing $this->pref_lang makes this test pass only on frontend.
-			$language = $lang;
-		} elseif ( $this->request && $lang = $this->request->get_language() ) {
-			// REST request.
-			$language = $lang;
-		} elseif ( ( $term = get_term( $term_id, $taxonomy ) ) && ! empty( $term->parent ) && $parent_lang = $this->model->term->get_language( $term->parent ) ) {
-			// Sets language from term parent if exists thanks to Scott Kingsley Clark.
-			$language = $parent_lang;
-		} elseif ( isset( $this->pref_lang ) && $user->can_translate( $this->pref_lang ) ) {
-			// Always defined on admin, never defined on frontend.
-			$language = $this->pref_lang;
-		} elseif ( ! empty( $this->curlang ) ) {
-			// Only on frontend due to the previous test always true on admin.
-			$language = $this->curlang;
-		} elseif ( $user->is_translator() ) {
-			// Use default language if user can translate into it or its preferred one.
-			$language = $user->can_translate( $default_language ) ? $default_language : $user->get_preferred_language( $this->model );
-		}
-
-		// In all other cases use default language because we must have a language to set.
-		return $language ?? $default_language;
-	}
 }
