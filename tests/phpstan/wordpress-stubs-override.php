88e3ea4dbf7d62d63f8d100cf5c938b2e6c8084d
Hug0-Drelon <hugo.drelon@gmail.com>
2025-11-05 13:03:43 +0100

    Propose wordpress stubs override.

---
+++ tests/phpstan/wordpress-stubs-override.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Sanitize key accepts anything and returns a string.
+ *
+ * @param mixed $key
+ * @return string
+ */
+function sanitize_key( $key ) {}
+
+/**
+ * Maybe serialize accepts only objects, arrays and strings and always returns a scalar.
+ *
+ * @param string|array<mixed>|object $data
+ * @return scalar
+ */
+function maybe_serialize( $data ) {}
