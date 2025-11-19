f9b6785b37c9b516718ce830a48fd8c0c6eb3e2c
Hug0-Drelon <hugo.drelon@gmail.com>
2025-11-19 20:43:38 +0100

    Lint tests/e2e/global.teardown.mjs.

--- tests/e2e/global.teardown.mjs
+++ tests/e2e/global.teardown.mjs
@@ -1,18 +1,12 @@
 // @ts-check
 import { execSync } from 'child_process';
 
-/**
- * @param {Object} config Playwright config object.
- */
-async function globalTeardown( config ) {
+async function globalTeardown() {
 	// Set permalink structure back to default.
-	execSync(
-		'npx wp-env run tests-cli wp rewrite structure "" --allow-root',
-		{
-			cwd: process.cwd(),
-			stdio: 'ignore',
-		}
-	);
+	execSync( 'npx wp-env run tests-cli wp rewrite structure "" --allow-root', {
+		cwd: process.cwd(),
+		stdio: 'ignore',
+	} );
 }
 
 export default globalTeardown;
