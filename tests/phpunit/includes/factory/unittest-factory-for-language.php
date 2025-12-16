296b6750f0e0d3ac28856d3ee1b4a57b5492393f
Hugo Drelon <69580439+Hug0-Drelon@users.noreply.github.com>
2025-12-16 15:39:34 +0100

    Update tests/phpunit/includes/factory/unittest-factory-for-language.php
    Co-authored-by: Chouby <frederic.demarle@yahoo.fr>

--- tests/phpunit/includes/factory/unittest-factory-for-language.php
+++ tests/phpunit/includes/factory/unittest-factory-for-language.php
@@ -92,7 +92,7 @@ class PLL_UnitTest_Factory_For_Language extends WP_UnitTest_Factory_For_Thing {
 
 		// Backward compatibility with Polylang < 3.8.
 		if ( ! $language instanceof PLL_Language ) {
-			$language = $this->pll_model->languages->get( $args['locale'] );
+			$language = $this->pll_model->languages->get( $args['slug'] ?? $args['locale'] );
 		}
 
 		return $language instanceof PLL_Language ? $language->term_id : new WP_Error( 'Could not get the created language.' );
