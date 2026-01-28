652af2ecff483e026f5c123ffcb3fae2d1de1447
Frédéric Demarle <frederic.demarle@yahoo.fr>
2026-01-28 15:40:10 +0100

    Revert "Simplify logic in `remove()`"
    This reverts commit 427f12a43fb7808b8a19bff1f9e9ede09c892754.

--- include/Options/Options.php
+++ include/Options/Options.php
@@ -472,19 +472,21 @@ class Options implements ArrayAccess, IteratorAggregate {
 			);
 		}
 
-		$option = $this->options[ $this->current_blog_id ][ $key ];
-		
-		if ( ! $option instanceof Abstract_List || ! $option instanceof Abstract_Map ) {
+		if ( $this->options[ $this->current_blog_id ][ $key ] instanceof Abstract_List ) {
+			$done = $this->options[ $this->current_blog_id ][ $key ]->remove( $value );
+		} elseif ( $this->options[ $this->current_blog_id ][ $key ] instanceof Abstract_Map && is_string( $value ) ) {
+			$done = $this->options[ $this->current_blog_id ][ $key ]->remove( $value );
+		} else {
 			return new WP_Error(
 				'pll_invalid_option_type',
 				/* translators: %s is the name of an option. */
 				sprintf( __( 'Option %s is not a list or map.', 'polylang' ), "'$key'" )
 			);
 		}
-		
-		if ( $option->remove( $value ) ) {
+
+		if ( $done ) {
 			$this->modified[ $this->current_blog_id ] = true;
-			return new WP_Error(); 
+			return new WP_Error();
 		}
 
 		return new WP_Error(
