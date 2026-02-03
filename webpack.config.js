5fd0cfce7115717acb893fb3ac06b2ed7dd9ec71
Hug0-Drelon <hugo.drelon@gmail.com>
2026-02-03 09:55:22 +0100

    Let getReactifiedConfig build CSS block file and improve commenting.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@
  */
 const path = require( 'path' );
 const {
-	transformCssEntry,
 	getVanillaConfig,
 	getReactifiedConfig,
 } = require( '@wpsyntex/polylang-build-scripts' );
@@ -12,14 +11,16 @@ const {
 function configureWebpack( options ) {
 	const mode = options.mode;
 	const isProduction = mode === 'production' || false;
-	console.log( 'Webpack mode:', mode );
-	console.log( 'isProduction:', isProduction );
-	console.log( 'dirname:', __dirname );
-
 	const workingDirectory = path.resolve( __dirname );
 	const jsBuildDirectory = path.join( workingDirectory, 'js/build' );
 	const cssBuildDirectory = path.join( workingDirectory, 'css/build' );
 
+	console.log( 'Webpack mode:', mode );
+	console.log( 'Working directory:', workingDirectory );
+
+	/*
+	 * Prepare vanilla config for Polylang Core.
+	 */
 	const commonFoldersToIgnore = [
 		'node_modules/**',
 		'coverage/**',
@@ -38,7 +39,6 @@ function configureWebpack( options ) {
 		'**/*.min.js',
 	];
 
-	// Generate vanilla JS and CSS configs
 	const vanillaConfig = getVanillaConfig( {
 		workingDirectory,
 		jsPatterns: [ '**/*.js' ],
@@ -50,18 +50,9 @@ function configureWebpack( options ) {
 		isProduction,
 	} );
 
-	/**
-	 * Prepare Blocks bundle
+	/*
+	 * Prepare Reactified config for Blocks bundle.
 	 */
-	const blockEditorEntryPointCssFilenameEntries = './css/src/blocks/navigation-language-switcher-editor-style.css';
-
-	console.log(
-		'Building Blocks bundle with entry point: ./js/src/blocks/index.js',
-		'and CSS file:',
-		blockEditorEntryPointCssFilenameEntries
-	);
-
-	// WordPress package dependencies declared as externals.
 	const wpDependencies = [
 		'api-fetch',
 		'block-editor',
@@ -75,7 +66,6 @@ function configureWebpack( options ) {
 		'primitives',
 	];
 
-	// Generate reactified config for blocks
 	const reactifiedConfig = getReactifiedConfig( {
 		entryPoints: { blocks: './js/src/blocks/index.js' },
 		outputPath: workingDirectory,
@@ -83,17 +73,16 @@ function configureWebpack( options ) {
 		isProduction,
 		wpDependencies,
 		additionalExternals: { lodash: 'lodash' },
+		sassLoadPaths: [
+			path.resolve(
+				workingDirectory,
+				'./css/src/blocks/navigation-language-switcher-editor-style.css'
+			),
+		],
 	} );
 
-	// Handle block editor CSS separately
-	const blockEditorCssConfig = [ blockEditorEntryPointCssFilenameEntries ].map(
-		transformCssEntry( cssBuildDirectory, isProduction )
-	);
-
-	// Make webpack configuration.
 	return [
 		...vanillaConfig,
-		...blockEditorCssConfig,
 		...reactifiedConfig,
 	];
 }
