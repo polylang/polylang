6081bb547521b87ae1e221255ed3fb268b3c6e16
Frédéric Demarle <frederic.demarle@yahoo.fr>
2026-05-01 15:42:18 +0200

    Remove wpcom_vip_get_page_by_path()
    deprecated since WP 6.1

--- src/functions.php
+++ src/functions.php
@@ -1,30 +1,10 @@
 <?php
 /**
  * @package Polylang
- */
-
-/**
- * Define wordpress.com VIP equivalent of uncached functions
+ *
  * WordPress backward compatibility functions
- * and miscellaneous utility functions
+ * and miscellaneous utility functions.
  */
-
-if ( ! function_exists( 'wpcom_vip_get_page_by_path' ) ) {
-	/**
-	 * Retrieves a page given its path.
-	 *
-	 * @since 2.8.3
-	 *
-	 * @param string       $page_path Page path.
-	 * @param string       $output    Optional. The required return type. One of OBJECT, ARRAY_A, or ARRAY_N, which correspond to
-	 *                                a WP_Post object, an associative array, or a numeric array, respectively. Default OBJECT.
-	 * @param string|array $post_type Optional. Post type or array of post types. Default 'page'.
-	 * @return WP_Post|array|null WP_Post (or array) on success, or null on failure.
-	 */
-	function wpcom_vip_get_page_by_path( $page_path, $output = OBJECT, $post_type = 'page' ) {
-		return get_page_by_path( $page_path, $output, $post_type ); // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.get_page_by_path_get_page_by_path
-	}
-}
 
 /**
  * Determines whether we should load the cache compatibility
