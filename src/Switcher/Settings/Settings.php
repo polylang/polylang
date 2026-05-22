545ee77a183253da29589a468882931c0de1e8c8
Grégory Viguier <git@screenfeed.fr>
2026-05-22 10:26:44 +0200

    Revert "Fixed `Settings::get_defaults()`"
    This reverts commit 24c018391620bcee5c7eb0dd551c691e4de3583b.

--- src/Switcher/Settings/Settings.php
+++ src/Switcher/Settings/Settings.php
@@ -178,10 +178,7 @@ class Settings extends Abstract_Settings_Legacy {
 	 * @return array
 	 */
 	public static function get_defaults(): array {
-		$properties = array_diff_key(
-			get_class_vars( self::class ),
-			array( 'increment' => 0, 'removed_entries' => 0, 'defaults' => 0 )
-		);
+		$properties = array_diff_key( get_class_vars( self::class ), array( 'increment' => 0 ) );
 
 		$properties['alignment'] = is_rtl() ? 'right' : 'left';
 
