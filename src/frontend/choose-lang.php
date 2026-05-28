dd2640c8417ad1ef0d78714275b148d4ad1d16a9
Marianne380 <marianne.honczaruk@gmail.com>
2026-05-28 09:41:06 +0200

    Fix fatal error when `$_REQUEST['lang']` is a locale in AJAX context. (#1896)

--- src/frontend/choose-lang.php
+++ src/frontend/choose-lang.php
@@ -3,6 +3,8 @@
  * @package Polylang
  */
 
+use WP_Syntex\Polylang\Model\Languages;
+
 /**
  * Base class to choose the language
  *
@@ -60,13 +62,20 @@ abstract class PLL_Choose_Lang {
 	 * @return void
 	 */
 	public function init() {
-		if ( Polylang::is_ajax_on_front() || ! wp_using_themes() ) {
-			$this->set_language( empty( $_REQUEST['lang'] ) ? $this->get_preferred_language() : $this->model->get_language( sanitize_key( $_REQUEST['lang'] ) ) ); // phpcs:ignore WordPress.Security.NonceVerification
+		add_action( 'pre_comment_on_post', array( $this, 'pre_comment_on_post' ) ); // sets the language of comment.
+		add_action( 'parse_query', array( $this, 'parse_main_query' ), 2 ); // sets the language in special cases.
+		add_action( 'wp', array( $this, 'maybe_setcookie' ), 7 );
+
+		if ( ! Polylang::is_ajax_on_front() && wp_using_themes() ) {
+			return;
 		}
 
-		add_action( 'pre_comment_on_post', array( $this, 'pre_comment_on_post' ) ); // sets the language of comment
-		add_action( 'parse_query', array( $this, 'parse_main_query' ), 2 ); // sets the language in special cases
-		add_action( 'wp', array( $this, 'maybe_setcookie' ), 7 );
+		if ( isset( $_REQUEST['lang'] ) && is_string( $_REQUEST['lang'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification
+			// Let's accept either WordPress locales or language codes.
+			$lang = $this->model->languages->get( Languages::is_locale( $_REQUEST['lang'] ) ? $_REQUEST['lang'] : sanitize_key( $_REQUEST['lang'] ) ); // phpcs:ignore WordPress.Security.NonceVerification, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		}
+
+		$this->set_language( empty( $lang ) ? $this->get_preferred_language() : $lang );
 	}
 
 	/**
