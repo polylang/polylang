5e486598ef90cb3c766388949d6505ae390056bf
Grégory Viguier <git@screenfeed.fr>
2026-06-10 15:28:08 +0200

    Adjust elements that should have a flag, but the corresponding language doesn't have one In that case, add a span around the label. Also align the flags to the baseline instead of the middle.

--- src/Switcher/Element/Nav.php
+++ src/Switcher/Element/Nav.php
@@ -71,7 +71,13 @@ class Nav extends Abstract_Element {
 	 */
 	public function get_label(): string {
 		if ( empty( $this->flag ) ) {
-			return esc_html( $this->label );
+			if ( ! $this->settings->show_flags ) {
+				return esc_html( $this->label );
+			}
+			// The element should display a flag but the language doesn't have one: add a `<span>` to maintain
+			// a quite-similar markup than the items that display a flag. It also provides an addistional way to the user
+			// to customize with CSS.
+			return sprintf( '<span class="pll-switcher-label">%s</span>', esc_html( $this->label ) );
 		}
 
 		if ( empty( $this->label ) ) {
@@ -79,7 +85,7 @@ class Nav extends Abstract_Element {
 		}
 
 		return sprintf(
-			'<span class="pll-switcher-flag">%s</span><span>%s</span>',
+			'<span class="pll-switcher-flag">%s</span><span class="pll-switcher-label">%s</span>',
 			(string) preg_replace( '/ style="[^"]*"/', '', $this->flag ),
 			esc_html( $this->label )
 		);
