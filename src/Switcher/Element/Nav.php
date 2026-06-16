c0dc4811c2c51337d3c2e57d35fc28b48ea5996f
Grégory Viguier <git@screenfeed.fr>
2026-06-16 09:54:56 +0200

    Always wrap in spans

--- src/Switcher/Element/Nav.php
+++ src/Switcher/Element/Nav.php
@@ -70,24 +70,17 @@ class Nav extends Abstract_Element {
 	 * @return string
 	 */
 	public function get_label(): string {
-		if ( empty( $this->flag ) ) {
-			if ( ! $this->settings->show_flags ) {
-				return esc_html( $this->label );
-			}
-			// The element should display a flag but the language doesn't have one: add a `<span>` to maintain
-			// a quite-similar markup than the items that display a flag. It also provides an addistional way to the user
-			// to customize with CSS.
-			return sprintf( '<span class="pll-switcher-label">%s</span>', esc_html( $this->label ) );
+		$label = '';
+
+		if ( ! empty( $this->label ) ) {
+			$label .= sprintf( '<span class="pll-switcher-label">%s</span>', esc_html( $this->label ) );
 		}
 
-		if ( empty( $this->label ) ) {
-			return (string) preg_replace( '/ style="[^"]*"/', '', $this->flag );
+		if ( ! empty( $this->flag ) ) {
+			$flag   = (string) preg_replace( '/ style="[^"]*"/', '', $this->flag );
+			$label .= sprintf( '<span class="pll-switcher-flag">%s</span>', $flag );
 		}
 
-		return sprintf(
-			'<span class="pll-switcher-flag">%s</span><span class="pll-switcher-label">%s</span>',
-			(string) preg_replace( '/ style="[^"]*"/', '', $this->flag ),
-			esc_html( $this->label )
-		);
+		return $label;
 	}
 }
