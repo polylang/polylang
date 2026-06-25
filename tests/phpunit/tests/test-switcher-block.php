44c3f58a0e48e4df92956fae35762ebd6d86c109
Hugo Drelon <69580439+Hug0-Drelon@users.noreply.github.com>
2026-06-25 18:53:05 +0200

    Handle `polylang/language-swticher` backward compatibility. (#1915)

--- tests/phpunit/tests/test-switcher-block.php
+++ tests/phpunit/tests/test-switcher-block.php
@@ -4,32 +4,47 @@ use WP_Syntex\Polylang\Blocks\Language_Switcher\Standard;
 use WP_Syntex\Polylang\Blocks\Language_Switcher\Navigation;
 
 class Switcher_Block_Test extends PLL_UnitTestCase {
-	protected static $administrator;
-	protected $server;
+	/**
+	 * @var string
+	 */
 	protected $structure = '/%postname%/';
 
 	/**
-	 * @param WP_UnitTest_Factory $factory
+	 * @var PLL_Frontend
 	 */
-	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
-		parent::wpSetUpBeforeClass( $factory );
+	protected $pll_env;
 
-		self::create_language( 'en_US' );
-		self::create_language( 'fr_FR' );
+	/**
+	 * @var WP_Post
+	 */
+	protected static $en;
 
-		self::$administrator = self::factory()->user->create( array( 'role' => 'administrator' ) );
+	/**
+	 * @var WP_Post
+	 */
+	protected static $fr;
 
-		global $wp_rest_additional_fields;
-		$wp_rest_additional_fields = array();
-		self::require_api();
+	/**
+	 * @param PLL_UnitTest_Factory $factory
+	 */
+	public static function pllSetUpBeforeClass( PLL_UnitTest_Factory $factory ) {
+		parent::pllSetUpBeforeClass( $factory );
+
+		$factory->language->create_many( 2 );
+
+		// Create posts to avoid empty languages in the tests.
+		$posts    = $factory->post->create_translated( array( 'lang' => 'en' ), array( 'lang' => 'fr' ) );
+		self::$en = $posts['en'];
+		self::$fr = $posts['fr'];
 	}
 
 	public function set_up() {
 		parent::set_up();
 
-		global $wp_rewrite, $wp_rest_server;
-		$this->server   = new Spy_REST_Server();
-		$wp_rest_server = $this->server;
+		global $wp_rewrite;
+
+		// Modern theme support for HTML5, allow to render `<nav>` tags.
+		add_theme_support( 'html5', array( 'navigation-widgets' ) );
 
 		self::$model->options['hide_default'] = 1;
 
@@ -42,17 +57,24 @@ class Switcher_Block_Test extends PLL_UnitTestCase {
 
 		$links_model = self::$model->get_links_model();
 		$links_model->init();
-		$pll_env                   = new PLL_REST_Request( $links_model );
-		$GLOBALS['polylang']       = &$pll_env;
-		$pll_env->links            = new PLL_Admin_Links( $pll_env );
-		$pll_env->switcher_block   = ( new Standard\Block( $pll_env ) )->init();
-		$pll_env->navigation_block = ( new Navigation\Block( $pll_env ) )->init();
+		$this->pll_env                   = new PLL_Frontend( $links_model );
+		$GLOBALS['polylang']             = &$this->pll_env;
+		$this->pll_env->links            = new PLL_Frontend_Links( $this->pll_env );
+		$this->pll_env->switcher_block   = ( new Standard\Block( $this->pll_env ) )->init();
+		$this->pll_env->navigation_block = ( new Navigation\Block( $this->pll_env ) )->init();
 
 		// flush rules
 		$wp_rewrite->flush_rules();
 
+		self::require_api();
+
 		do_action( 'init' );
-		do_action( 'rest_api_init' );
+	}
+
+	public static function wpTearDownAfterClass() {
+		wp_delete_post( self::$en, true );
+		wp_delete_post( self::$fr, true );
+		parent::wpTearDownAfterClass();
 	}
 
 	public function tear_down() {
@@ -70,19 +92,12 @@ class Switcher_Block_Test extends PLL_UnitTestCase {
 	 * @return void
 	 */
 	public function test_switcher_block( $block_name ) {
-		wp_set_current_user( self::$administrator );
+		$rendered = $this->render_switcher_block( $block_name );
 
-		$request = new WP_REST_Request( 'GET', '/wp/v2/block-renderer/' . $block_name );
-		$request->set_param( 'context', 'edit' );
-		$request->set_param( 'lang', 'fr' );
-		$response = $this->server->dispatch( $request );
-		$this->assertEquals( 200, $response->get_status() );
-		$switcher = $response->get_data();
-
-		$this->assertNotEmpty( $switcher['rendered'] );
+		$this->assertNotEmpty( $rendered );
 
 		$doc = new DomDocument();
-		@$doc->loadHTML( $switcher['rendered'] );
+		@$doc->loadHTML( $rendered );
 		$xpath = new DOMXpath( $doc );
 
 		$a = $xpath->query( '//li/a[@lang="en-US"]' );
@@ -101,25 +116,23 @@ class Switcher_Block_Test extends PLL_UnitTestCase {
 	 * @return void
 	 */
 	public function test_switcher_block_in_get_posts( $block_name ) {
-		$en = self::factory()->post->create( array( 'post_content' => '<!-- wp:' . $block_name . ' /-->' ) );
-		self::$model->post->set_language( $en, 'en' );
+		$posts = $this->factory()->post->create_translated_and_get(
+			array(
+				'lang'         => 'en',
+				'post_content' => '<!-- wp:' . $block_name . ' /-->',
+			),
+			array(
+				'lang'         => 'fr',
+				'post_content' => '<!-- wp:' . $block_name . ' /-->',
+			)
+		);
+		$en = $posts['en'];
+		$fr = $posts['fr'];
 
-		$fr = self::factory()->post->create( array( 'post_content' => '<!-- wp:' . $block_name . ' /-->' ) );
-		self::$model->post->set_language( $fr, 'fr' );
 
-		self::$model->post->save_translations( $en, compact( 'fr' ) );
-
-		self::$model->clean_languages_cache(); // To get an exact count of posts for the languages.
-
-		$request = new WP_REST_Request( 'GET', '/wp/v2/posts' );
-		$request->set_param( 'lang', 'fr' );
-		$response = $this->server->dispatch( $request );
-		$this->assertEquals( 200, $response->get_status() );
-		$data = $response->get_data();
-
-		$post = reset( $data );
-		$doc = new DomDocument();
-		@$doc->loadHTML( $post['content']['rendered'] );
+		$rendered = $this->render_post_blocks( $fr );
+		$doc      = new DomDocument();
+		@$doc->loadHTML( $rendered );
 		$xpath = new DOMXpath( $doc );
 
 		$a = $xpath->query( '//li/a[@lang="en-US"]' );
@@ -131,17 +144,17 @@ class Switcher_Block_Test extends PLL_UnitTestCase {
 
 	public function test_multiple_switcher_block_dropdown_in_page() {
 		// Create a post with three dropdown language switcher blocks.
-		$en = self::factory()->post->create( array( 'post_content' => '<!-- wp:polylang/language-switcher {"dropdown":true} /--><!-- wp:polylang/language-switcher {"dropdown":true} /--><!-- wp:polylang/language-switcher {"dropdown":true} /-->' ) );
-		self::$model->post->set_language( $en, 'en' );
+		$en = self::factory()->post->create_and_get(
+			array(
+				'post_content' => '<!-- wp:polylang/language-switcher {"dropdown":true} /--><!-- wp:polylang/language-switcher {"dropdown":true} /--><!-- wp:polylang/language-switcher {"dropdown":true} /-->',
+				'lang'         => 'en',
+			)
+		);
 
 		libxml_use_internal_errors( true );
-		$request = new WP_REST_Request( 'GET', '/wp/v2/posts/' . $en );
-		$response = $this->server->dispatch( $request );
-		$this->assertEquals( 200, $response->get_status() );
-		$en_post = $response->get_data();
-		$this->assertNotEmpty( $en_post['content'] );
-		$doc = new DomDocument();
-		$doc->loadHTML( $en_post['content']['rendered'] );
+		$rendered = $this->render_post_blocks( $en );
+		$doc      = new DomDocument();
+		$doc->loadHTML( $rendered );
 		$xpath = new DOMXpath( $doc );
 		libxml_clear_errors();
 
@@ -164,17 +177,13 @@ class Switcher_Block_Test extends PLL_UnitTestCase {
 	 * @return void
 	 */
 	public function test_custom_class_list_switcher_block( $block_name, $class_query_path, $block_class ) {
-		wp_set_current_user( self::$administrator );
-
-		$request = new WP_REST_Request( 'GET', '/wp/v2/block-renderer/' . $block_name );
-		$request->set_param( 'context', 'edit' );
-		$request->set_param( 'lang', 'fr' );
-		$request->set_param( 'attributes', array( 'className' => 'test-class' ) );
-		$response = $this->server->dispatch( $request );
-		$switcher = $response->get_data();
+		$rendered = $this->render_switcher_block(
+			$block_name,
+			array( 'className' => 'test-class' )
+		);
 
 		$doc = new DomDocument();
-		$doc->loadHTML( $switcher['rendered'] );
+		@$doc->loadHTML( $rendered ); // libxml doesn't like `<nav>` tags...
 		$xpath = new DOMXpath( $doc );
 		$node = $xpath->query( $class_query_path );
 		$classes = explode( ' ', $node->item( 0 )->getAttribute( 'class' ) );
@@ -183,60 +192,78 @@ class Switcher_Block_Test extends PLL_UnitTestCase {
 	}
 
 	public function test_switcher_block_list_is_wrapped_in_nav_tag() {
-		wp_set_current_user( self::$administrator );
+		$rendered = $this->render_switcher_block(
+			'polylang/language-switcher',
+			array( 'dropdown' => 0 )
+		);
 
-		$request = new WP_REST_Request( 'GET', '/wp/v2/block-renderer/polylang/language-switcher' );
-		$request->set_param( 'context', 'edit' );
-		$request->set_param( 'attributes', array( 'dropdown' => 0 ) );
-		$response = $this->server->dispatch( $request );
-		$switcher = $response->get_data();
+		$processor = new WP_HTML_Tag_Processor( $rendered );
 
-		$this->assertStringStartsWith( '<nav role="navigation" aria-label="Choose a language">', $switcher['rendered'] );
-		$this->assertStringEndsWith( '</nav>', $switcher['rendered'] );
+		$this->assertTrue( $processor->next_tag(), 'The rendered output should start with a tag.' );
+		$this->assertSame( 'NAV', $processor->get_tag(), 'The first tag should be a nav element.' );
+		$this->assertFalse( $processor->is_tag_closer(), 'The first tag should be an opening nav tag.' );
+
+		$id = $processor->get_attribute( 'id' );
+		$this->assertIsString( $id, 'The nav tag should have an id attribute.' );
+		$this->assertStringStartsWith( 'pll-switcher-', $id, 'The nav id should start with pll-switcher-.' );
+
+		$this->assertSameSets(
+			array( 'pll-switcher', 'pll-layout-vertical', 'pll-alignment-inherit', 'wp-block-polylang-language-switcher' ),
+			iterator_to_array( $processor->class_list() ),
+			'The nav class list should match the expected values.'
+		);
+
+		$this->assertSame( 'Choose a language', $processor->get_attribute( 'aria-label' ), 'The nav tag should have the expected aria-label.' );
+
+		$this->assertTrue(
+			$processor->next_tag(
+				array(
+					'tag_name'    => 'NAV',
+					'tag_closers' => 'visit',
+				)
+			),
+			'The nav tag should have a closing tag.'
+		);
+		$this->assertTrue( $processor->is_tag_closer(), 'The closing tag should be a nav tag.' );
+		$this->assertFalse( $processor->next_tag(), 'Nothing should follow the closing nav tag.' );
 	}
 
 	public function test_switcher_block_dropdown_has_aria_label() {
-		wp_set_current_user( self::$administrator );
+		$rendered = $this->render_switcher_block(
+			'polylang/language-switcher',
+			array( 'dropdown' => 1 )
+		);
 
-		$request = new WP_REST_Request( 'GET', '/wp/v2/block-renderer/polylang/language-switcher' );
-		$request->set_param( 'context', 'edit' );
-		$request->set_param( 'attributes', array( 'dropdown' => 1 ) );
-		$response = $this->server->dispatch( $request );
-		$switcher = $response->get_data();
-
-		$this->assertStringNotContainsString( '<nav>', $switcher['rendered'] );
+		$this->assertStringNotContainsString( '<nav ', $rendered );
 		$this->assertStringContainsString(
-			'<label class="screen-reader-text" for="lang_choice_',
-			$switcher['rendered']
+			'<label class="screen-reader-text" for="pll-switcher-',
+			$rendered
 		);
 		$this->assertStringContainsString(
 			'">Choose a language</label>',
-			$switcher['rendered']
+			$rendered
 		);
 	}
 
 	public function test_custom_class_dropdown_switcher_block() {
-		wp_set_current_user( self::$administrator );
-
-		$request = new WP_REST_Request( 'GET', '/wp/v2/block-renderer/polylang/language-switcher' );
-		$request->set_param( 'context', 'edit' );
-		$request->set_param( 'lang', 'fr' );
-		$request->set_param(
-			'attributes',
+		$rendered = $this->render_switcher_block(
+			'polylang/language-switcher',
 			array(
 				'className' => 'test-class',
 				'dropdown'  => 'true',
 			)
 		);
-		$response = $this->server->dispatch( $request );
-		$switcher = $response->get_data();
 
 		$doc = new DomDocument();
-		$doc->loadHTML( $switcher['rendered'] );
+		$doc->loadHTML( $rendered );
 		$xpath = new DOMXpath( $doc );
 		$node = $xpath->query( '//div[@class]' );
 
-		$this->assertEquals( 'test-class wp-block-polylang-language-switcher', $node->item( 0 )->getAttribute( 'class' ) );
+		$this->assertSameSets(
+			array( 'pll-switcher', 'test-class', 'pll-layout-select', 'pll-alignment-inherit', 'wp-block-polylang-language-switcher' ),
+			explode( ' ', $node->item( 0 )->getAttribute( 'class' ) ),
+			'The class list should be the same.'
+		);
 	}
 
 	/**
@@ -250,8 +277,51 @@ class Switcher_Block_Test extends PLL_UnitTestCase {
 	 */
 	public function polylang_block_name_provider() {
 		return array(
-			array( 'polylang/language-switcher', '//ul[@class]', 'wp-block-polylang-language-switcher' ),
+			array( 'polylang/language-switcher', '//nav[@class]', 'wp-block-polylang-language-switcher' ),
 			array( 'polylang/navigation-language-switcher', '//li[@class]', 'wp-block-polylang-navigation-language-switcher' ),
 		);
+	}
+
+	/**
+	 * Renders a Polylang switcher block using WP_Block.
+	 *
+	 * @param string $block_name Block name.
+	 * @param array  $attributes Block attributes.
+	 * @param array  $context    Block context (for the navigation language switcher).
+	 * @return string
+	 */
+	protected function render_switcher_block( $block_name, $attributes = array(), $context = array() ) {
+		$block = new WP_Block(
+			array(
+				'blockName'    => $block_name,
+				'attrs'        => $attributes,
+				'innerBlocks'  => array(),
+				'innerHTML'    => '',
+				'innerContent' => array(),
+			),
+			$context
+		);
+
+		return $block->render();
+	}
+
+	/**
+	 * Renders the blocks contained in a post. Sets the global `WP_Post` and `WP_Query` objects to mimic a frontend request.
+	 *
+	 * @global WP_Post  $post     The global post object.
+	 * @global WP_Query $wp_query The global WP_Query object.
+	 *
+	 * @param WP_Post $post_to_render The post to render.
+	 * @return string
+	 */
+	protected function render_post_blocks( WP_Post $post_to_render ) {
+		global $post, $wp_query;
+
+		$post                        = $post_to_render;
+		$wp_query->queried_object    = $post_to_render;
+		$wp_query->queried_object_id = $post_to_render->ID;
+		$wp_query->is_single         = true;
+
+		return do_blocks( $post_to_render->post_content );
 	}
 }
