833e8d0a35a75c62813c193c5635c634b10a437a
sebastienserre <sebastienserre@wp-syntex.fr>
2026-08-11 09:00:48 +0200

    fix php CS

--- tests/e2e/plugins/force-classic-editor/force-classic-editor.php
+++ tests/e2e/plugins/force-classic-editor/force-classic-editor.php
@@ -3,4 +3,4 @@
  * Plugin Name: Force Classic Editor
  * Description: Forces Polylang to use classic editor for specific E2E tests.
  */
-add_filter('use_block_editor_for_post_type', '__return_false', 100);
+add_filter( 'use_block_editor_for_post_type', '__return_false', 100 );
