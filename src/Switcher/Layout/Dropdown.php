9ff92c3d90af078321410963035a1b0c99f0b1ac
Grégory Viguier <git@screenfeed.fr>
2026-09-01 17:04:31 +0200

    Improve the "Open submenu" label So it's relevant when closed or open.

--- src/Switcher/Layout/Dropdown.php
+++ src/Switcher/Layout/Dropdown.php
@@ -106,7 +106,7 @@ class Dropdown extends Abstract_Layout {
 		$svg = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none" aria-hidden="true" focusable="false"><path d="M1.5 4L6 8L10.5 4" stroke-width="1.5"></path></svg>';
 		return sprintf(
 			'<button aria-label="%1$s" class="pll-submenu-toggle">%2$s</button>',
-			esc_attr( __( 'Open languages submenu', 'polylang' ) ),
+			esc_attr( __( 'Open/Close languages submenu', 'polylang' ) ),
 			$svg
 		);
 	}
