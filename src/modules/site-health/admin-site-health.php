cb38b0d8555a28e4a13c28c55d23a6684145f655
Aurore Collet <auroreclt@gmail.com>
2026-09-04 10:45:21 +0200

    add type for phpstan

--- src/modules/site-health/admin-site-health.php
+++ src/modules/site-health/admin-site-health.php
@@ -212,7 +212,8 @@ class PLL_Admin_Site_Health {
 	 */
 	public function status_tests( $tests ) {
 		// Add the test only if the homepage is set to an existing static page.
-		$page_on_front = get_option( 'page_on_front' );
+		$page_on_front = (int) get_option( 'page_on_front' );
+
 		if ( 'page' === get_option( 'show_on_front' ) && $page_on_front && get_post( $page_on_front ) ) {
 			$tests['direct']['pll_homepage'] = array(
 				'label' => esc_html__( 'Homepage translated', 'polylang' ),
