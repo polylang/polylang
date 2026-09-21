6b41e485cc88202e714de9e4ccb1c84bcd5c24cf
Grégory Viguier <Screenfeed@users.noreply.github.com>
2026-09-21 10:39:54 +0200

    Update src/admin/admin-filters-post.php
    Co-authored-by: Chouby <fred@wp-syntex.com>

--- src/admin/admin-filters-post.php
+++ src/admin/admin-filters-post.php
@@ -357,12 +357,13 @@ class PLL_Admin_Filters_Post {
 		}
 
 		if ( isset( $views['all'] ) ) { // In case a filter removes this key.
-			$replacement = 'lang=all&';
-
-			if ( ! str_contains( $views['all'], 'all_posts=1' ) ) {
-				$replacement .= 'all_posts=1&';
-			}
-			$views['all'] = str_replace( 'edit.php?', 'edit.php?' . $replacement, $views['all'] );
+   		$views['all'] = add_query_arg(
+   			array( 
+   				'lang'      => 'all',
+   				'all_posts' => 1,
+   			),
+   			$views['all']
+   		);
 		}
 
 		return $views;
